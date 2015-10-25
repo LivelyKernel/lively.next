36bcc6d0604dcf144fe9dcbaa99313d74b539e26
Robert Krahn <robert.krahn@gmail.com>
2015-10-25 00:50:23 -0700

    fix runtime

--- lively-runtime.js
+++ lively-runtime.js
@@ -1,48 +1,17 @@
 lively.require("lively.lang.Runtime").toRun(function() {
 
-  var r = lively.lang.Runtime;
-  r.Registry.addProject(r.Registry.default(), {
+  lively.lang.Runtime.Registry.addProject({
     name: "lively.vm",
-    rootDir: "/home/lively/expt/lively.vm/",
 
     reloadAll: function(project, thenDo) {
-      // var project = r.Registry.default().projects["lively.vm"];
-      // project.reloadAll(project, function(err) { err ? show(err.stack || String(err)) : alertOK("reloaded!"); })
-      var files = ["env.js",
-                   "index.js",
-                   "tests/vm-test.js"
-                  ];
-
-      lively.lang.fun.composeAsync(
-        function deps(n) { lively.requires("lively.MochaTests").toRun(function() { n(); }); },
-        function readFiles(n) {
-          lively.lang.arr.mapAsyncSeries(files,
-            function(fn,_,n) {
-              lively.shell.cat(fn, {cwd: project.rootDir},
-              function(err, c) {
-                show(c)
-                n(err, {name: fn, content: c}); });
-            }, n);
-        },
-        function(fileContents, next) {
-debugger;
-          lively.lang.arr.mapAsyncSeries(fileContents,
-            function(ea,_,n) {
-debugger;
-show(lively.lang.string.joinPath(project.rootDir, ea.name))
-              r.Project.processChange(
-                project, lively.lang.string.joinPath(project.rootDir, ea.name),
-                ea.content, n);
-            },
-            next);
-        }
-      )(thenDo);
+      var files = ["env.js", "index.js", "tests/vm-test.js"];
+      lively.lang.Runtime.loadFiles(project, files, thenDo);
     },
 
     resources: {
 
       "env.js": {
-        matches: /lively.vm\/env.js$/,
+        matches: /env.js$/,
         changeHandler: function(change, project, resource, whenHandled) {
           var state = project.state || (project.state = {
             lively: {
@@ -50,46 +19,32 @@ show(lively.lang.string.joinPath(project.rootDir, ea.name))
               ast: project.state ? project.state.lively.ast : lively.ast
             }
           });
-show(state)
-          evalCode(change.newSource, state, change.resourceId);
-    	  	whenHandled();
+          lively.lang.Runtime.evalCode(project, change.newSource, state, change.resourceId, whenHandled);
         }
       },
 
       "interface code": {
-        matches: /lively.vm\/(lib\/.*|index)\.js$/,
+        matches: /(lib\/.*|index)\.js$/,
         changeHandler: function(change, project, resource, whenHandled) {
           var state = project.state || {};
-          evalCode(change.newSource, state, change.resourceId);
-    	  	whenHandled();
+          lively.lang.Runtime.evalCode(project, change.newSource, state, change.resourceId, whenHandled);
+          if (state.vm) lively.vm = state.vm;
         }
       },
 
       "tests": {
-        matches: /lively.vm\/tests\/.*\.js$/,
+        matches: /tests\/.*\.js$/,
         changeHandler: function(change, project, resource, whenHandled) {
           if (!project.state) {
             var msg = "cannot update runtime for " + change.resourceId + "\n because the runtime state is undefined."
             show(msg); whenHandled(new Error(msg)); return;
           }
           lively.requires("lively.MochaTests").toRun(function() {
-            evalCode(change.newSource, project.state, change.resourceId);
-            lively.MochaTests.runAll();
-      	  	whenHandled();
-          })
+            lively.lang.Runtime.evalCode(project, change.newSource, project.state, change.resourceId, whenHandled);
+          });
         }
       }
     }
   });
 
-  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-  function evalCode(code, state, resourceName) {
-    lively.lang.VM.runEval(code,
-      {topLevelVarRecorder: state, context: state, sourceURL: resourceName},
-      function(err, _result) {
-    		err && show("error when updating the runtime for " + resourceName + "\n" + (err.stack || err));
-    		!err && alertOK("updated");
-    	});
-  }
 });
