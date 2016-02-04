92ab5329589fe0e37cab10fde53e92aaa5e11c24
Robert Krahn <robert.krahn@gmail.com>
2016-02-03 18:46:24 -0800

    ensure tests work on different platforms

--- tests/completion-test.js
+++ tests/completion-test.js
@@ -14,29 +14,26 @@ describe("completion", () => {
     vm.getCompletions(code => vm.syncEval(code, {topLevelVarRecorder: {foo: {bar: 23}}}), "foo.")
       .then(result => {
         expect(result).property("startLetters").to.equal("");
-        expect(result).property("completions").to.deep.equal([
-          ["[object Object]", ["bar"]],
-          ["Object", [
-            "__defineGetter__()",
-            "__defineSetter__()",
-            "__lookupGetter__()",
-            "__lookupSetter__()",
-            "constructor()",
-            "hasOwnProperty()",
-            "isPrototypeOf()",
-            "propertyIsEnumerable()",
-            "toLocaleString()",
-            "toString()",
-            "valueOf()",
-            "__proto__"]]
-        ]);
+        expect(result).property("completions").property(0).to.deep.equal(["[object Object]", ["bar"]]);
+        expect(result).property("completions").property(1).to.containSubset(["Object", [
+          "__defineGetter__()",
+          "__defineSetter__()",
+          "__lookupGetter__()",
+          "__lookupSetter__()",
+          "constructor()",
+          "hasOwnProperty()",
+          "isPrototypeOf()",
+          "propertyIsEnumerable()",
+          "toLocaleString()",
+          "toString()",
+          "valueOf()"]]);
       }));
 
   it("finds inherited props", () =>
     vm.getCompletions(
       vm.syncEval,
-      "obj1 = {m2: function() {}, m3:function(a,b,c) {}}\n"
-            + "obj2 = {a: 3, m1: function(a) {}, m2:function(x) {}, __proto__: obj1}\n"
+      "var obj1 = {m2: function() {}, m3:function(a, b, c) {}},\n"
+            + "obj2 = {a: 3, m1: function(a) {}, m2:function(x) {}, __proto__: obj1};\n"
             + "obj2.")
       .then(result => {
         expect(result).property("startLetters").equal("");
@@ -44,7 +41,7 @@ describe("completion", () => {
         const compls = result.completions,
               objectCompletions = compls.slice(0,2),
               expected = [["[object Object]", ["m1(a)","m2(x)","a"]],
-                          ["prototype", ["m3(a,b,c)"]]]
+                          ["prototype", ["m3(a, b, c)"]]];
         expect(compls).to.have.length(3);
         expect(compls[2][0]).to.equal("Object");
         expect(objectCompletions).to.deep.equal(expected);
