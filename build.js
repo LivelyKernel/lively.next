1ff126edd89cf44660edd37f4af17eae57d663e1
Robert Krahn <robert.krahn@gmail.com>
2016-02-19 23:02:42 -0800

    estree visitor generation in build

--- build.js
+++ build.js
@@ -15,6 +15,7 @@ var uglify          = require("uglify-js"),
     path            = require("path"),
     fun             = require("lively.lang").fun,
     arr             = require("lively.lang").arr,
+    estreeVisitor   = "generated/estree-visitor.js",
     target          = "dist/lively.ast.js",
     targetES5       = target.replace(/\.js$/,".es5.js"),
     targetMin       = target.replace(/\.js$/,".min.js"),
@@ -32,13 +33,15 @@ lively.ast = require('./index');`;
 
 function buildchain(target, exportCode, externals, thenDo) {
   fun.composeAsync(
-      log("1. Creating exports file"),
+      log("1. Creating estree visitor"),
+      createEstreeVisitorModule,
+      log("2. Creating exports file"),
       n => fs.writeFile(exportsFile, exportCode, n),
       n => fs.unlink(target, (err) => n()),
       n => fs.unlink(targetES5, (err) => n()),
       n => fs.unlink(targetMin, (err) => n()),
       n => fs.mkdir("dist", (err) => n()),
-      log("2. browserify"),
+      log("3. browserify"),
       n => {
         var b = browserify().add("./exports.js");
         externals.forEach(ea => b.external(ea));
@@ -46,16 +49,23 @@ function buildchain(target, exportCode, externals, thenDo) {
       },
       (code, n) => n(null, String(code)),
       (code, n) => n(null, code.replace(/require\(['"](lively.lang)['"]\)/g, '$1')),
-      log("3. write " + target),
+      log("4. write " + target),
       (code, n) => fs.writeFile(target, code, err => n(err, code)),
-      log("4. write " + target.replace(/\.js$/,".es5.js")),
+      log("5. write " + target.replace(/\.js$/,".es5.js")),
       (code, n) => fs.writeFile(target.replace(/\.js$/,".es5.js"), babel.transform(code).code, n),
-      log("5. minification"),
+      log("6. minification"),
       n => fs.writeFile(targetMin, uglify.minify(targetES5).code, n)
     )(err => {
       fs.unlink(exportsFile);
       (typeof thenDo === "function") && thenDo(err);
     });
+}
+
+function createEstreeVisitorModule(thenDo) {
+  var estree = require("estree-to-js"),
+      estreeSpec = JSON.parse(fs.readFileSync(require.resolve("estree-to-js/generated/es6.json"))),
+      source = estree.createVisitor(estreeSpec, []/*exceptions*/, "Visitor") + "\nmodule.exports = Visitor;"
+  fs.writeFile(estreeVisitor, source, thenDo);
 }
 
 module.exports = function build() {
