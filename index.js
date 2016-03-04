4f8983a07902000fc2a5dc14ddecc7118c22d465
Robert Krahn <robert.krahn@gmail.com>
2016-03-04 05:41:30 -0800

    converting all lib files to es6

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-/*global module,exports,require*/
+import * as completions from "./lib/completions.js";
+import * as cjs from "./lib/commonjs-interface.js";
+import * as es6 from "./lib/es6-interface.js";
 
-var lang = require("lively.lang");
 
-module.exports = lang.obj.merge(
-  require("./lib/evaluator"), {
-    completions: require("./lib/completions"),
-    cjs: require("./lib/commonjs-interface"),
-    es6: require("./lib/es6-interface")
-  });
+function bootstrap() {
+  return Promise.resolve()
+    .then(() => {
+      // // 1. remove bootVMs nodejs modules from loading cache, not its node_modules
+      // // though!
+      var cache = System._nodeRequire('module').Module._cache,
+          toRemove = Object.keys(cache).filter(name => !name.match("node_modules"));
+      toRemove.forEach(ea => delete cache[ea]);
+  
+      // 2. configure
+      es6._init(); // create a new System instance
+      System._nodeRequire(cjs.resolve("./index-node")).configure();
+      es6.wrapModuleLoad();
+
+      // 3. load the entire vm package minus node_modules via es6
+      return es6.import("./index.js")
+    });
+}
+
+export * from "./lib/evaluator.js";
+export { completions, cjs, es6, bootstrap }
