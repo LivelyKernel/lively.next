2d1068a950d68d5df86a6d86ea413e5decbf407b
Robert Krahn <robert.krahn@gmail.com>
2016-03-05 17:58:20 -0800

    canonicalize es6 paths / urls

--- bootstrap-nodejs.js
+++ bootstrap-nodejs.js
@@ -1,22 +1,15 @@
-// require("./index-node.js").load()
-//   .then(vm => vm.bootstrap(() => System._nodeRequire(__dirname + "/index-node").configure()))
-//   .then(vm => {
-//     console.log("lively.vm bootstrapped...!");
-//     return vm.es6.runEval("Object.keys(currentSystem().__lively_vm__.loadedModules)", {targetModule: "./lib/es6-interface.js"})
-//   })
-//   .then(result => console.log(result)) // load check
-//   .then(() => console.log("DONE"))
-//   .catch(err => console.error(err.stack || err));
-
 var show = console.log;
+
+var testEval = "Object.keys(currentSystem().__lively_vm__.loadedModules)";
 
 require("./index-node.js").bootstrap()
   .then(vm => {
     show("lively.vm bootstrapped...!");
-    return vm.es6.runEval("Object.keys(currentSystem().__lively_vm__.loadedModules)", {targetModule: "lively.vm/lib/es6-interface.js"})
+    return vm.es6.runEval(testEval, {targetModule: "lively.vm/lib/es6-interface.js"})
   })
   .then(result => {
-    show(result);
+    if (result.error || result.value instanceof Error) throw (result.error || result.value);
+    show(testEval + " => ", result.value);
     show("DONE")
   })
   .catch(err => {
