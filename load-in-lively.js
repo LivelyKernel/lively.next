f1f6520cce0b89e9caf234e3a587deb34e2a2610
Robert Krahn <robert.krahn@gmail.com>
2016-03-31 19:47:34 -0700

    load in lively fix

--- load-in-lively.js
+++ load-in-lively.js
@@ -1,6 +1,31 @@
 var System = lively.vm.es6.currentSystem();
 
-System.baseURL
+System.normalizeSync = System.normalizeSync.getOriginal().wrap(function(proceed, name, parentName, parentAddress) {
+  return packageMainRedirect(packageJSONNoJs(proceed(name, parentName, parentAddress)));
+})
+  
+System.normalize = System.normalize.getOriginal().wrap(function(proceed, name, parentName, parentAddress) {
+  var System = this;
+  return proceed(name, parentName, parentAddress)
+    .then(result => packageMainRedirect(packageJSONNoJs(result)))
+});
+
+function packageMainRedirect(path) {
+  var base = path.replace(/\.js$/, "");
+  if (base in System.packages) {
+    var main = System.packages[base].main;
+    if (main) return base.replace(/\/$/, "") + "/" + main.replace(/^\.?\//, "");
+  }
+  return path;
+}
+
+function packageJSONNoJs(path) {
+  return path.match(/package\.json\.js$/) ? path.replace(/\.js$/, "") : path;
+}
+
+// System.baseURL
+// System.normalize("lively.lang", "http://localhost:9001/lively.modules/index.js").then(show.curry("%s"))
+System.normalize("http://localhost:9001/lively.modules/node_modules/lively.ast/package.json").then(show.curry("%s"))
 
 System.config({
   transpiler: "babel",
@@ -9,17 +34,30 @@ System.config({
     "mocha-es6": "lively.modules/node_modules/mocha-es6",
     "lively.vm": "node_modules/lively.vm",
     "lively.ast": "http://localhost:9001/node_modules/lively.vm/node_modules/lively.ast",
+          "path": "@empty",
+      "fs": "@empty",
+      "events": "@empty",
+      "util": "@empty",
+      "os": "@empty",
+      "child_process": "@empty",
   },
   meta: {
     "https://cdnjs.cloudflare.com/ajax/libs/fetch/0.11.0/fetch.js": {
       "format": "global",
       "exports": "fetch"
-    }
+    },
+    "lively.modules": {main: "index.js"},
+  },
+  packages: {
+    "node_modules/lively.lang": {main: "index.js"},
+    "lively.modules/node_modules/lively.lang": {main: "index.js"},
+    "lively.modules/node_modules/lively.ast": {main: "index.js"}
   },
   packageConfigPaths: [
     "lively.modules/node_modules/lively.lang/package.json",
-    "node_modules/lively.vm/package.json",
-    "node_modules/lively.lang/package.json",
+    "lively.modules/node_modules/lively.vm/node_modules/lively.ast/package.json",
+    "lively.modules/node_modules/lively.vm/package.json",
+    "lively.modules/node_modules/lively.lang/package.json",
     "lively.modules/package.json",
     "lively.modules/node_modules/mocha-es6/package.json"]
 })
@@ -30,6 +68,7 @@ System.import("lively.lang")
   .then(() => System.import("lively.vm/node_modules/lively.ast"))
   // .then(() => System.import("lively.modules/index.js"))
   // .then(() => System.import("lively.modules/src/eval.js"))
+  .then(() => System.import("lively.modules"))
   .then(show.curry("%s"))
   .catch(show.curry("%s"))
 
@@ -53,3 +92,18 @@ System.import("lively.lang")
 //   }
 //   return JSON.stringify(json, null, 2);
 // }
+
+// var origNormalize = System.normalize;
+
+System.normalize.getOriginal().wrap(function(proceed, name, parentName, parentAddress) {
+  var System = this;
+  return proceed(name, parentName, parentAddress)
+    .then(result => {
+      var packages = Object.keys(System.packages);
+      if (packages.include(result)) {
+        var main = System.packages[result].main;
+        if (main) return lively.lang.string.joinPath(result, main);
+      }
+      result;
+    })
+})
