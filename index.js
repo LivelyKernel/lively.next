e1c540622f5f23c7a2f3ac9666ba36ea51550660
Robert Krahn <robert.krahn@gmail.com>
2016-03-31 19:54:34 -0700

    start building real interface

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import { obj, arr } from "lively.lang";
 import { getSystem } from "./src/system.js";
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// module access
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+var defaultSystem = getSystem("default");
+export { defaultSystem as System }
 
+import { registerPackage as _registerPackage } from './src/packages.js'
+function registerPackage(packageURL) { return _registerPackage(defaultSystem, packageURL); }
+export { registerPackage }
 
-export {}
+import { moduleSourceChange as _moduleSourceChange } from './src/change.js'
+function moduleSourceChange(moduleName, newSource, options) { return _moduleSourceChange(defaultSystem, moduleName, newSource, options); }
+export { moduleSourceChange };
+
+import {
+  findDependentsOf as _findDependentsOf,
+  findRequirementsOf as _findRequirementsOf,
+  forgetModule as _forgetModule,
+  reloadModule as _reloadModule
+} from './src/dependencies.js'
+function findDependentsOf(module) { return _findDependentsOf(defaultSystem, module); }
+function findRequirementsOf(module) { return _findRequirementsOf(defaultSystem, module); }
+function forgetModule(module, opts) { return _forgetModule(defaultSystem, module, opts); }
+function reloadModule(module, opts) { return _reloadModule(defaultSystem, module, opts); }
+export { findDependentsOf, findRequirementsOf, forgetModule, reloadModule }
