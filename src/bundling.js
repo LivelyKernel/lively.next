4eac1f3e3bdd414f33ddd226544e88d978d5c3a9
Robert Krahn <robert.krahn@gmail.com>
2016-04-23 15:17:30 -0700

    bundler works for self-hosting :)

--- src/bundling.js
+++ src/bundling.js
@@ -4,33 +4,39 @@ import { writeFile, removeFile } from "../tests/helpers.js";
 export { bundle }
 
 function bundle(System, bundleFile, files) {
-  return Promise.all(
-    files.map(file =>
-      System.normalize(file)
-        .then(url =>
-          (System.get(url)  ? Promise.resolve() : System.import(url))
-            .then(() => createRegisterModuleFor(System, url))
-            .then(source => ({url: url, source: source})))))
-    .then(urlAndSources => {
-      var source = arr.pluck(urlAndSources, "source").join('\n\n');
-      return System.normalize(bundleFile).then(outFile => writeFile(outFile, source))
-    });
+  // files = [{moduleName, nameInBundle}]
+  return Promise.all(files.map(ea => {
+        var moduleName = typeof ea === "string" ? ea : ea.moduleName;
+        if (!moduleName) throw new Error("Error bundling module " + ea);
+        var newName = ea.nameInBundle || moduleName;
+        return System.normalize(moduleName)
+          .then(url =>
+            (System.get(url)  ? Promise.resolve() : System.import(url))
+              .then(() => createRegisterModuleFor(System, url, ea.nameInBundle)));
+      }))
+    .then(sources =>
+      System.normalize(bundleFile)
+        .then(outFile => writeFile(outFile, sources.join('\n'))));
 }
 
-function createRegisterModuleFor(System, url, formatOverride) {
+function createRegisterModuleFor(System, url, nameInBundle, formatOverride) {
   var load = System.loads && System.loads[url];
   if (!load) throw new Error(url + " not loaded / traced!");
   var format = formatOverride || load.metadata.format;
-  if (format === "esm" || format === "es6") {
-    return createRegisterModuleFromLoad(load);
+  if (format === "json") {
+    return createJSONRegisterDynamicModuleFromLoad(System, url, nameInBundle, load);
+  } else if (format === "esm" || format === "es6") {
+    return createRegisterModuleFromLoad(load, nameInBundle);
+  } else if (format === "global") {
+    return createGlobalRegisterDynamicModule(System, url, nameInBundle, load.source, load.deps);
   } else if (format === "cjs") {
-    return createRegisterModuleForCommonJSModule(System, url, load.metadata.entry.executingRequire, load.deps);
+    return createCommonJSRegisterDynamicModule(System, url, nameInBundle, load.metadata.entry.executingRequire, load.deps);
   } else {
     throw new Error("Cannot create register module for " + url + " with format " + format);
   }
 }
 
-function createRegisterModuleFromLoad(load) {
+function createRegisterModuleFromLoad(load, nameInBundle) {
   return new Promise((resolve, reject) => {
     var name = load.name;
   
@@ -47,11 +53,11 @@ function createRegisterModuleFromLoad(load) {
     if ("register"       !== Path("body.0.expression.callee.body.body.0.expression.callee.property.name").get(parsed))
       return reject(new Error("Not a call to System.register! (" + name + ")"));
     
-    var moduleName = Path("body.0.expression.arguments.0.value").get(parsed);
+    var moduleName = nameInBundle || Path("body.0.expression.arguments.0.value").get(parsed);
     
     if (!moduleName)
       return reject(new Error("Could not extract module name from " + name));
-  
+
     var registerCall = Path("body.0.expression.callee.body.body.0.expression").get(parsed)
     registerCall["arguments"].unshift({type: "Literal", value: moduleName})
   
@@ -59,136 +65,27 @@ function createRegisterModuleFromLoad(load) {
   });
 }
 
-function createRegisterModuleForCommonJSModule(System, url, executingRequire, deps) {
+function createGlobalRegisterDynamicModule(System, url, nameInBundle, source, deps) {
+  var modSource = `var _retrieveGlobal = System.get("@@global-helpers").prepareGlobal(module.id, null, null);\n`
+                + `(function() {\n${source}\n})();\n`
+                + "return _retrieveGlobal();"
+  return createRegisterDynamicModuleWithSource(System, url, nameInBundle, modSource, false, deps);  
+}
+
+function createCommonJSRegisterDynamicModule(System, url, nameInBundle, executingRequire, deps) {
   // FIXME! deps...
-  var load = {status: 'loading', address: url, name: url, linkSets: [], dependencies: [], metadata: {}},
-      depsString = "[" + (deps.length ? "'" + deps.join("', '") + "'" : "") + "]";
-  return System.fetch(load)
-    .then(source => `System.registerDynamic('${url}', ${depsString}, ${!!executingRequire}, `
-                  + `function(require, exports, module) {\n${source}\n});\n`)
+  var load = {status: 'loading', address: url, name: url, linkSets: [], dependencies: [], metadata: {}};
+  return System.fetch(load).then(source =>
+    createRegisterDynamicModuleWithSource(System, url, nameInBundle, source, executingRequire, deps));
 }
 
-
-
-
-
-function fooooo() {
-// lvm = null
-// lively.vm.es6.currentSystem().import("lively.modules").then(lvm => Global.lvm = lvm)
-// lively.vm.es6.currentSystem().import("lively.modules/src/packages.js").then(lvmp => Global.lvmp = lvmp)
-// lively.vm.es6.currentSystem().import("lively.modules/src/hooks.js").then(lvmh => Global.lvmh = lvmh)
-// lively.vm.es6.currentSystem().import("lively.modules/src/system.js").then(lvms => Global.lvms = lvms)
-
-function reset() {
-  lvms.removeSystem("bundle-test");
-  var S = lvms.getSystem("bundle-test");
-  return S;
+function createJSONRegisterDynamicModuleFromLoad(System, url, nameInBundle, load) {
+  return createRegisterDynamicModuleWithSource(System, url, nameInBundle, "return " + load.source, false, []);
 }
 
-function register() {}
-
-function load() {}
-
-var bundleSystem = reset();
-
-// write("test-dep.js", "export var y = 3;");
-// write("foo.js", "import { y } from './test-dep.js'; debugger; export var x = 2 + y;");
-
-
-var ast = lively.ast;
-var lang = lively.lang;
-
-
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-// bundleSystem.debug = true
-// lvmp.registerPackage(bundleSystem, URL.root + "lively.ast-es6")
-//   .then(_ => {
-//     show(bundleSystem.packages)
-//     show(bundleSystem.map)
-//     return Promise.all([
-//       bundleSystem.import("acorn"),
-//       bundleSystem.import("acorn/dist/walk.js"),
-//       bundleSystem.import("acorn/dist/acorn_loose.js")
-//     ]).then(show.curry("%s")).catch(show.curry("%s"))
-//   })
-//   .catch(show.curry("%s"))
-
-// Object.keys(bundleSystem.loads)
-
-// var urls = [
-//   "http://localhost:9001/lively.ast-es6/node_modules/acorn/dist/acorn.js",
-//   "http://localhost:9001/lively.ast-es6/node_modules/acorn/dist/walk.js",
-//   "http://localhost:9001/lively.ast-es6/node_modules/acorn/dist/acorn_loose.js"]
-
-// Promise.all(urls.map(url => convertCJSToSystemRegister(bundleSystem, url, false, [])))
-//   .then(sources => write("acorn-test-bundle.js", sources.join("\n\n")))
-
-
-// function convertCJSToSystemRegister(System, url, executingRequire, deps) {
-//   return lvms.sourceOf(System, url)
-//     .then(source => `System.registerDynamic('${url}', [], ${!!executingRequire}, `
-//                   + `function(require, exports, module) {\n${source}\n});\n`)
-// }
-
-
-
-// bundleSystem.import("lively.lang").then(show.curry("%s")).catch(show.curry("%s"))
-
-// write("foo-bundle.js",
-//   convertLoadSourceToRegisterModule(bundleSystem.loads["http://localhost:9001/foo.js"])
-//   + "\n"
-//   + convertLoadSourceToRegisterModule(bundleSystem.loads["http://localhost:9001/test-dep.js"]))
-
-bundleSystem.config({
-  bundles: {
-    "foo-bundle.js": ["test-dep.js", "foo.js"]
-  }
-})
-
-
-// bundleSystem.config({
-//   bundles: {
-//     "acorn-test-bundle.js": ["acorn.js", "acorn_loose.js", "walk.js"]
-//   }
-// })
-
-// bundleSystem.register("test-dep.js", [], function($__export) {
-//   "use strict";
-//   return {
-//     setters: [],
-//     execute: function() { $__export("y", 2); }
-//   }
-// });
-
-// bundleSystem.register("foo.js", ["./test-dep.js"], function($__export) {
-//   "use strict";
-//   var x = 23, y;
-//   return {
-//     setters: [function($__m) {
-//       y = $__m.y;
-//     }],
-//     execute: function() {
-//       $__export("x", x + y);
-//     }
-//   }
-// });
-
-// register().then(_ => load()).catch(show.curry("%s"))
-
-// load().catch(show.curry("%s"))
-
-
-// bundleSystem.import("walk.js")
-//   .then(show)
-//   .catch(this.showError.bind(this))
-
-
-bundleSystem.loads["http://localhost:9001/foo-bundle.js"].source
-bundleSystem.import("foo.js")
-  .then(show)
-  .catch(this.showError.bind(this))
-  
-
+function createRegisterDynamicModuleWithSource(System, url, nameInBundle, source, executingRequire, deps) {
+  var depsString = "[" + (deps.length ? "'" + deps.join("', '") + "'" : "") + "]";
+  return Promise.resolve(
+    `System.registerDynamic('${nameInBundle || url}', ${depsString}, ${!!executingRequire}, `
+  + `function(require, exports, module) {\n${source}\n});\n`);
 }
