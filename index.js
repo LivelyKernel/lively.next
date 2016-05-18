aed90d143f3c15b13126fde237bf71781e5a2263
Robert Krahn <robert.krahn@gmail.com>
2016-05-18 20:04:10 +0200

    using node creation helpers

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import { acorn } from "./lib/acorn-extension.js";
 import stringify, { escodegen } from "./lib/stringify.js";
 export { escodegen, acorn }
 
+import * as nodes from "./lib/nodes.js";
 import * as query from "./lib/query.js";
 import * as transform from "./lib/transform.js";
 import * as capturing from "./lib/capturing.js";
 import * as comments from "./lib/comments.js";
 import * as categorizer from "./lib/code-categorizer.js";
-export { query, transform, capturing, comments, categorizer, stringify }
+export { query, transform, capturing, comments, categorizer, stringify, nodes }
