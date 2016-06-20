4902624925d9649378e16ccbf9b2d1ac1d0000f7
Robert Krahn <robert.krahn@gmail.com>
2016-06-19 19:42:57 -0700

    support for anonymous classes

--- tests/class-helper-test.js
+++ tests/class-helper-test.js
@@ -17,36 +17,35 @@ describe("create or extend classes", function() {
   });
 
   it("produces new class", function() {
-    var Foo = createOrExtend({}, null, "Foo", [
+    var Foo = createOrExtend("Foo", null, [
           {key: "m", value: function m() { return 23 }}
-        ]);
+        ], undefined, {});
     expect(Foo.name).equals("Foo");
     expect(new Foo().m()).equals(23);
   });
 
   it("is not stored in classHolder by default", function() {
     var classHolder = {}
-    var Foo = createOrExtend(classHolder, null, "Foo", [
+    var Foo = createOrExtend("Foo", null, [
           {key: "m", value: function m() { return 23 }}
-        ]);
+        ], undefined, classHolder);
     expect(Foo.name).equals("Foo");
     expect(classHolder).to.not.have.property("Foo");
   });
 
   it("is initialized with arguments from constructor call", function() {
-    var Foo = createOrExtend({}, null, "Foo", [
+    var Foo = createOrExtend("Foo", null, [
           {key: initializeSymbol, value: function(a, b) { this.x = a + b; }}
-        ]);
+        ], undefined, {});
     expect(new Foo(2,3).x).equals(5);
   });
 
   it("accepts getter and setter", function() {
-    var Foo = createOrExtend({}, null, "Foo",
-      [{
+    var Foo = createOrExtend("Foo", null, [{
         key: "x",
         get: function get() { return this._x },
         set: function set(v) { return this._x = v }
-      }]);
+      }], undefined, {});
     var foo = new Foo();
     foo.x = 23;
     expect(foo.x).equals(23);
@@ -54,18 +53,16 @@ describe("create or extend classes", function() {
   });
 
   it("same name does not mean same class", function() {
-    var Foo1 = createOrExtend({}, null, "Foo"),
-        Foo2 = createOrExtend({}, null, "Foo");
+    var Foo1 = createOrExtend("Foo", null, undefined, undefined, {}),
+        Foo2 = createOrExtend("Foo", null, undefined, undefined, {});
     expect(Foo1).to.not.equal(Foo2);
   });
 
   it("inherits", function() {
-    var Foo = createOrExtend({}, null, "Foo",
-          [{key: "m", value: function m() { return this.x + 23 }},
-           {key: "n", value: function n() { return 123 }}]),
-        Foo2 = createOrExtend({}, Foo, "Foo2",
-          [{key: "m", value: function m() {
-            return 2 + this.constructor[superclassSymbol].prototype.m.call(this); }}]),
+    var Foo = createOrExtend("Foo", null, [{key: "m", value: function m() { return this.x + 23 }},
+           {key: "n", value: function n() { return 123 }}], undefined, {}),
+        Foo2 = createOrExtend("Foo2", Foo, [{key: "m", value: function m() {
+            return 2 + this.constructor[superclassSymbol].prototype.m.call(this); }}], undefined, {}),
         foo = new Foo2();
     foo.x = 1;
     expect(foo.m()).equals(26);
@@ -74,26 +71,26 @@ describe("create or extend classes", function() {
   });
 
   it("super in initialize", function() {
-    var Foo = createOrExtend({}, null, "Foo", [{key: initializeSymbol, value: function(a, b) { this.x = a + b; }}]),
-        Foo2 = createOrExtend({}, Foo, "Foo2", [{key: initializeSymbol, value: function(a, b) { this.constructor[superclassSymbol].prototype[initializeSymbol].call(this,a, b); this.y = a; }}])
+    var Foo = createOrExtend("Foo", null, [{key: initializeSymbol, value: function(a, b) { this.x = a + b; }}], undefined, {}),
+        Foo2 = createOrExtend("Foo2", Foo, [{key: initializeSymbol, value: function(a, b) { this.constructor[superclassSymbol].prototype[initializeSymbol].call(this,a, b); this.y = a; }}], undefined, {})
     expect(new Foo2(2,3).x).equals(5);
     expect(new Foo2(2,3).y).equals(2);
   });
 
   it("modifying the superclass affects the subclass and its instances", function() {
-    var Foo = createOrExtend({}, null, "Foo"),
-        Foo2 = createOrExtend({}, Foo, "Foo2"),
+    var Foo = createOrExtend("Foo", null, undefined, undefined, {}),
+        Foo2 = createOrExtend("Foo2", Foo, undefined, undefined, {}),
         foo = new Foo2();
-    createOrExtend({Foo: Foo}, null, "Foo", [{key: "m", value: function m() { return 23 }}])
+    createOrExtend("Foo", null, [{key: "m", value: function m() { return 23 }}], undefined, {Foo: Foo})
     expect(foo.m()).equals(23);
   });
 
   it("changing the superclass will leave existing instances stale", function() {
-    var Foo = createOrExtend({}, null, "Foo", [{key: "m", value: function m() { return 23 }}]),
-        Foo2 = createOrExtend({}, Object, "Foo2"),
+    var Foo = createOrExtend("Foo", null, [{key: "m", value: function m() { return 23 }}], undefined, {}),
+        Foo2 = createOrExtend("Foo2", Object, undefined, undefined, {}),
         foo = new Foo2();
     expect(foo).to.not.have.property("m");
-    createOrExtend({Foo2: Foo2}, Foo, "Foo2");
+    createOrExtend("Foo2", Foo, undefined, undefined, {Foo2: Foo2});
     // Changing the superclass currently means changing the prototype, the
     // thing that instances have in  common with their class. When that's replaced
     // the instances are orphaned. That's not a feature but to change that we
@@ -103,4 +100,12 @@ describe("create or extend classes", function() {
     var anotherFoo = new Foo2();
     expect(anotherFoo).to.have.property("m");
   });
+  
+  it("works with anonymous classes", () => {
+    var X = createOrExtend(undefined, undefined, [{key: "m", value: function() { return 23; }}], undefined, {}),
+        Y = createOrExtend(undefined, X, [{key: "m", value: function() { return super.m() + 1; }}], undefined, {});
+    expect(new X().m()).equals(23);
+    expect(new Y().m()).equals(24);
+  });
+
 });
