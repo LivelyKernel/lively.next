a8209f961faa82e2ac685b91054678a2412c82c3
Christopher Schuster <cs@livoris.net>
2016-06-20 17:51:10 -0700

    added tests

--- tests/imports-exports-test.js
+++ tests/imports-exports-test.js
@@ -16,12 +16,13 @@ var dir = System.decanonicalize("lively.modules/tests/"),
       "file5.js": "export default async function foo() {}",
       "file6.js": "export function bar() {}",
       "file7.js": "export class Baz {}",
+      "file8.js": "var x = 1; var y = x;",
       "package.json": '{"name": "imports-exports-test-project", "main": "file1.js"}'
     };
 
 describe("imports and exports", () => {
 
-  var S, module1, module2, module3, module4, module5, module6, module7;
+  var S, module1, module2, module3, module4, module5, module6, module7, module8;
   beforeEach(() => {
     S = getSystem('import-export-test');
     module1 = module(S, testProjectDir + "file1.js"),
@@ -31,6 +32,7 @@ describe("imports and exports", () => {
     module5 = module(S, testProjectDir + "file5.js"),
     module6 = module(S, testProjectDir + "file6.js"),
     module7 = module(S, testProjectDir + "file7.js");
+    module8 = module(S, testProjectDir + "file8.js");
     return createFiles(testProjectDir, testProjectSpec)
   });
   afterEach(() => removeDir(testProjectDir));
@@ -82,6 +84,35 @@ describe("imports and exports", () => {
     var result = await module4.imports();
     expect(result).to.have.length(1);
     expect(result[0]).to.containSubset({fromModule: "./file2.js", imported: "*", local: "file2"});
+  });
+  
+  describe("resolve references", () => {
+    
+    it("within module", async () => {
+      const decls = await module8.declarationsForRefAt(19);
+      expect(decls).to.containSubset([{
+        start: 4,
+        end: 5,
+        name: "x",
+        module: {id: testProjectDir + "file1.js"}
+      }]);
+    });
+    
+    it("imported", async () => {
+      const decls = await module1.declarationsForRefAt(55);
+      expect(decls).to.containSubset([{
+        start: 5,
+        end: 6,
+        name: "yyy",
+        module: {id: testProjectDir + "file1.js"}
+      },{
+        start: 9,
+        end: 12,
+        name: "y",
+        module: {id: testProjectDir + "file2.js"}
+      }]);
+    });
+    
   });
 
 })
