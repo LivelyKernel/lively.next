4a696aef0e88ac3afa564656c8bff7cfbf116636
Robert Krahn <robert.krahn@gmail.com>
2016-06-26 09:22:11 -0700

    shell execution that works for both browser and node

---
+++ shell-exec.js
@@ -0,0 +1,23 @@
+import { exec as node_exec } from "child_process";
+
+export async function exec(cmdString, opts) {
+  opts = Object.assign({cwd: undefined, log: []}, opts)
+
+  opts.log.push(`$ ${cmdString}\n`);
+  
+  if (System.get("@system-env").node) {
+    var proc, e, o;
+    await new Promise((resolve, reject) =>
+      proc = node_exec(cmdString, {cwd: opts.cwd},
+      (err, out) => { e = err; o = out; resolve(); }));
+    var cmd = {
+      code: proc.exitCode,
+      output: (o || "") + (e ? "\n"+String(e) : "")};
+  } else {
+    var cmd = lively.shell.run(cmdString, {cwd: opts.cwd});
+    lively.bindings.connect(cmd, 'stdout', opts.log, 'push', {updater: ($upd, x) => $upd(x)});
+    lively.bindings.connect(cmd, 'stderr', opts.log, 'push', {updater: ($upd, x) => $upd(x)});
+  }
+
+  return cmd;
+}
