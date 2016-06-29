f2375ffe6140d6638483eced4758cdbbe53f7f1d
Robert Krahn <robert.krahn@gmail.com>
2016-06-28 18:01:17 -0700

    log output of commands

--- shell-exec.js
+++ shell-exec.js
@@ -6,20 +6,20 @@ export async function exec(cmdString, opts) {
   opts.log && opts.log.push(`$ ${cmdString}\n`);
   
   if (System.get("@system-env").node) {
-    var proc, e, o;
+    var proc, e, stdout, stderr;
     await new Promise((resolve, reject) =>
       proc = node_exec(cmdString, {cwd: opts.cwd},
-      (err, out) => { e = err; o = out; resolve(); }));
+      (err, _stdout, _stderr) => { e = err; stdout = _stdout, stderr = _stderr; resolve(); }));
     var cmd = {
       code: proc.exitCode,
-      output: (o || "") + (e ? "\n"+String(e) : "")};
+      output: (e ? "\n"+String(e) : "") + stdout + "\n" + stderr};
+    opts.log.push(cmd.output);
   } else {
     var cmd = lively.shell.run(cmdString, {cwd: opts.cwd});
     if (opts.log) {
-      lively.bindings.connect(cmd, 'stdout', opts.log, 'push', {updater: ($upd, x) => $upd(x)});
-      lively.bindings.connect(cmd, 'stderr', opts.log, 'push', {updater: ($upd, x) => $upd(x)});
+      lively.bindings.connect(cmd, 'stdout', opts.log, 'push', {updater: function ($upd, x) { return $upd(this.targetObj, x); }});
+      lively.bindings.connect(cmd, 'stderr', opts.log, 'push', {updater: function ($upd, x) { return $upd(this.targetObj, x); }});
     }
   }
-
   return cmd;
 }
