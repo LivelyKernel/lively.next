b8fbadeac9f9b561b1e5213aa3d8fda318b0c124
Robert Krahn <robert.krahn@gmail.com>
2016-07-03 03:47:30 -0700

    fixing example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,14 +1,27 @@
+import { num, arr } from "lively.lang";
 import { pt, Color, Point } from "lively.graphics";
 import { Renderer } from "lively.morphic/renderer.js";
-import { Morph } from "lively.morphic/morph.js";
+import { Morph, WorldMorph } from "lively.morphic/morph.js";
+import { EventDispatcher } from "lively.morphic/events.js";
 
-var {scrollWidth: w, scrollHeight: h} = document.body,
-    world = new Morph({extent: pt(w, h)}),
-    m1 = new Morph({position: pt(20,20), extent: pt(200,200), fill: Color.red}),
-    m2 = new Morph({position: pt(34,20), extent: pt(50,100), fill: Color.green})
+var {world, renderer, eventHandler} = setupWorld();
+var eventDispatcher = new EventDispatcher(window, world);
+eventDispatcher.install();
 
-world.addMorph(m1)
-m1.addMorph(m2)
+world.eventDispatcher = eventDispatcher;
 
-var r = new Renderer(world, document.getElementById("lively-world"))
-r.renderWorld();
+window.$world = world;
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+function setupWorld() {
+  var world = new WorldMorph({
+    name: "world", extent: pt(300,300), submorphs: [
+      {name: "morph 1", position: pt(20,20), extent: pt(200,200), fill: Color.red, submorphs: [
+        {name: "morph 2", position: pt(34,20), extent: pt(50,100), fill: Color.green}]}]})
+  
+  var renderer = new Renderer(world, document.getElementById("lively-world"));
+  renderer.startRenderWorldLoop();
+
+  return {world, renderer}
+}
