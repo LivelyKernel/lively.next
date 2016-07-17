9f984efbf003a68c88aee6b5a6c35910c70d4236
Robert Krahn <robert.krahn@gmail.com>
2016-07-17 14:31:39 -0700

    adding object drawer to example page

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,6 +1,7 @@
 import { num, arr, installGlobals } from "lively.lang";
 import { pt, Color, Point } from "lively.graphics";
 import { EventDispatcher, Renderer, Morph, World } from "lively.morphic";
+import { ObjectDrawer } from "lively.morphic/widgets.js";
 
 installGlobals();
 
@@ -14,11 +15,8 @@ window.$world = world;
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 function setupWorld() {
-  var world = new World({
-    name: "world", extent: pt(window.innerWidth,window.innerHeight), submorphs: [
-      {name: "icon", position: pt(0, 0), extent: pt(40,40), styleClasses: ["fa", "fa-search"]},
-      {name: "morph 1", position: pt(20,20), origin: pt(0, 0), rotation: 0, extent: pt(200,200), fill: Color.blue, submorphs: [
-        {name: "morph 2", position: pt(34,20), extent: pt(50,100), fill: Color.green}]}]})
+  var world = new World({name: "world", extent: pt(window.innerWidth,window.innerHeight)})
+  world.addMorph(new ObjectDrawer());
 
   var renderer = new Renderer(world, document.getElementById("lively-world"));
   renderer.startRenderWorldLoop();
