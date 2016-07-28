2f0bfce1a8a0f7cc85bdfbb574079eb9fb61aefe
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2016-07-27 17:30:32 -0700

    FontMetric.indexFromPoint() bug fixes

--- rendering/font-metric.js
+++ rendering/font-metric.js
@@ -97,6 +97,45 @@ export default class FontMetric {
     return { height: height, width: width };
   }
 
+  indexFromPoint(fontFamily, fontSize, str, point) {
+    var sizeMap = this.sizeListForStr(fontFamily, fontSize, str),
+        { x, y } = point,
+        line = sizeMap.find(l => {
+          let char = l[0],
+              start = char.position.y,
+              end = start + char.height;
+          return y >= start && y <= end;
+        });
+    if (!line) return str.length;
+
+    let char = line.find(c => {
+      let start = c.position.x,
+          end = start + c.width;
+      return x >= start && x <= end;
+    });
+    return char ? char.index : line[line.length-1].index;
+  }
+
+  sizeListForStr(fontFamily, fontSize, str) {
+    var sizeList = [], totalHeight = 0, index = 0;
+    for (let line of str.split('\n')) {
+      let lineSizeList = [],
+          lineHeight = this.sizeFor(fontFamily, fontSize, " ").height,
+          lineWidth = 0;
+      for (let char of line.split('')) {
+        let { height: charHeight, width: charWidth } = this.sizeFor(fontFamily, fontSize, char);
+        if (charHeight > lineHeight) lineHeight = charHeight;
+        lineSizeList.push({ index: index++, position: pt(lineWidth, totalHeight), width: charWidth }) // note: height will be set later
+        lineWidth += charWidth;
+      }
+      lineSizeList.push({ index: index++, position: pt(lineWidth, totalHeight), width: 0}); // newline
+      lineSizeList.map(c => c.height = lineHeight);
+      sizeList.push(lineSizeList);
+      totalHeight += lineHeight || this.sizeFor(fontFamily, fontSize, " ").height;
+    }
+    return sizeList;
+  }
+
   asciiSizes(fontFamily, fontSize) {
     var result = {};
     for (var i = 32; i <= 126; i++) {
