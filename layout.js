aeb08ff6c70c7f5aa95a15a07c062f64f8e05421
Robin Schreiber <robin.schreiber@me.com>
2016-08-01 18:58:31 +0200

    add tiling layout

--- layout.js
+++ layout.js
@@ -1,7 +1,8 @@
 import { pt } from "lively.graphics";
 
 class Layout {
-  constructor({spacing} = {}) {
+  constructor({spacing, border} = {}) {
+    this.border = {top: 0, left: 0, right: 0, bottom: 0, ...border};
     this.spacing = spacing || 0;
     this.active = false;
   }
@@ -66,7 +67,48 @@ export class GridLayout extends Layout {
 export class TilingLayout extends Layout {
   
   applyTo(morph) {
+    var width = this.getOptimalWidth(morph, morph.submorphs),
+        currentRowHeight = 0,
+        currentRowWidth = 0,
+        spacing = this.spacing,
+        previousRowHeight = spacing,
+        i = 0, rowSwitch = true;
+        
+    if (this.active) return;
+    this.active = true;
+
+    while (i < morph.submorphs.length) {
+        var submorphExtent = morph.submorphs[i].extent;
+        if (rowSwitch || currentRowWidth + submorphExtent.x + 2*spacing <= width) {
+            rowSwitch = false;
+            morph.submorphs[i].position = pt(currentRowWidth + spacing, previousRowHeight);
+            currentRowHeight = Math.max(currentRowHeight, submorphExtent.y);
+            currentRowWidth += spacing + submorphExtent.x;
+            i++;
+        } else {
+            previousRowHeight += spacing + currentRowHeight;
+            currentRowWidth = currentRowHeight = 0;
+            rowSwitch = true;
+        }
+    }
     
+    this.active = false;
+  }
+  
+  getMinWidth(container, submorphs) {
+      return submorphs.reduce((s, e) => (e.extent.x > s) ? e.extent.x : s, 0) +
+          this.border.left + this.border.right;
+  }
+  
+  getMinHeight(container, submorphs) {
+      return submorphs.reduce((s, e) => (e.extent.y > s) ? e.extent.y : s, 0) +
+          this.border.top + this.border.bottom;
+  }
+  
+  getOptimalWidth(container, submorphs) {
+      var width = container.width,
+          maxSubmorphWidth = this.getMinWidth(container, submorphs);
+      return Math.max(width, maxSubmorphWidth);
   }
   
 }
