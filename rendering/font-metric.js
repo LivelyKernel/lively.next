e87308a422e620fb77eacdb1b3c4d51c6e9b9174
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2016-08-26 13:43:56 -0700

    fontMetric cleanup: remove sizeForStr, kerningFor, ligatureAdjustmentFor

--- rendering/font-metric.js
+++ rendering/font-metric.js
@@ -34,7 +34,6 @@ export default class FontMetric {
 
   constructor() {
     this.charMap = [];
-    this.kerningMap = [];
     this.element = null;
   }
 
@@ -101,8 +100,7 @@ export default class FontMetric {
   }
 
   sizeFor(fontFamily, fontSize, char) {
-    if (char.length > 1)
-      return this.sizeForStr(fontFamily, fontSize, false, char);
+    if (char.length > 1) return this.measure(fontFamily, fontSize, char);
 
     if (!this.charMap[fontFamily]) {
       this.charMap[fontFamily] = [];
@@ -113,77 +111,6 @@ export default class FontMetric {
     if (!this.charMap[fontFamily][fontSize][char])
       this.charMap[fontFamily][fontSize][char] = this.measure(fontFamily, fontSize, char);
     return this.charMap[fontFamily][fontSize][char];
-  }
-
-  sizeForStr(fontFamily, fontSize, fontKerning, str) {
-    var height = 0, width = 0,
-        defaultLineHeight = this.defaultLineHeight(fontFamily, fontSize);
-    for (let line of str.split('\n')) {
-      let lineHeight = defaultLineHeight, lineWidth = 0,
-          chars = line.split(''), nChars = chars.length;
-      for (let charIndex = 0; charIndex < nChars; charIndex++) {
-        let char = chars[charIndex],
-          { height: charHeight, width: charWidth } = this.sizeFor(fontFamily, fontSize, char);
-        if (charHeight > lineHeight) lineHeight = charHeight;
-        if (fontKerning) {
-          let nextChar = chars[charIndex+1],
-              prevChar = chars[charIndex-1],
-              kerning  = this.kerningFor(fontFamily, fontSize, ...chars.slice(0, charIndex+2)),
-              ligatureOffset = 0;
-          if (charIndex % 2 === 0) {
-            let prevChar = chars[charIndex-1];
-            ligatureOffset = this.ligatureAdjustmentFor(fontFamily, fontSize, prevChar, char, nextChar);
-          }
-          charWidth += kerning + ligatureOffset;
-        }
-        lineWidth += charWidth;
-      }
-      if (lineWidth > width) width = lineWidth;
-      height += lineHeight;
-    }
-    return { height: height, width: width };
-  }
-
-  // FIXME? do browsers implement contextual kerning?
-  kerningFor(fontFamily, fontSize, ...chars) {
-    var left = chars.slice(0, -1).join(''),
-        right = chars.slice(-1).join(''),
-        measureStr = `${left}${right}`,
-        indexStr = `_${measureStr}`;
-    if (measureStr.length < 2 || string.lines(measureStr).length !== 1) return 0;
-    if (!this.kerningMap[fontFamily]) {
-      this.kerningMap[fontFamily] = [];
-    }
-    if (!this.kerningMap[fontFamily][fontSize]) {
-      this.kerningMap[fontFamily][fontSize] = [];
-    }
-    if (this.kerningMap[fontFamily][fontSize][indexStr] === undefined) {
-      let leftWidth = this.measure(fontFamily, fontSize, left).width,
-          rightWidth = this.sizeFor(fontFamily, fontSize, right).width,
-          totalWidth = this.measure(fontFamily, fontSize, measureStr).width;
-      this.kerningMap[fontFamily][fontSize][indexStr] = totalWidth - leftWidth - rightWidth;
-    }
-    return this.kerningMap[fontFamily][fontSize][indexStr];
-  }
-
-  ligatureAdjustmentFor() { return 0 }
-
-  _ligatureAdjustmentFor(fontFamily, fontSize, pre, anchor, next) {
-    var measureStr = `${pre}${anchor}${next}`,
-        indexStr = `_${measureStr}`;
-    if (measureStr.length !== 3 || string.lines(measureStr).length !== 1) return 0;
-    if (!this.kerningMap[fontFamily]) {
-      this.kerningMap[fontFamily] = [];
-    }
-    if (!this.kerningMap[fontFamily][fontSize]) {
-      this.kerningMap[fontFamily][fontSize] = [];
-    }
-    if (this.kerningMap[fontFamily][fontSize][indexStr] === undefined) {
-      let unadjustedWidth = this.sizeForStr(fontFamily, fontSize, true, measureStr).width,
-          measuredWidth = this.measure(fontFamily, fontSize, measureStr).width;
-      this.kerningMap[fontFamily][fontSize][indexStr] = measuredWidth - unadjustedWidth;
-    }
-    return this.kerningMap[fontFamily][fontSize][indexStr];
   }
 
   asciiSizes(fontFamily, fontSize) {
--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -2,12 +2,6 @@ import { arr, string } from "lively.lang"
 
 export var dummyFontMetric = {
   height: 14, width: 6,
-  sizeForStr(fontFamily, fontSize, fontKerning, text) {
-    // ea char 10*10
-    var lines = string.lines(text),
-        maxCols = arr.max(lines, line => line.length).length;
-    return {width: maxCols*this.width, height: lines.length*this.height}
-  },
   sizeFor(fontFamily, fontSize, text) {
     return {width: this.width, height: this.height}
   },
