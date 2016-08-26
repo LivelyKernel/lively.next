d93c15d31e0a9d1b5c62449b9b38c275a6071dac
Tyler Smith <tylervernonsmith@users.noreply.github.com>
2016-08-25 18:51:01 -0700

    text test fixes

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -11,6 +11,16 @@ export var dummyFontMetric = {
   sizeFor(fontFamily, fontSize, text) {
     return {width: this.width, height: this.height}
   },
+  charBoundsForStr(fontFamily, fontSize, fontKerning, text) {
+    var prevX = 0;
+    return text.split('').map(function (char, col) {
+      let x = prevX,
+          { width, height } = this;
+      if (col === text.length - 1) width = 0;
+      prevX += width;
+      return { x, y: 0, width, height };
+    }, this);
+  },
   kerningFor: () => 0,
   ligatureAdjustmentFor: () => 0
 }
