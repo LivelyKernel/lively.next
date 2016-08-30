0f63ba9de8e374fda149964a26466ed8aa60c90f
Robert Krahn <robert.krahn@gmail.com>
2016-08-29 17:06:31 -0700

    text undo tests

---
+++ tests/text/undo-test.js
@@ -0,0 +1,33 @@
+/*global System, declare, it, xit, describe, xdescribe, beforeEach, afterEach, before, after*/
+import { Text } from "../../text/morph.js";
+import { Range } from "../../text/selection.js";
+import { expect } from "mocha-es6";
+import { dummyFontMetric as fontMetric } from "../test-helpers.js";
+
+function range(startRow, startCol, endRow, endCol) {
+  return {start: {row: startRow, column: startCol}, end: {row: endRow, column: endCol}}
+}
+
+var text;
+
+describe("undo", () => {
+
+  beforeEach(() => text = new Text({textString: "hello\nworld", fontMetric}));
+
+  it("can undo simple insert", () => {
+    text.insertText("foo", {row: 1, column: 0});
+    expect(text.textString).equals("hello\nfooworld");
+    text.textUndo();
+    expect(text.textString).equals("hello\nworld");
+    expect(text.selection).stringEquals("Selection(1/0 -> 1/0)");
+  });
+
+  xit("can undo and redo simple insert", () => {
+    text.insertText("foo", {row: 1, column: 0});
+    text.textUndo();
+    text.textRedo();
+    expect(text.textString).equals("hello\nfooworld");
+    expect(text.selection).stringEquals("Selection(1/0 -> 1/3)");
+  });
+
+});
