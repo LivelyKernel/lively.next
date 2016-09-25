b9907d230ef4c4a3e324a5a35197dad757b81f09
Robert Krahn <robert.krahn@gmail.com>
2016-09-25 16:56:14 -0700

    delay MorphAfterRenderHook for smoother update

--- rendering/morphic-default.js
+++ rendering/morphic-default.js
@@ -248,17 +248,19 @@ function MorphAfterRenderHook(morph, renderer) { this.morph = morph; this.render
 MorphAfterRenderHook.prototype.hook = function(node, propertyName, previousValue) {
   // 1. wait for node to be really rendered, i.e. it's in DOM
   // this.morph._dirty = false;
-  promise.waitFor(200, () => !!node.parentNode).catch(err => false).then(isInDOM => {
-    if (isInDOM) {
-      // 2. update scroll of morph itself
-      if (this.morph.isClip()) this.updateScroll(this.morph, node);
-      // 3. Update scroll of DOM nodes of submorphs
-      if (this.morph._submorphOrderChanged && this.morph.submorphs.length) {
-        this.morph._submorphOrderChanged = false;
-        this.updateScrollOfSubmorphs(this.morph, this.renderer);
+  requestAnimationFrame(() => {
+    promise.waitFor(400, () => !!node.parentNode).catch(err => false).then(isInDOM => {
+      if (isInDOM) {
+        // 2. update scroll of morph itself
+        if (this.morph.isClip()) this.updateScroll(this.morph, node);
+        // 3. Update scroll of DOM nodes of submorphs
+        if (this.morph._submorphOrderChanged && this.morph.submorphs.length) {
+          this.morph._submorphOrderChanged = false;
+          this.updateScrollOfSubmorphs(this.morph, this.renderer);
+        }
       }
-    }
-    this.morph._rendering = false; // see morph.makeDirty();
+      this.morph._rendering = false; // see morph.makeDirty();
+    });
   });
 }
 MorphAfterRenderHook.prototype.updateScroll = function(morph, node) {
