8ee73d5e5243a4be2bb4ba2448c9ddc69f17966e
Robert Krahn <robert.krahn@gmail.com>
2016-09-27 20:20:00 -0700

    text highlighting: no unecessary style copies

--- ide/code-editor.js
+++ ide/code-editor.js
@@ -62,16 +62,16 @@ export default class CodeEditor extends Morph {
     });
     connect(this, "extent", this.submorphs[0], "extent");
   }
-  
+
   highlight() {
     if (!this.theme) return;
     let textMorph = this.submorphs[0],
         tokens = this.mode.highlight(textMorph.textString),
         defaultStyle = this.submorphs[0].styleProps;
     let textAttributes = tokens.map(({token, from, to}) =>
-          TextAttribute.fromPositions({...defaultStyle, ...this.theme.styleCached(token)}, from, to));
+          TextAttribute.fromPositions(this.theme.styleCached(token), from, to));
     textAttributes.unshift(TextAttribute.create(defaultStyle, 0, -1, textMorph.documentEndPosition.row+1, 0));
-    textMorph.setTextAttributesSorted(textAttributes);
+    textMorph.setSortedTextAttributes(textAttributes);
 
     if (this._checker)
       this._checker.onDocumentChange({}, this);
