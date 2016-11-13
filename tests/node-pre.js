dc47cfef2f472e41608270a1de86facd3a39a1f5
Robert Krahn <robert.krahn@gmail.com>
2016-11-13 15:52:41 -0800

    travis debug....

--- tests/node-pre.js
+++ tests/node-pre.js
@@ -2,10 +2,20 @@
 // with all the sub-sub-sub packages
 
 function requireFromPackage(moduleName, pkgName) {
-var path = require("path"),
-    module = require("module"),
-    lookupPaths = [path.join(require.resolve(pkgName), "../node_modules")],
-    id = module._resolveFilename(moduleName, {paths: lookupPaths});
+  var path = require("path"),
+      module = require("module");
+  try {
+    var lookupPaths = [path.join(require.resolve(pkgName), "../node_modules")];
+  } catch (e) {
+    console.error(`[requireFromPackage] failure resolving ${pkgName}: ${e.stack || e}`);
+    return;
+  }
+  try {
+    var id = module._resolveFilename(moduleName, {paths: lookupPaths});
+  } catch (e) {
+    console.error(`[requireFromPackage] failure resolving ${moduleName} from ${lookupPaths[0]}: ${e.stack || e}`);
+    return;
+  }
   return require(id);
 }
 
