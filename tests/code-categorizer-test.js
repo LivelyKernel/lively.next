3eb67645168ded66ad4e70d544b1fdaa937209af
Robert Krahn <robert.krahn@gmail.com>
2016-11-21 02:53:02 -0800

    better code categorizer

--- tests/code-categorizer-test.js
+++ tests/code-categorizer-test.js
@@ -23,23 +23,29 @@ describe('code categorizer', function() {
       ]);
     });
 
-    it("test02FindFuncDecls", function() {
+    it("finds exported decls", function() {
+      var result = categorize("export var x = 23,\n    y = 24");
+      expect(result.decls).to.deep.equal([
+        {name: "x", type: "var-decl", node: result.ast.body[0].declaration.declarations[0]},
+        {name: "y", type: "var-decl", node: result.ast.body[0].declaration.declarations[1]}
+      ]);
+    });
+
+    it("find func decls", function() {
       var result = categorize("3 + 4;\nfunction test() { return 23; }");
       expect(result.decls).to.deep.equal([
         {name: "test", type: "function-decl", node: result.ast.body[1]},
       ]);
     });
 
-    it("test03FindDeclsInsideModule", function() {
-      var result = categorize("module('foo.bar').requires().toRun(function() {\nfunction test() { return 23; }\n});");
-      expect(result.decls).to.deep.equal([{
-        name: "test", type: "function-decl",
-        node: result.ast.body[0].expression.arguments[0].body.body[0],
-        parent: {name: "foo.bar", node: result.ast.body[0]}
-      }]);
+    it("find export default func decls", function() {
+      var result = categorize("export default function test() { return 23; }");
+      expect(result.decls).to.deep.equal([
+        {name: "test", type: "function-decl", node: result.ast.body[0].declaration},
+      ]);
     });
 
-    it("test04FindDeclsInsideFunctionWrapper", function() {
+    it("find decls inside function wrapper", function() {
       var result = categorize("(function() {\nfunction test() { return 23; }\n})();");
       expect(result.decls).to.deep.equal([{
         name: "test", type: "function-decl", node: result.ast.body[0].expression.callee.body.body[0],
@@ -47,38 +53,27 @@ describe('code categorizer', function() {
       }]);
     });
 
-    it("test05FindMethodAndPropertiessOfClass", function() {
-      var result = categorize("Object.subclass('Foo', {foo: function() { return 23; }, bar: 24});"),
-          expected = [
-            {name: "Foo", type: "lively-class-definition", node: result.ast.body[0]},
-            {name: "foo", type: "lively-class-instance-method", node: result.ast.body[0].expression.arguments[1].properties[0], get parent() { return expected[0]; }},
-            {name: "bar", type: "lively-class-instance-property", node: result.ast.body[0].expression.arguments[1].properties[1], get parent() { return expected[0]; }}];
-      expect(result.decls).to.deep.equal(expected, "1");
-
-      var result = categorize("Object.subclass('Foo', 'test', {foo: function() { return 23; }, bar: 24}, 'baz');"),
-          expected = [
-            {name: "Foo", type: "lively-class-definition", node: result.ast.body[0]},
-            {name: "foo", type: "lively-class-instance-method", node: result.ast.body[0].expression.arguments[2].properties[0], get parent() { return expected[0]; }},
-            {name: "bar", type: "lively-class-instance-property", node: result.ast.body[0].expression.arguments[2].properties[1], get parent() { return expected[0]; }}];
-      expect(result.decls).to.deep.equal(expected, "2mm");
-    }),
-
-    it("test06FindMethodAndPropertiessOfObject", function() {
+    it("find method and properties of object", function() {
       // lively.ast.printAst("var x = {foo: function() { return 23; }, bar: 24};")
       var result = categorize("var x = {foo: function() { return 23; }, bar: 24};"),
           expected = [
-            {name: "x", type: "var-decl", node: result.ast.body[0].declarations[0]},
+            {name: "x", type: "object-decl", node: result.ast.body[0].declarations[0]},
             {name: "foo", type: "object-method", node: result.ast.body[0].declarations[0].init.properties[0], get parent() { return expected[0]; }},
             {name: "bar", type: "object-property", node: result.ast.body[0].declarations[0].init.properties[1], get parent() { return expected[0]; }}];
-      expect(result.decls).to.deep.equal(expected, result.decls);
+      expect(result.decls).containSubset(expected);
     });
 
     it("finds classes", function() {
-      var result = categorize("class Foo {}"),
+      var result = categorize("class Foo {constructor(xxx) {} foo() {} static bar() {} get zork() {}}"),
           expected = [
-            {name: "Foo", type: "class", node: result.ast.body[0]}
+            {name: "Foo", type: "class-decl", node: result.ast.body[0]},
+            {name: "constructor", type: "class-constructor", node: result.ast.body[0].body.body[0]},
+            {name: "foo", type: "class-instance-method", node: result.ast.body[0].body.body[1]},
+            {name: "bar", type: "class-class-method", node: result.ast.body[0].body.body[2]},
+            {name: "zork", type: "class-instance-getter", node: result.ast.body[0].body.body[3]}
           ];
-      expect(result.decls).to.deep.equal(expected, result.decls);
+
+      expect(result.decls).containSubset(expected, result.decls);
     });
 
   });
