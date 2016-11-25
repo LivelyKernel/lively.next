279fdedd91555bdf1a81aa554b0a2f3e3ba97e5e
Robert Krahn <robert.krahn@gmail.com>
2016-11-25 03:39:46 -0800

    truncateLeft

--- dist/lively.lang.dev.js
+++ dist/lively.lang.dev.js
@@ -4020,6 +4020,11 @@
             truncation = truncation === undefined ? '...' : truncation;
             return s.length > length ? s.slice(0, length - truncation.length) + truncation : String(s);
         },
+        truncateLeft: function (s, length, truncation) {
+            length = length || 30;
+            truncation = truncation === undefined ? '...' : truncation;
+            return s.length > length ? truncation + s.slice(-length) : String(s);
+        },
         regExpEscape: function (s) {
             return s.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').replace(/\x08/g, '\\x08');
         },
