9563be54a3bf1e772b3005ffa4e180a69adde1b3
Robert Krahn <robert.krahn@gmail.com>
2016-11-29 16:50:28 -0800

    fix dir to jsdav for usage of server with different baseDir

--- plugins/dav.js
+++ plugins/dav.js
@@ -2,9 +2,11 @@
 
 import { obj } from "lively.lang";
 
-var DavHandler = System._nodeRequire('jsDAV/lib/DAV/handler'),
-    FsTree = System._nodeRequire('jsDAV/lib/DAV/backends/fs/tree'),
-    defaultPlugins = System._nodeRequire("jsDAV/lib/DAV/server").DEFAULT_PLUGINS;
+// FIXME...
+var serverDir = System.decanonicalize("lively.server/").replace("file://"),
+    DavHandler = System._nodeRequire(`${serverDir}/node_modules/jsDAV/lib/DAV/handler`),
+    FsTree = System._nodeRequire(`${serverDir}/node_modules/jsDAV/lib/DAV/backends/fs/tree`),
+    defaultPlugins = System._nodeRequire(`${serverDir}/node_modules/jsDAV/lib/DAV/server`).DEFAULT_PLUGINS;
 
 
 export default class LivelyDAVPlugin {
