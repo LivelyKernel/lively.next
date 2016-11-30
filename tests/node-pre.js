9a95d61254ffeac9e14c2cb2b6a18c7a8cf60cc9
Robert Krahn <robert.krahn@gmail.com>
2016-11-29 18:56:46 -0800

    map node_modules to debug travis

--- tests/node-pre.js
+++ tests/node-pre.js
@@ -21,3 +21,54 @@ function requireFromPackage(moduleName, pkgName) {
 
 requireFromPackage("socket.io", "lively.server");
 requireFromPackage("socket.io-client", "lively.2lively");
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+var fs = require("fs");
+var path = require("path");
+var j = path.join
+
+var ignore = [
+  ".bin", "babel-preset-es2015-rollup", "rollup-plugin-babel", "rollup", "recast",
+  'repeating',
+  'private',
+  'minimatch',
+  'path-is-absolute',
+  'mkdirp',
+  'once',
+  'esprima-fb',
+  'glob',
+  'lodash',
+  'regenerate',
+  'minimist',
+  'jsesc' 
+  ];
+
+function recursively_map_node_modules(dir, maxDepth = 10, depth = 0) {
+  // copy so that only dependent modules get to know their uppers
+
+  if (depth > maxDepth) return;
+
+  var subDirs = map_node_modules(dir);
+  subDirs
+    .filter(ea => !fs.lstatSync(ea).isSymbolicLink())
+    .forEach(ea => recursively_map_node_modules(ea, maxDepth, depth + 1));
+}
+
+function map_node_modules(dir, knownPackages) {
+  var confFile = j(dir, "package.json"),
+      node_modulesDir = j(dir, "node_modules");
+  if (!fs.existsSync(node_modulesDir)) return [];
+  var node_modulesPackages = fs.readdirSync(node_modulesDir).filter(ea => ignore.indexOf(ea) === -1)
+  console.log(`In ${node_modulesDir}:\n  ${node_modulesPackages.join("\n  ")}`);  
+  return node_modulesPackages.map(ea => j(node_modulesDir, ea))
+}
+
+// module.exports = recursively_map_node_modules;
+
+
+var dir = __dirname + "/..";
+
+recursively_map_node_modules(dir)
