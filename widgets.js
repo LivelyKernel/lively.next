a1960369dd85e5c87c58427b0945128b0f9c9feb
Robin Schreiber <robin.schreiber@me.com>
2016-11-30 14:56:58 +0100

    widgets: add mode selector widget

--- widgets.js
+++ widgets.js
@@ -1,6 +1,6 @@
-import { obj, num } from "lively.lang";
+import { obj, num, arr } from "lively.lang";
 import { pt, Color, Rectangle } from "lively.graphics";
-import { Morph, Button, List, Text } from "./index.js";
+import { Morph, Button, List, Text, GridLayout } from "./index.js";
 import { Icon } from "./icons.js";
 import { signal, connect } from "lively.bindings";
 import { Tooltip } from "./tooltips.js";
@@ -112,4 +112,84 @@ export class CheckBox extends Morph {
     return renderer.renderCheckBox(this);
   }
 
+}
+
+export class ModeSelector extends Morph {
+
+   constructor(props) {
+     var {items, initLabel, tooltips = {}} = props, keys, values;
+     if (obj.isArray(items)) {
+         keys = values = items;
+     } else {
+         keys = obj.keys(items);
+         values = obj.values(items);
+     }
+     super({
+         keys, values, tooltips,
+         morphClasses: ['root'],
+         layout: new GridLayout({
+             grid: [[null, ...arr.interpose(keys.map(k => k + "Label"), null), null]],
+             autoAssign: false, fitToCell: false, 
+         }),
+         ...props
+      })
+      this.build();
+      this.switchLabel(initLabel ? initLabel : keys[0]);
+      connect(this, "extent", this, "relayout");
+    }
+
+    build() {
+       this.submorphs = [
+            {name: "typeMarker"},
+            ...this.createLabels(this.keys, this.values, this.tooltips)
+         ]
+       this.applyStyler();
+    }
+
+    applyStyler() {
+       this.withAllSubmorphsDo(m => {
+         var styleProps;
+         if (styleProps = this.styler[m.name]) {
+            Object.assign(m, styleProps);
+         } else if (m.morphClasses) {
+            styleProps = obj.merge(arr.compact(m.morphClasses.map(c => this.styler[c])));
+            Object.assign(m, styleProps);
+         }
+      })
+    }
+
+    get styler() {
+       return {
+          root: {fill: Color.transparent, 
+                 height: 30, origin: pt(0,5)},
+          typeMarker: {fill: Color.gray.darker(), borderRadius: 3},
+          label: {fontWeight: 'bold', nativeCursor: "pointer", padding: 4}
+       }
+    }
+
+    createLabels(keys, values, tooltips = {}) {
+       return arr.zip(keys, values)
+                 .map(([name, value]) => {
+                   const tooltip = tooltips[name];
+                   return {
+                      name: name + "Label", morphClasses: ['label'],
+                      type: "label", value: name, 
+                      ...tooltip && {tooltip},
+                      onMouseDown: () => {
+                         this.switchLabel(name, value);
+                }}});
+    }
+
+    async relayout() { 
+        return this.currentLabel && this.get("typeMarker").animate({bounds: this.currentLabel.bounds(), duration: 200}); 
+    }
+    
+    async switchLabel(label, value) {
+       const newLabel = this.get(label + "Label"), duration = 200;
+       newLabel.animate({fontColor: Color.white, duration});
+       this.currentLabel && this.currentLabel.animate({fontColor: Color.black, duration});
+       this.currentLabel = newLabel;
+       await this.relayout();
+       signal(this, label, value)
+    }
 }
