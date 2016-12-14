fd680aac1e042708ad666021ae9c24f0a328454b
Robert Krahn <robert.krahn@gmail.com>
2016-12-14 02:07:34 -0800

    proxying via /proxy and /proxy-ssl path (in addition to x-lively-proxy-request header)

--- plugins/proxy.js
+++ plugins/proxy.js
@@ -20,7 +20,8 @@ var content = await res.text(); content.slice(0,200)
 // var server = LivelyServer.servers.get("0.0.0.0:9011")
 // var p = server.findPlugin("proxy")
 
-var redirectRegex = /^201|30(1|2|7|8)$/;
+var proxyURLRe = /^\/proxy(-ssl)?\/([^\/]+)/,
+    redirectRe = /^201|30(1|2|7|8)$/;
 
 export default class ProxyPlugin {
 
@@ -34,33 +35,51 @@ export default class ProxyPlugin {
 
   setup(livelyServer) {}
   async close() {}
-  
-  handleRequest(req, res, next) {
-    var proxyHeader = req.headers["x-lively-proxy-request"];
-    if (!proxyHeader) return next();
 
-    delete req.headers["x-lively-proxy-request"];
+  handleRequest(req, res, next) {
+    var proxyHeader = req.headers["x-lively-proxy-request"],
+        proxyMatch = !proxyHeader && req.url.match(proxyURLRe);
+
+    if (!proxyHeader && !proxyMatch) return next();
+
+    var opts;
+
+    if (proxyHeader) {
+      delete req.headers["x-lively-proxy-request"];
+
+      opts = {
+        target: proxyHeader,
+        ignorePath: true, // don't use path from req.url
+        changeOrigin: true,
+        autoRewrite: true // doesn't seem to work???
+      }
+
+
+    } else {
+      var [prefix, ssl, domain] = proxyMatch,
+          target = `http${ssl ? "s" : ""}://${domain}`;
+
+      opts = {
+        target,
+        ignorePath: true, // don't use path from req.url
+        changeOrigin: true,
+        pathRewrite: {['^' + prefix] : ''},
+        // autoRewrite: true
+      }
+    }
 
     var proxy = createProxy({});
 
-    // for custom responses on "proxyRes" see https://github.com/nodejitsu/node-http-proxy/pull/850
+    // for custom responses on "proxyRes" see
+    // https://github.com/nodejitsu/node-http-proxy/pull/850
 
     proxy.on('proxyRes', function (proxyRes, req, res) {
       var {statusMessage, statusCode, headers} = proxyRes;
-      console.log(req.url)
       console.log(`[lively.server proxy] <proxy response>`)
       console.log(`=> ${proxyRes.req.path} ${JSON.stringify(proxyRes.req._headers, true, 2)}`)
       console.log(`<= ${statusCode}/${statusMessage} ${JSON.stringify(headers, true, 2)}`);
       console.log(`[lively.server proxy] </proxy response>`)
     });
-
-    var opts = {
-      target: proxyHeader,
-      ignorePath: true, // don't use path from req.url
-      changeOrigin: true,
-      //   pathRewrite: {'^/api' : ''}      // <-- this will remove the /api prefix
-      autoRewrite: true // doesn't seem to work???
-    }
 
     console.log(`[lively.server proxy] Proxying request to ${opts.target}`);
 
