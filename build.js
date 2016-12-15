a4f9f047e899cc818228abe27dd0a2ee21e8e469
Robert Krahn <robert.krahn@gmail.com>
2016-12-14 20:30:16 -0800

    build without lively.lang

--- build.js
+++ build.js
@@ -4,21 +4,16 @@ var fs = require("fs");
 var path = require("path");
 var rollup = require('rollup');
 var babel = require('rollup-plugin-babel');
-
-var targetFile1 = "dist/lively.notifications_no-deps.js";
-var targetFile2 = "dist/lively.notifications.js";
-
-var langSource = fs.readFileSync(require.resolve("lively.lang/dist/lively.lang.dev.js"));
+var targetFile = "dist/lively.notifications.js";
 
 module.exports = Promise.resolve()
   .then(() => rollup.rollup({
     entry: "index.js",
-    plugins: [babel({
-      exclude: 'node_modules/**',
-      sourceMap: true,
-      babelrc: false,
-      plugins: ["syntax-object-rest-spread", "transform-object-rest-spread"],
-      presets: ["es2015-rollup"]
+    plugins: [babel({    
+      exclude: 'node_modules/**', sourceMap: false,
+      "presets": [["es2015", {modules: false}]],
+      "plugins": ['transform-async-to-generator', "syntax-object-rest-spread", "transform-object-rest-spread", "external-helpers"],
+      babelrc: false
     })]
   }))
   .then(bundle =>
@@ -32,21 +27,73 @@ module.exports = Promise.resolve()
 
   // 3. massage code a little
   .then(bundled => {
-    const noDeps = `(function() {
+    return `(function() {
   var GLOBAL = typeof window !== "undefined" ? window :
       typeof global!=="undefined" ? global :
         typeof self!=="undefined" ? self : this;
   ${bundled.code}
   if (typeof module !== "undefined" && module.exports) module.exports = GLOBAL.lively.notifications;
 })();`;
-    return {noDeps: noDeps, complete: `${langSource}\n${noDeps}`};
   })
 
   // 4. create files
-  .then(sources => {
-    fs.writeFileSync(targetFile1, sources.noDeps);
-    fs.writeFileSync(targetFile2, sources.complete);
+  .then(source => {
+    fs.writeFileSync(targetFile, source);
   })
 
   .catch(err => console.error(err));
 
+
+
+
+
+/*global require, process*/
+
+var fs = require("fs");
+var path = require("path");
+var rollup = require('rollup');
+var babel = require('rollup-plugin-babel');
+
+var targetFile = "dist/lively.notifications.js";
+
+// output format - 'amd', 'cjs', 'es6', 'iife', 'umd'
+module.exports = Promise.resolve()
+
+  .then(() => rollup.rollup({
+    entry: "index.js",
+    plugins: [
+      babel({
+        exclude: 'node_modules/**', sourceMap: false,
+        "presets": [["es2015", {modules: false}]],
+        "plugins": ['transform-async-to-generator', "syntax-object-rest-spread", "transform-object-rest-spread", "external-helpers"],
+        babelrc: false
+      })]
+  }))
+
+  .then(bundle =>
+    bundle.generate({
+      format: 'iife',
+      moduleName: 'lively.notifications',
+      globals: {
+        "lively.lang": "lively.lang",
+        "lively.ast": "lively.ast",
+        "module": "typeof module !== 'undefined' ? module.constructor : {}"
+      },
+    }))
+
+  // 3. massage code a little
+  .then(bundled => {
+    return `(function() {
+  var GLOBAL = typeof window !== "undefined" ? window :
+      typeof global!=="undefined" ? global :
+        typeof self!=="undefined" ? self : this;
+  ${bundled.code}
+  if (typeof module !== "undefined" && module.exports) module.exports = GLOBAL.lively.classes;
+})();`;
+  })
+
+  // 4. inject dependencies
+  .then(source => {
+    fs.writeFileSync(targetFile, source);
+  })
+  .catch(err => { console.error(err.stack || err); throw err; })
