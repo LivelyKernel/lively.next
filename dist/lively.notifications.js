0311cefe17dda8c18fb33cf43c0d7e8e1959c01c
Robert Krahn <robert.krahn@gmail.com>
2016-12-29 16:59:20 -0800

    build

--- dist/lively.notifications.js
+++ dist/lively.notifications.js
@@ -18,43 +18,36 @@
 
 var env = void 0;
 
-function getEnv(system) {
+function getEnv(_System) {
   // System? -> Env
-  if (system === undefined) {
-    if (typeof System === 'undefined') {
+  if (_System === undefined) {
+    if (typeof System === "undefined") {
       // fallback if not System is available
-      if (env !== undefined) {
-        return env;
-      }
-      return env = {
+      if (env !== undefined) return env;
+      return env || (env = {
         emitter: lively_lang.events.makeEmitter({}, { maxListenerLimit: 10000 }),
         notifications: []
-      };
-    } else {
-      system = System;
+      });
     }
+
+    _System = System;
   }
 
-  var livelyEnv = system.get("@lively-env");
-  var options = void 0;
-  if (livelyEnv === undefined) {
-    options = {};
-    system.set("@lively-env", system.newModule({ options: options }));
-  } else {
-    options = livelyEnv.options;
-  }
-  if (!options) {
-    throw new Error("@lively-env registered read-only");
-  }
+  var livelyEnv = _System.get("@lively-env");
+  if (!livelyEnv) _System.set("@lively-env", _System.newModule({ options: {} }));
+
+  var options = livelyEnv.options;
+
+  if (!options) throw new Error("@lively-env registered read-only");
+
   if (!options.emitter) {
     Object.assign(options, {
-      emitter: system["__lively.notifications_emitter"] || (system["__lively.notifications_emitter"] = lively_lang.events.makeEmitter({}, { maxListenerLimit: 10000 })),
-      notifications: system["__lively.notifications_notifications"] || (system["__lively.notifications_notifications"] = [])
+      emitter: _System["__lively.notifications_emitter"] || (_System["__lively.notifications_emitter"] = lively_lang.events.makeEmitter({}, { maxListenerLimit: 10000 })),
+      notifications: _System["__lively.notifications_notifications"] || (_System["__lively.notifications_notifications"] = [])
     });
   }
-  var _options = options,
-      emitter = _options.emitter,
-      notifications = _options.notifications;
+  var emitter = options.emitter,
+      notifications = options.notifications;
 
   return { emitter: emitter, notifications: notifications };
 }
@@ -65,15 +58,20 @@ function subscribe(type, handler, system) {
   return handler;
 }
 
+function subscribeOnce(type, handler, system) {
+  // EventType, Handler, System? -> Handler
+  getEnv(system).emitter.once(type, handler);
+  return handler;
+}
+
 function emit(type) {
   var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
   var time = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : Date.now();
   var system = arguments[3];
 
   // EventType, Notification?, EventTime?, System? -> Notification
-  var notification = Object.assign({ type: type, time: time }, data);
-
-  var _getEnv = getEnv(system),
+  var notification = Object.assign({ type: type, time: time }, data),
+      _getEnv = getEnv(system),
       emitter = _getEnv.emitter,
       notifications = _getEnv.notifications;
 
@@ -128,7 +126,7 @@ function getRecord(system) {
 function log(notification) {
   // Notification -> ()
   var padded = notification.type + " ".repeat(Math.max(0, 32 - notification.type.length));
-  console.log(padded + ' ' + lively_lang.obj.inspect(notification, { maxDepth: 2 }));
+  console.log(padded + " " + lively_lang.obj.inspect(notification, { maxDepth: 2 }));
 }
 
 function startLogging(system) {
@@ -142,6 +140,7 @@ function stopLogging(system) {
 }
 
 exports.subscribe = subscribe;
+exports.subscribeOnce = subscribeOnce;
 exports.emit = emit;
 exports.unsubscribe = unsubscribe;
 exports.unsubscribeAll = unsubscribeAll;
