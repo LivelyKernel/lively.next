86db423cf146a27b6985fdac9513252ac001d1cf
Robert Krahn <robert.krahn@gmail.com>
2017-01-05 15:49:35 -0800

    debugging visualizer based on cytoscape

--- debugging.js
+++ debugging.js
@@ -1,7 +1,8 @@
-import { arr, num, string, graph, Path } from "lively.lang";
+import { fun, obj, arr, num, string, graph, Path } from "lively.lang";
 
 import { ObjectPool } from "lively.serializer2";
 import ClassHelper from "./class-helper.js";
+import { HTMLMorph, inspect } from "lively.morphic";
 
 /*
 
@@ -33,13 +34,13 @@ export class SnapshotInspector {
     pool.objectRefs().forEach(ref => {
       var snap = ref.currentSnapshot,
           {className} = snap[ClassHelper.classMetaForSerializationProp] || {};
-  
+
       var propNames = Object.keys(snap.props);
       if (className == null) {
         if (propNames.length > 3) className = "{" + propNames.slice(0, 3).join(", ") + ", ...}";
         else className = "{" + propNames.join(", ") + "}";
       }
-  
+
       if (!classes[className])
         classes[className] = {count: 0, bytes: 0, name: className, objects: []};
       classes[className].count++;
@@ -50,7 +51,7 @@ export class SnapshotInspector {
         var value = snap.props[key].value;
         if (!value || typeof value !== "string"
          || !pool.expressionSerializer.isSerializedExpression(value)) return;
-        
+
         var {__expr__} = pool.expressionSerializer.exprStringDecode(value);
         var expr = expressions[__expr__]
         if (!expr)
@@ -149,28 +150,28 @@ export class SnapshotInspector {
     path = path.replace(/^\./, "");
     // foo[0].baz => foo.0.baz
     path = path.replace(/\[([^\]])+\]/g, ".$1")
-  
+
     var parts = Path(path).parts(),
         current = this.snapshot[fromId],
         counter = 0;
-  
+
     while (true) {
       if (counter++ > 1000) throw "stop";
       var key = parts.shift();
       if (!key) return current;
-  
+
       if (!current.props || !current.props[key])
         throw new Error(`Property ${key} not found for ref ${JSON.stringify(current)}`);
-  
+
       var {value} = current.props[key];
       if (!value)
         throw new Error(`Property ${key} has no value`);
-  
+
       while (Array.isArray(value))
         value = value[parts.shift()];
-  
+
       if (!value || !value.__ref__) return value;
-  
+
       current = this.snapshot[value.id];
     }
     return current;
@@ -281,4 +282,238 @@ function findPathFromToId(snapshot, fromId, toId, options = {}) {
   if (showPath)
     result = '.' + result.join('.');
   return result;
+}
+
+
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import cytoscape from "http://js.cytoscape.org/js/cytoscape.js";
+import coseBilkentLayout from "https://cdn.rawgit.com/cytoscape/cytoscape.js-cose-bilkent/1.0.5/cytoscape-cose-bilkent.js";
+import { connect } from "lively.bindings";
+coseBilkentLayout(cytoscape);
+
+
+// var viz = await ObjectGraphVisualizer.renderSnapshotAndOpen(window.snapshot)
+
+export class ObjectGraphVisualizer extends HTMLMorph {
+
+  static renderSnapshotAndOpen(snapshot) {
+    var viz = this.renderSnapshot(snapshot);
+    viz.openInWindow({title: "object graph visualizer"});
+    return viz
+  }
+
+  static renderSnapshot(snapshot) {
+    return new this({snapshot});
+  }
+
+  constructor(props = {}) {
+    super({
+      draggable: true,
+      ...obj.dissoc(props, ["snapshot"])
+    });
+    this.state = {cy: null, snapshot: props.snapshot};
+    connect(this, 'extent', this, 'updateDebounced', {updater: ($upd, x, y) => window.inspect({x,y}) && $upd()});
+    this.update();
+  }
+
+  cleanup() {
+    this.html = "";
+    if (this.state.cy) {
+      this.state.cy.destroy();
+      this.state.cy = null;
+    }
+  }
+
+  update() {
+    this.cleanup();
+    if (this.state.snapshot)
+      this.renderSnapshot(this.state.snapshot).catch(err => this.showError(err));
+  }
+
+  updateDebounced() {
+    fun.debounceNamed("updateDebounced-" + this.id, 100, () => this.update())();
+  }
+
+  get snapshot() { return this.state.snapshot; }
+  set snapshot(x) { this.state.snapshot = x; this.updateDebounced(); }
+
+  async renderSnapshot(snapshot) {
+    this.cleanup();
+
+    var i = new SnapshotInspector(snapshot);
+    var g = i.referenceGraph();
+    var reverseG = graph.invert(g);
+    var morphIds = Object.keys(g).filter(id => !!Path("props.submorphs").get(snapshot[id]));
+
+    var nodes = Object.keys(g).map(id => {
+      // fixme
+      var objectSnapshot = snapshot[id];
+      var label = string.truncate(i.explainId(id), 20);
+      var classes = "", parent;
+
+      var isMorph = morphIds.includes(id),
+          className = objectSnapshot["lively.serializer-class-info"]
+                   && objectSnapshot["lively.serializer-class-info"].className;
+
+      if (isMorph && className !== "ListItemMorph") {
+        classes = "Morph " + className;
+        label += "\n" + (Path("props.name.value").get(objectSnapshot) || "");
+        // parent = "morph";
+        // var refs = Object.keys(g).filter(otherId => g[otherId].includes(id))
+        // var owner = refs.find(ref =>
+        //   (path(`props.submorphs.value`).get(snapshot[ref]) || []).some(ea => ea.id === id));
+        // if (owner) parent = owner
+      }
+      // else {
+      //
+      //   // trying to find a morph parent to group in the graphs
+      //   var remaining = reverseG[id] || [], seen = {}, maxIterations = 1000, counter = 0;
+      //   while (true) {
+      //     if (counter++ > maxIterations) break;
+      //     var id = remaining.shift();
+      //     if (!id) break;
+      //     if (id in seen) continue;
+      //     seen[id] = true;
+      //     var pointersToMe = reverseG[id] || [];
+      //     var morphId = pointersToMe.find(id => morphIds.includes(id));
+      //     if (morphId) { parent = morphId; break; }
+      //     remaining.push(...pointersToMe);
+      //   }
+      //   if (parent) show(`${id} => ${parent}`)
+      // }
+
+      return {
+        group: "nodes",
+        autounselectify: true,
+        data: {id, parent, label, snapshot: objectSnapshot},
+        classes
+      }
+    });
+
+
+    // nodes.push({group: "nodes", data: {id: "morph"}})
+
+
+    var edges = arr.flatmap(Object.keys(g), id => g[id].map(id2 => {
+      return {group: "edges", data: {source: id, target: id2}};
+    }));
+
+
+    var cy = this.state.cy = cytoscape({
+      // container: iframeMorph.innerWindow.document.body,
+      container: this.domNode,
+
+      autoungrabify: true,
+      // boxSelectionEnabled: false,
+      // autounselectify: true,
+
+      layout: {
+        name: "spread",
+        // idealEdgeLength: 3,
+        // nodeOverlap: 2
+      },
+
+      layout: {
+        name: "cose-bilkent",
+        idealEdgeLength: 50,
+        numIter: 2500,
+        // edgeElasticity: 0.2,
+        // gravity: .5
+        // padding: 50,
+        nodeRepulsion: 45000,
+        animate: "end",
+      },
+
+      // layout: {
+      //   name: "concentric"
+      // },
+
+      style: [
+
+        {
+          selector: "",
+          style: {
+            	"active-bg-color": "#ccc",
+            	"active-bg-opacity": 0.333
+          }
+        },
+
+        {
+          selector: "node",
+          style: {
+            padding: 10,
+            "background-color": "#999",
+            "background-opacity" : ".5",
+            width: "label", height: 20,
+            "label": "data(label)",
+            "text-valign": "center",
+            "text-halign": "center",
+            "font-size": "10px",
+            "color": "#666",
+            shape: "roundrectangle",
+            "text-wrap": "wrap",
+            "text-outline-color": "#666",
+            "text-outline-width": 3,
+            "text-outline-opacity": 1,
+            "color": "white",
+          }
+        },
+
+        {
+          selector: "node.Morph",
+          style: {
+            width: "label", height: "label",
+            "background-color": "#666",
+            "background-opacity" : ".3",
+            "font-size": "18px",
+            "text-outline-color": "#666",
+            "text-outline-width": 3,
+            "text-outline-opacity": 1,
+          }
+        },
+
+        {
+          selector: "edge",
+          style: {
+            "width": 4,
+            "line-color": "#666",
+            "curve-style": "bezier",
+            "target-arrow-shape": "triangle"
+          }
+        },
+
+        {
+          selector: "edge:selected",
+          style: {
+            "line-color": "blue"
+          }
+        }
+      ],
+
+      elements: nodes.concat(edges)
+    });
+
+    this.onDrag = function(evt) {
+      var thing = cy.$(":active")[0];
+      if (!thing) return;
+      var {x,y} = evt.positionIn(this);
+      thing.renderedPosition("x", x);
+      thing.renderedPosition("y", y);
+    };
+
+    this.onMouseUp = function(evt) {
+      // show("draaaag" + cy.$(":active").length);
+      var node = cy.$("node:active")[0];
+      var edge = cy.$("edge:active")[0];
+      node && inspect({id: node.data().id, snapshot: node.data().snapshot});
+      edge &&  this.setStatusMessage(i.findPathFromToId(edge.data().source, edge.data().target));
+    };
+
+    return this;
+  }
+
+
 }
