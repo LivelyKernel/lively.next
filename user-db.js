9449518f67e72a0c5f12301a6648cf4d16d497ac
Matthew Hemmings <discount.yoyos@gmail.com>
2017-02-17 13:53:23 -0800

    factored authentication to dummy auth server

--- user-db.js
+++ user-db.js
@@ -1,11 +1,21 @@
-// export class dbAccess{
-  
 export function push(obj){
   return('push')
+}
+
+export async function userList(){
+  var userList = 'empty';  
+  return userList
 }
 
 export function pop(obj){
   return('pop')
 }
 
-// }
+function getJSON(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () { 
+    callback(this.responseText) 
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
--- user.js
+++ user.js
@@ -1,11 +1,13 @@
 import { string, promise } from "lively.lang";
-import * as bcrypt from "node_modules/bcryptjs";
+import * as bcrypt from "lively.user/node_modules/bcryptjs";
+import * as authserver from "lively.user/authserver.js"
 
-export class user {
+
+export default class user {
   constructor(options) {
   var {name, password} = options  
   this.name = name ? name : 'anonymous'
-  this.authenticated = this.authenticate(name,password)
+  this.token = this.authenticate(name,password)
 
   //Freeze to prevent manual changes to the object
   Object.freeze(this)
@@ -18,22 +20,11 @@ export class user {
   authenticate(name,pwd){
   // replace with db accessor pulling hash from db
   // currently resolves always to true
-    var userHash = this.storepwd(pwd)
-    //Next two lines are simply to force authentication to return true
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(pwd, salt);    
-    return this.pwcheck(pwd,hash)
+    return authserver.authenticate(name,pwd)
   }
 
-  pwcheck(pwd,hash){   
-    // Compare hash to entered pwd
-    return bcrypt.compareSync(pwd,hash)
-  }
-  storepwd(pwd){
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(pwd, salt);    
-    //Add db accessor to store hash here
-  }
+  
+  
 }
 
 
