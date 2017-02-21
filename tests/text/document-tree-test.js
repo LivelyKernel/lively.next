35c5c40ca9703e0cf91b906f27dbd9b1a5129327
Robert Krahn <robert.krahn@gmail.com>
2017-02-20 21:14:43 -0800

    tree balances mostly correctly, there some delete rebalancing issues left, I think

--- tests/text/document-tree-test.js
+++ tests/text/document-tree-test.js
@@ -1,29 +1,65 @@
 /*global System, declare, it, xit, describe, xdescribe, beforeEach, afterEach, before, after*/
 import { expect } from "mocha-es6";
 import TextTree from "../../text/document-tree.js";
+import { arr } from "lively.lang";
+
+var opts = {maxLeafSize: 3, minLeafSize: 2, maxNodeSize: 5, minNodeSize: 2};
+
+function consistencyCheck(tree) {
+  tree.root.traverse(ea => {
+    var {
+      isRoot,
+      isLeaf,
+      children,
+      options: {maxLeafSize, maxNodeSize, minLeafSize, minNodeSize}
+    } = ea;
+    if (isRoot)
+      return;
+    var max = isLeaf ? maxLeafSize : maxNodeSize,
+      min = isLeaf ? minLeafSize : minNodeSize;
+    expect(children.length).within(min, max, `children count of ${ea}`);
+    if (isLeaf)
+      expect().assert(
+        children.every(ea => ea.isLine),
+        `children of ${ea} are not all lines!`
+      );
+    else
+      expect().assert(
+        children.every(ea => ea.isNode),
+        `children of ${ea} are not all nodes!`
+      );
+  });
+}
 
 describe("text tree", () => {
 
-  it("appends", () => {
-    var textTree = new TextTree();
-    textTree.appendLine("hello world");
-    expect(textTree.print()).equals(`root (size: 1)\n line 0 (height: 0, text: "hello world")`)
+  it("finds lines by row", () => {
+    var textTree = new TextTree(["a", "b", "c", "d"]);
+    consistencyCheck(textTree);
+    var lines = [0,1,2,3,4].map(n => textTree.root.findRow(n));
+    expect().assert(lines[0], "line 0 not found");
+    expect().assert(lines[1], "line 1 not found");
+    expect().assert(lines[2], "line 2 not found");
+    expect().assert(lines[3], "line 3 not found");
+    expect(lines[0]).containSubset({text: "a"});
+    expect(lines[1]).containSubset({text: "b"});
+    expect(lines[2]).containSubset({text: "c"});
+    expect(lines[3]).containSubset({text: "d"});
+
+    expect(lines[0].parent).equals(textTree.root.children[0], "parent line 0");
+    expect(lines[1].parent).equals(textTree.root.children[0], "parent line 1");
+    expect(lines[2].parent).equals(textTree.root.children[1], "parent line 2");
+    expect(lines[3].parent).equals(textTree.root.children[1], "parent line 3");
+
+    expect(lines[0].row).equals(0);
+    expect(lines[1].row).equals(1);
+    expect(lines[2].row).equals(2);
+    expect(lines[3].row).equals(3);
   });
 
-  it("inserts", () => {
-    var textTree = new TextTree();
-    textTree.appendLine("c");
-    textTree.insertLine("a", 0);
-    textTree.insertLine("b", 1);
-    expect(textTree.print()).equals(
-      `root (size: 3)\n`
-    + ` line 0 (height: 0, text: "a")\n`
-    + ` line 1 (height: 0, text: "b")\n`
-    + ` line 2 (height: 0, text: "c")`);
-  });
 
-  it("balances", () => {
-    var textTree = new TextTree(["a", "b", "c", "d"], {maxLeafSize: 3});
+  it("balances leaf nodes", () => {
+    var textTree = new TextTree(["a", "b", "c", "d"], opts);
     textTree.balance();
     expect(textTree.print()).equals(
       `root (size: 4)\n`
@@ -35,4 +71,114 @@ describe("text tree", () => {
     + `  line 3 (height: 0, text: "d")`);
   });
 
+  describe("insertion", () => {
+
+    it("appends", () => {
+      var textTree = new TextTree();
+      textTree.insertLine("hello world");
+      expect(textTree.print()).equals(`root (size: 1)\n line 0 (height: 0, text: "hello world")`)
+    });
+
+    it("inserts", () => {
+      var textTree = new TextTree();
+      textTree.insertLine("c");
+      textTree.insertLine("a", 0);
+      textTree.insertLine("b", 1);
+      expect(textTree.print()).equals(
+        `root (size: 3)\n`
+      + ` line 0 (height: 0, text: "a")\n`
+      + ` line 1 (height: 0, text: "b")\n`
+      + ` line 2 (height: 0, text: "c")`);
+    });
+
+    it("balances leaf nodes after insert", () => {
+      var textTree = new TextTree(["a", "b"], opts);
+      textTree.insertLine("x", 0);
+      textTree.insertLine("y", 3);
+      expect(textTree.print()).equals(
+        `root (size: 4)\n`
+      + ` leaf (size: 2)\n`
+      + `  line 0 (height: 0, text: "x")\n`
+      + `  line 1 (height: 0, text: "a")\n`
+      + ` leaf (size: 2)\n`
+      + `  line 2 (height: 0, text: "b")\n`
+      + `  line 3 (height: 0, text: "y")`);
+    });
+
+    it("balances after insert 1", () => {
+      var textTree = new TextTree([], opts);
+      textTree.insertLines(["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"])
+      textTree.insertLine("b");
+      textTree.insertLine("b");
+      consistencyCheck(textTree);
+    });
+
+    it("balances after insert 2", () => {
+      var textTree = new TextTree([], opts);
+      textTree.insertLines(["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"])
+      textTree.insertLine("b");
+      textTree.insertLine("b");
+      consistencyCheck(textTree);
+    });
+
+  });
+
+
+  describe("removal", () => {
+
+    it("removes line", () => {
+      var textTree = new TextTree(["a", "b", "c"]);
+      textTree.removeLine(1);
+      expect(textTree.print()).equals(`root (size: 2)\n line 0 (height: 0, text: "a")\n line 1 (height: 0, text: "c")`);
+      textTree.removeLine(1);
+      textTree.print2()
+
+      expect(textTree.print()).equals(`root (size: 1)\n line 0 (height: 0, text: "a")`);
+      textTree.removeLine(0);
+      expect(textTree.print()).equals(`root (size: 0)`);
+    });
+
+    it("balances leaf nodes after remove 1", () => {
+      var textTree = new TextTree(["a", "b", "c", "d"], opts);
+      textTree.removeLine(3);
+      expect(textTree.print()).equals(
+          `root (size: 3)\n`
+        + ` line 0 (height: 0, text: "a")\n`
+        + ` line 1 (height: 0, text: "b")\n`
+        + ` line 2 (height: 0, text: "c")`);
+      textTree.removeLine(1);
+      expect(textTree.print()).equals(
+          `root (size: 2)\n`
+        + ` line 0 (height: 0, text: "a")\n`
+        + ` line 1 (height: 0, text: "c")`);
+    });
+
+    it("balances leaf nodes after remove 2", () => {
+      var textTree = new TextTree(["a", "b", "c", "d"], opts);
+      textTree.removeLine(1);
+      expect(textTree.print()).equals(
+          `root (size: 3)\n`
+        + ` line 0 (height: 0, text: "a")\n`
+        + ` line 1 (height: 0, text: "c")\n`
+        + ` line 2 (height: 0, text: "d")`);
+      textTree.removeLine(1);
+      expect(textTree.print()).equals(
+          `root (size: 2)\n`
+        + ` line 0 (height: 0, text: "a")\n`
+        + ` line 1 (height: 0, text: "d")`);
+    });
+
+  });
+
+
 });
+
+
+function fooo() {
+
+  var textTree = new TextTree(["a", "b", "c", "d"], {maxLeafSize: 3, minLeafSize: 2});
+
+
+  textTree.insertLines(["hello", "world", "hello", "world", "hello", "world", "hello", "world", "hello", "world", "hello", "world", "hello", "world", "hello", "world", "hello", "world", ])
+  textTree.print2()
+}
--- text/document-tree.js
+++ text/document-tree.js
@@ -1,76 +1,265 @@
-import { arr } from "lively.lang";
+import { arr, string } from "lively.lang";
+import { printTree } from "lively.lang/text-graphics.js";
+
 
 var defaultOptions = {
   maxLeafSize: 3,
+  minLeafSize: 2,
   leafSplit: 0.5,
   maxNodeSize: 10,
+  minNodeSize: 5,
   nodeSplit: 0.5
 }
 
-class Node {
+
+class TreeNode {
+
+  constructor(parent) {
+    this.parent = parent;
+  }
+
+  get root() { return this.parent ? this.parent.root : this; }
+
+  get isRoot() { return !this.parent; }
+
+  withParents() {
+    var node = this, result = [];
+    while (node) { result.push(node); node = node.parent; }
+    return result;
+  }
+  
+  traverse(doFn) {
+    var result = [doFn.call(null,this)];
+    if (this.isLeaf) return result;
+    for (var i = 0; i < this.children.length; i++) {
+      result.push(...this.children[i].traverse(doFn));
+    }
+    return result;
+  }
+
+}
+
+
+class InnerTreeNode extends TreeNode {
 
   constructor(parent = null, children = [], size = 0, options) {
+    super(parent);
     this.options = options;
-    this.parent = parent;
     this.children = children;
     this.size = size;
     this.height = 0;
     this.isLeaf = true;
   }
 
-  get root() { return this.parent ? this.parent.root : this; }
+  get isNode() { return true; }
 
   resize(n) {
     this.size += n;
     this.parent && this.parent.resize(n);
+    // if (n > 0) this.balanceAfterGrowth(n);
   }
 
-  insert(lines, atIndex = this.size, options = {}) {
+  findRow(row) {
+    if (row < 0 || row >= this.size) return null;
+    if (this.isLeaf) return this.children[row];
+    for (var i = 0; i < this.children.length; i++) {
+      var child = this.children[i], childSize = child.size;
+      if (row < childSize) return child.findRow(row);
+      row -= childSize;
+    }
+    return null;
+  }
+
+  insert(lines, atIndex = this.size) {
     if (this.isLeaf) {
       lines = lines.map(text => new Line(this, text));
       this.children.splice(atIndex, 0, ...lines);
       this.resize(lines.length);
-      this.balance();
+      this.balanceAfterGrowth();
       return lines;
     }
 
     if (this.children.length === 0)
-      this.children.push(new Node(this, [], 0, this.options));
+      this.children.push(new InnerTreeNode(this, [], 0, this.options));
 
     var i = 0;
     for (; i < this.children.length; i++) {
       var child = this.children[i], childSize = child.size;
-      if (atIndex <= childSize) return child.insert(lines, atIndex, options);
+      if (atIndex <= childSize) return child.insert(lines, atIndex);
       atIndex -= childSize;
     }
 
     var last = this.children[i-1];
-    return last.insert(lines, last.size, options);
+    return last.insert(lines, last.size);
   }
 
-  balance() {
-    var {isLeaf, parent, children, options: {maxLeafSize, maxNodeSize}} = this,
-        maxChildren = isLeaf ? maxLeafSize : maxNodeSize;
+  remove(index) {
+    if (index > this.size) throw new Error(`Trying to remove index ${index} from ${this}`);
 
-    if (children.length <= maxChildren) return;
-
-    if (!parent) {
-      parent = this.parent = new Node(null, [this], this.size, this.options);
+    if (!this.isLeaf) {
+      for (var i = 0; i < this.children.length; i++) {
+        var child = this.children[i], childSize = child.size;
+        if (index < childSize) return child.remove(index);
+        index -= childSize;
+      }
+      throw new Error("Should never get here...");
     }
 
-    var splitIndex = Math.floor(children.length / 2);
-    var i = 0, mySize = 0, otherSize = 0;
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // merge left or right
+
+    var {children, options} = this,
+        line = children[index],
+        maxChildren = options.maxLeafSize,
+        minChildren = options.minLeafSize;
+
+    var needsMerge = children.length - 1 < minChildren;
+    if (needsMerge) {
+      var prevLine = children[0].prevLine();
+      var leftSibling = prevLine ? prevLine.parent : null;
+      var nextLine = arr.last(children).nextLine();
+      var rightSibling = nextLine ? nextLine.parent : null;
+    }
+
+    // remove line from my children
+    children.splice(index, 1);
+    line.parent = null;
+    this.resize(-1);
+
+    // if less than desired nodes...
+    if (needsMerge) {
+
+      // ...try to merge with left or right sibling
+      var mergeLeft = leftSibling && leftSibling.children.length + children.length <= maxChildren,
+          mergeRight = !mergeLeft && rightSibling && rightSibling.children.length + children.length <= maxChildren,
+          mergeTarget = mergeLeft ? leftSibling : mergeRight ? rightSibling : null;
+
+      if (mergeTarget) {
+        // update size of sibling and parents up to common parent
+        var mySize = this.size,
+            myParents = this.withParents(),
+            nodeToResize = mergeTarget,
+            commonParent;
+        while (nodeToResize) {
+          if (myParents.includes(nodeToResize)) {
+            commonParent = nodeToResize; break; }
+          nodeToResize.size += mySize;
+          nodeToResize = nodeToResize.parent;
+        }
+
+        // subtract my size from self and all parents up to common parent with sibling
+        if (commonParent)
+          myParents.slice(0, myParents.indexOf(commonParent)).forEach(n => n.size -= mySize);
+        // move children over
+        children.forEach(ea => ea.parent = mergeTarget);
+        if (mergeLeft) mergeTarget.children.push(...children);
+        else mergeTarget.children.unshift(...children);
+        children.length = 0;
+      }
+    }
+
+    if (children.length === 0) {
+      var p = this.parent;
+      if (p) {
+        this.parent = null;
+        p.children.splice(p.children.indexOf(this), 1);
+        p.balanceAfterShrink();
+      }
+    } else {
+      this.balanceAfterShrink();
+    }
+
+    return line;
+  }
+
+  balanceAfterGrowth() {
+    var {isLeaf, size, parent, children, options} = this,
+        maxChildren = isLeaf ? options.maxLeafSize : options.maxNodeSize,
+        split = children.length > maxChildren;
+
+    if (!split) return;
+
+    if (!parent) {
+      parent = this.parent = new InnerTreeNode(null, [this], size, options);
+      parent.isLeaf = false;
+    }
+
+    var splitIndex = Math.floor(children.length / 2),
+        i = 0, mySize = 0, otherSize = 0;
     for (; i < splitIndex; i++) mySize += children[i].size;
     for (; i < children.length; i++) otherSize += children[i].size;
 
     this.children = children.slice(0, splitIndex);
     this.size = mySize;
 
-    var indexInParent = parent.children.indexOf(this);
-    parent.children.splice(
-      indexInParent + 1, 0,
-      new Node(parent, children.slice(splitIndex), otherSize, this.options));
+    var indexInParent = parent.children.indexOf(this),
+        otherChildren = children.slice(splitIndex),
+        otherNode = new InnerTreeNode(parent, otherChildren, otherSize, options);
+    otherNode.isLeaf = isLeaf;
+    otherChildren.forEach(ea => ea.parent = otherNode);
+    parent.children.splice(indexInParent + 1, 0, otherNode);
 
+    if (otherNode.children.length > maxChildren)
+      otherNode.balanceAfterGrowth();
+    if (this.children.length > maxChildren)
+      this.balanceAfterGrowth();
+
+    this.parent.balanceAfterGrowth();
+  }
+
+  balanceAfterShrink() {
+    var {isLeaf, parent, children, options} = this,
+        {maxLeafSize, maxNodeSize, minLeafSize, minNodeSize} = options,
+        maxChildren = isLeaf ? maxLeafSize : maxNodeSize,
+        minChildren = isLeaf ? minLeafSize : minNodeSize;
+
+    if (children.length >= minChildren) {
+      parent && parent.balanceAfterShrink();
+      return;
+    }
+
+    if (children.length === 0) {
+      if (parent) {
+        arr.remove(parent.children, this);
+        parent.balanceAfterShrink();
+      }
+      return;
+    }
+
+    // less children than desired
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    if (isLeaf) {
+      // its a leaf so children are lines, try to remove myself...
+      if (parent && parent.children.length === 1) {
+        // I'm the only child...
+        this.parent = null;
+        parent.children.length = 0;
+        parent.children.push(...children);
+        children.forEach(ea => ea.parent = parent);
+        parent.isLeaf = true;
+      }
+      parent && parent.balanceAfterShrink();
+      return;
+    }
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // not a leaf, try to get rid of myself anyway...
+    var grandChildren = [];
+    for (var i = 0; i < children.length; i++) {
+      grandChildren.push(...children[i].children);
+    }
+
+    var leaf = children[0].isLeaf;
+    if (grandChildren.length > (leaf ? maxLeafSize : maxNodeSize)) return;
+
+    children.forEach(ea => ea.parent = null);
+    grandChildren.forEach(ea => ea.parent = this);
+    children.length = 0;
+    children.push(...grandChildren);
+    this.isLeaf = leaf;
+
+    parent && parent.balanceAfterShrink();
   }
 
   print(index = 0, depth = 0, optName) {
@@ -93,42 +282,85 @@ class Node {
     return printed;
   }
 
+  toString() {
+    return `node (${this.isLeaf ? "leaf, " : ""}size: ${this.size})`;
+  }
+
 }
 
-class Line {
+
+class Line extends TreeNode {
 
   constructor(parent, text = "") {
-    this.parent = parent;
+    super(parent);
     this.text = text;
     this.height = 0;
   }
 
-  get size() { return 1 }
+  get isLine() { return true; }
 
-  get root() { return this.parent ? this.parent.root : null; }
+  get size() { return 1; }
+
+  get row() {
+    var p = this.parent;
+    if (!p) return 0;
+
+    var parents = this.withParents().reverse();
+    if (parents.length === 1)
+      return parents.children.indexOf(this);
+
+    var index = 0;
+    for (var i = 0; i < parents.length-1; i++) {
+      var parent = parents[i],
+          node = parents[i + 1],
+          nodeIndex = parent.children.indexOf(node);
+      for (var j = 0; j < nodeIndex; j++) {
+        index += parent.children[j].size;
+      }
+    }
+    return index;
+  }
+
+  prevLine() {
+    var {parent, root, row} = this;
+    if (!parent) return null;
+    if (row <= 0) return null;
+    return root.findRow(row-1);
+  }
+
+  nextLine() {
+    var {parent, root, row} = this;
+    if (!parent) return null;
+    return root.findRow(row+1);
+  }
 
   print(index = 0, depth = 0) {
     var indent = " ".repeat(depth);
     return `${indent}line ${index} (height: ${this.height}, text: ${JSON.stringify(this.text)})`
   }
 
+  toString() {
+    return `line (${string.truncate(this.text, 30)})`;
+  }
+
 }
+
 
 export default class TextTree {
 
   constructor(lines, options) {
     this.options = {...defaultOptions, ...options};
-    this.root = new Node(null, [], 0, this.options);
+    this.root = new InnerTreeNode(null, [], 0, this.options);
     if (lines)
       this.insertLines(lines);
   }
 
-  appendLine(text) {
-    return this.insertLine(text, this.root.size)[0];
+  insertLine(text, atIndex = this.root.size) {
+    return this.insertLines([text], atIndex)[0];
   }
 
-  insertLine(text, atIndex) {
-    return this.insertLines([text], atIndex)[0];
+  removeLine(row) {
+    this.root.remove(row);
   }
 
   insertLines(lines, atIndex) {
@@ -138,7 +370,7 @@ export default class TextTree {
   }
 
   balance() {
-    this.root.balance();
+    this.root.balanceAfterGrowth();
     this.fixRoot();
   }
 
@@ -149,6 +381,13 @@ export default class TextTree {
 
   print() {
     return this.root.print(0, 0, "root");
+  }
+
+  print2() {
+    return printTree(this.root,
+      (node) => node.toString(),
+      (node) => node.children,
+      {padding: [2,1,2,1]});
   }
 
 }
