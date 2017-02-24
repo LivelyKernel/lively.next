7e215b0115fa887e755fdb887f742025e177c5d7
Matthew Hemmings <discount.yoyos@gmail.com>
2017-02-24 15:47:17 -0800

    Updated tests to include expiry checking

--- tests/test.js
+++ tests/test.js
@@ -9,6 +9,20 @@ var salt = bcrypt.genSaltSync(10);
 
 import { expect } from "mocha-es6";
 describe("Authentication", () => {
+
+  before(async () =>{
+     await authserver.addUser("testUser1", "testuser1@lively-next.org", "test",'adminpassword')
+     await authserver.addUser("testUser2", "testuser2@lively-next.org", "test",'adminpassword')
+     malleableUser = new user({name: 'testUser2', email: "testuser2@lively-next.org", password: 'test'})
+     await malleableUser.authenticated(300);
+     var response = await authserver.verify(malleableUser)
+  })
+
+  after(async () =>{
+    await authserver.removeUser("testUser1", "testuser1@lively-next.org", 'adminpassword')
+    await authserver.removeUser("testUser2", "testuser2@lively-next.org", 'adminpassword')
+  })
+
   it("Check if hash can be encoded/decoded", async () => {
     //This should work as long as bcrypt is correctly loading    
     var password = 'password'    
@@ -16,13 +30,13 @@ describe("Authentication", () => {
     expect(bcrypt.compareSync(password,hash)).equals(true)
   });
   it("Check if valid token is generated during correct login", async () =>{
-    var tempUser = new user({name: 'Matt', email: "a@b.c", password: 'password'})
+    var tempUser = new user({name: 'testUser1', email: "testuser1@lively-next.org", password: 'test'})
     await tempUser.authenticated(300);
     var response = await authserver.verify(tempUser)    
     expect(response.type == 'success').equals(true,'token is invalid: \n' + JSON.stringify(response.reason))
   });
   it("Check if INvalid token is generated during bad login", async () =>{
-    var tempUser = new user({name: 'baduser', email: "a@bademail.com", password: 'badpassword'})
+    var tempUser = new user({name: 'testUser2', email: "testUser2@lively-next.org", password: 'badpassword'})
     await tempUser.authenticated(300);    
     var response = await authserver.verify(tempUser).catch(err => {      
       return (err)
@@ -30,6 +44,20 @@ describe("Authentication", () => {
     expect((response.type == 'failed') && (response.reason == 'JWT malformed')).equals(true,'Token does not correctly refuse authentication')
   });
   it("Check if token correctly checks for timeout", async () =>{
-    
+      // console.log(malleableUser)
+      var {username, email} = malleableUser,
+      date = Date.now(),
+      key = 'mysecret'
+      var decode = jwt.verify(malleableUser.token, key)
+      var cert = (Math.floor(decode.date/1000)<decode.exp)      
+      expect(cert).equals(true)
+      var expiredToken = await jwt.sign({ username: username, email: email, date: date}, key,{ expiresIn: 0 });
+      malleableUser.token = expiredToken
+      var status = await new Promise((resolve,reject)=>{
+        jwt.verify(malleableUser.token, key,(err, decoded) =>{
+          if (err) reject(err)
+          resolve(decoded)
+        })
+      }).catch((err)=>{expect(err.name).equals('TokenExpiredError')})
   })
 });
