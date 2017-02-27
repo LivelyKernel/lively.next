a1059c53a32741f8837d14818ae109090a33eed3
Robert Krahn <robert.krahn@gmail.com>
2017-02-26 23:11:40 -0800

    build

--- dist/lively.classes.js
+++ dist/lively.classes.js
@@ -310,12 +310,12 @@ function prepareClassForManagedPropertiesAfterCreation(klass) {
 function prepareClassForProperties(klass, propertySettings, properties) {
   ensurePropertyInitializer(klass);
 
-  var _defaultPropertySetti = _extends({}, defaultPropertySettings, propertySettings),
-      valueStoreProperty = _defaultPropertySetti.valueStoreProperty,
-      defaultGetter = _defaultPropertySetti.defaultGetter,
-      defaultSetter = _defaultPropertySetti.defaultSetter,
+  var valueStoreProperty = propertySettings.valueStoreProperty,
+      defaultGetter = propertySettings.defaultGetter,
+      defaultSetter = propertySettings.defaultSetter,
       myProto = klass.prototype,
       keys = Object.keys(properties);
+
 
   keys.forEach(function (key) {
     var descriptor = properties[key];
@@ -378,7 +378,7 @@ function ensurePropertyInitializer(klass) {
 
 function propertiesAndSettingsInHierarchyOf(klass) {
   // walks class proto chain
-  var propertySettings = {},
+  var propertySettings = _extends({}, defaultPropertySettings),
       properties = {},
       allPropSettings = lively_lang.obj.valuesInPropertyHierarchy(klass, "propertySettings"),
       allProps = lively_lang.obj.valuesInPropertyHierarchy(klass, "properties");
@@ -404,15 +404,12 @@ function propertiesAndSettingsInHierarchyOf(klass) {
 }
 
 function prepareInstanceForProperties(instance, propertySettings, properties, values) {
-  var _defaultPropertySetti2 = _extends({}, defaultPropertySettings, propertySettings),
-      valueStoreProperty = _defaultPropertySetti2.valueStoreProperty,
+  var valueStoreProperty = propertySettings.valueStoreProperty,
       sortedKeys = lively_lang.obj.sortKeysWithBeforeAndAfterConstraints(properties),
       propsNeedingInitialize = [],
       initActions = {};
 
   // 1. this[valueStoreProperty] is were the actual values will be stored
-
-
   if (!instance.hasOwnProperty(valueStoreProperty)) instance[valueStoreProperty] = {};
 
   for (var i = 0; i < sortedKeys.length; i++) {
