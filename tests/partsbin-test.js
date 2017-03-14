afab7fb873ae07780da3a52dc29f737006536779
Robert Krahn <robert.krahn@gmail.com>
2017-03-14 14:48:25 +0100

    fix partsbin tests

--- tests/partsbin-test.js
+++ tests/partsbin-test.js
@@ -5,22 +5,25 @@ import { saveObjectToPartsbinFolder, loadObjectFromPartsbinFolder } from "../par
 import { morph, World, MorphicEnv, inspect } from "lively.morphic";
 import { resource } from "lively.resources";
 import { arr } from "lively.lang";
-import { addScript } from "lively.classes/object-classes.js";
+import ObjectPackage, { addScript } from "lively.classes/object-classes.js";
 import { createDOMEnvironment } from "../rendering/dom-helper.js";
 
 
-var partsbinFolder = "local://morphic-partsbin-tests/", env
-var isNode = System.get("@system-env").node;
+let partsbinFolder = "local://morphic-partsbin-tests/", env, packagesToRemove;
+let isNode = System.get("@system-env").node;
 
 async function setup() {
   if (isNode) {
     env = MorphicEnv.pushDefault(new MorphicEnv(await createDOMEnvironment()));
     env.setWorld(new World({name: "world", extent: pt(300,300)}));
   }
+  packagesToRemove = [];
 }
 
-function teardown() {
-  return isNode && MorphicEnv.popDefault().uninstall();
+async function teardown() {
+  isNode && MorphicEnv.popDefault().uninstall();
+  await Promise.all(packagesToRemove.map(ea => ea.remove()));
+  await resource(partsbinFolder).remove();
 }
 
 describe("partsbin", function () {
@@ -28,11 +31,7 @@ describe("partsbin", function () {
   this.timeout(6000);
 
   beforeEach(setup);
-
-  afterEach(async () => {
-    await resource(partsbinFolder).remove();
-    await teardown()
-  });
+  afterEach(teardown);
 
   it("publishes part as file", async () => {
     var m = morph({name: "test-morph"}),
@@ -44,7 +43,11 @@ describe("partsbin", function () {
   });
 
   it("loads a part", async () => {
+    let p = ObjectPackage.withId("package-for-loads-a-part-test");
+    packagesToRemove.push(p);    
+
     var m = morph({name: "test-morph"}),
+        _ = await p.adoptObject(m),
         _ = await addScript(m, () => 23, "foo"),
         {partName} = await saveObjectToPartsbinFolder(m, m.name, {partsbinFolder}),
         m2 = await loadObjectFromPartsbinFolder(partName, {partsbinFolder});
@@ -54,7 +57,11 @@ describe("partsbin", function () {
 
   it("loads most recent part state from file", async () => {
     // publish version 1
+    let p = ObjectPackage.withId("package-for-loads-a-part-test");
+    packagesToRemove.push(p);    
+
     var m = morph({name: "test-morph", fill: Color.red});
+    await p.adoptObject(m);
     await addScript(m, () => 23, "foo");
     var {url} = await saveObjectToPartsbinFolder(m, m.name, {partsbinFolder}),
         version1 = await resource(url).read();
