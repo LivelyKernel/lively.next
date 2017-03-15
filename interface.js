8f62de03a7049c06e1a1269b64187bb68a8b79a7
Matthew Hemmings <discount.yoyos@gmail.com>
2017-03-15 11:11:17 -0700

    Added function for user to self-register

--- interface.js
+++ interface.js
@@ -1,13 +1,26 @@
 import L2LClient from "lively.2lively/client.js";
 
-export async function createUser(options){
+var defaultClient = L2LClient.default()
+
+export async function getUser(options){
   var opts = {
     name: 'anonymous',
     email: null,
     password: null,
     ...options
   }
-  var defaultClient = L2LClient.default()
+  
   var defaultUser = (await defaultClient.sendToAndWait(defaultClient.trackerId,'newUser',opts)).data
   return defaultUser
+}
+
+export async function createUser(options,ackFn){
+  var {name,email,password} = options
+  if(!name || !email || !password) {
+    var errMsg = 'Insufficient options specified: Requires name, email, password'
+    throw new Error(errMsg)
+  }
+  
+  await defaultClient.sendToAndWait(defaultClient.trackerId,'createUser',options,ackFn)
+  
 }
