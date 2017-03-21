8da93b3871d6a644097dbe42c813b58e5ea92837
Robin Schreiber <robin.schreiber@me.com>
2017-03-21 22:53:49 +0100

    user: streamline constructor, authentication

--- user.js
+++ user.js
@@ -3,33 +3,22 @@ import { string, promise } from "lively.lang";
 import * as authserver from "lively.user/authserver.js"
 
 export default class user {
-  constructor(options) {  
-  var {name, password, email}  = {
-      name: 'anonymous',
-      email: null,
-      password: null,
-      ...options
-    }
-  this.name = name
-  this.email = email
-  this.authenticate(name,email,password)
-  }  
+  constructor({name, username, password, email, avatar} ) {  
+    this.name = name || username || 'anonymous';
+    this.email = email
+    this.avatar = avatar;
+    this.password = password;
+  }
 
   isReady() {
       return ((this.token) && (typeof this.token == 'string' || this.token.status == 'error'));
     }  
-
-  authenticated(timeout) {
-    return promise.waitFor(timeout, () => this.isReady())
-            .catch(err =>            
-              Promise.reject(/timeout/i.test(String(err)) ?
-                new Error(`Timeout in ${this}.authenticated`) : err))
-  }
-  
-  async authenticate(name,email,pwd){  
-  // replace with db accessor pulling hash from db
-    var result = await authserver.authenticate(name,email,pwd)    
-    this.token = await result  
+    
+  async authenticate(timeout = 300){  
+   // replace with db accessor pulling hash from db
+   (async () => this.token = await authserver.authenticate(this))();
+   await new Promise(r => setTimeout(r, timeout));
+   return this.isReady();
   }
 }
 
