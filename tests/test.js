836a5606813a3811c31d1a470bea06c77ea8460e
Robin Schreiber <robin.schreiber@me.com>
2017-03-21 22:55:49 +0100

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,4 @@
+/* global describe, before, after, it, System */
 var bcryptPath = System.decanonicalize("lively.user/node_modules/bcryptjs/index.js").replace(/^file:\/\//, "");
 var bcrypt = System._nodeRequire(bcryptPath);
 var jwtpath = System.decanonicalize("lively.user/node_modules/jsonwebtoken/index.js").replace(/^file:\/\//, "");
@@ -5,22 +6,22 @@ var jwt = System._nodeRequire(jwtpath);
 import user from "lively.user/user.js"
 import * as authserver from "lively.user/authserver.js"
 
-var salt = bcrypt.genSaltSync(10);
+var salt = bcrypt.genSaltSync(10), malleableUser;
 
 import { expect } from "mocha-es6";
 describe("Authentication", () => {
 
-  before(async () =>{
-     await authserver.addUser("testUser1", "testuser1@lively-next.org", "test",'adminpassword')
-     await authserver.addUser("testUser2", "testuser2@lively-next.org", "test",'adminpassword')
+  before(async () => {
+     await authserver.addUser({name: "testUser1", email: "testuser1@lively-next.org", password: "test"},'adminpassword')
+     await authserver.addUser({name: "testUser2", email: "testuser2@lively-next.org", password: "test"},'adminpassword')
      malleableUser = new user({name: 'testUser2', email: "testuser2@lively-next.org", password: 'test'})
-     await malleableUser.authenticated(300);
+     await malleableUser.authenticate(300);
      var response = await authserver.verify(malleableUser)
   })
 
   after(async () =>{
-    await authserver.removeUser("testUser1", "testuser1@lively-next.org", 'adminpassword')
-    await authserver.removeUser("testUser2", "testuser2@lively-next.org", 'adminpassword')
+    await authserver.removeUser({name: "testUser1", email: "testuser1@lively-next.org"}, 'adminpassword')
+    await authserver.removeUser({name: "testUser2", email: "testuser2@lively-next.org"}, 'adminpassword')
   })
 
   it("Check if hash can be encoded/decoded", async () => {
@@ -29,20 +30,23 @@ describe("Authentication", () => {
     var hash = bcrypt.hashSync(password,salt)
     expect(bcrypt.compareSync(password,hash)).equals(true)
   });
-  it("Check if valid token is generated during correct login", async () =>{
+  
+  it("Check if valid token is generated during correct login", async () => {
     var tempUser = new user({name: 'testUser1', email: "testuser1@lively-next.org", password: 'test'})
-    await tempUser.authenticated(300);
+    await tempUser.authenticate(300);
     var response = await authserver.verify(tempUser)    
     expect(response.type == 'success').equals(true,'token is invalid: \n' + JSON.stringify(response.reason))
   });
-  it("Check if INvalid token is generated during bad login", async () =>{
+  
+  it("Check if INvalid token is generated during bad login", async () => {
     var tempUser = new user({name: 'testUser2', email: "testUser2@lively-next.org", password: 'badpassword'})
-    await tempUser.authenticated(300);    
+    await tempUser.authenticate(300);    
     var response = await authserver.verify(tempUser).catch(err => {      
       return (err)
     })    
     expect((response.type == 'failed') && (response.reason == 'JWT malformed')).equals(true,'Token does not correctly refuse authentication')
   });
+  
   it("Check if token correctly checks for timeout", async () =>{
       // console.log(malleableUser)
       var {username, email} = malleableUser,
