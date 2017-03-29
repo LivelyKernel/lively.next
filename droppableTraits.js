ed9596a47ecc0b81a59c7896295f5495e9c5c461
Matthew Hemmings <discount.yoyos@gmail.com>
2017-03-29 13:55:36 -0700

    Experimenting with adding functions via addScript

--- droppableTraits.js
+++ droppableTraits.js
@@ -1,7 +1,43 @@
+import { copy } from "lively.serializer2";
+import { pt } from "lively.graphics";
+import ObjectPackage, { addScript } from "lively.classes/object-classes.js";
+
 function onDropOn(target){
-  alert('test function')
+  console.log(target)
+  return
 }
 
-export function makeDroppable(aMorph){
-  aMorph.onDropOn = onDropOn
+function onDragStart(evt){
+  console.log(evt)
+  return
+  var newMorph = copy(this)
+  newMorph.openInWorld();
+  var hand = evt.hand
+  hand.grab(newMorph)  
+  newMorph.position = pt(0,0)
+}
+
+function onDragEnd(evt){
+  super.onDragEnd(evt)
+  var hand = evt.hand
+  hand.onDrop(evt)
+}
+
+export async function makeDroppable(aMorph){
+   var functionsToApply = [onDragStart,onDragEnd,onDropOn]
+   let obj = $world.get(aMorph)   
+
+   if (!ObjectPackage.lookupPackageForObject(obj)) {
+     let pkg = ObjectPackage.withId("DroppedMethods");
+     await pkg.adoptObject(obj);
+   }  
+   
+   functionsToApply.forEach(async function(fn){     
+     var name = fn.name
+     console.log(name)
+     let {methodName} = await addScript(obj, fn, name);
+   })
+   
+   
+   
 }
