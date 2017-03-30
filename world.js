8f86de1de6a88886674871bcbb245ebb5090963a
Robert Krahn <robert.krahn@gmail.com>
2017-03-29 21:30:46 -0700

    world load ui: public and local locations

--- world.js
+++ world.js
@@ -781,21 +781,9 @@ var worldCommands = [
     exec: async (world, args = {}) => {
       let toLoad = args.resource;
 
-      if (false && !toLoad) { // old world selection
-        let exclude = ea => !ea.name().endsWith(".json") || ea.name() === "package.json",
-            baseURL = System.decanonicalize(`lively.morphic/worlds/`),
-            files = await lively.resources.resource(baseURL).dirList(1, {exclude}),
-            items = files.map(ea =>
-              ({isListItem: true, value: ea, string: ea.name().replace(".json", "")})),
-            lastSaved = arr.last(InputLine.getHistory("lively.morphic-save-world-names").items),
-            preselect = lastSaved ? files.findIndex(ea => ea.name().replace(".json", "") === lastSaved) : 0,
-            {selected: [selected]} = await world.listPrompt("Which world to load", items, {preselect});
-        toLoad = selected;
-      }
-
       if (!toLoad) { // old world selection
         let worldList = world.get("world-list") || await loadObjectFromPartsbinFolder("world-list");
-        return await worldList.bringToFront().alignInWorld(world).update();
+        return await worldList.bringToFront().alignInWorld(world).setLocationToLastChoiceOr("public");
       }
 
       if (!toLoad) return;
