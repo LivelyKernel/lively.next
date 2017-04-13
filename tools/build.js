30e52bf279d9cc06b2ecdcede378f60b512dc0aa
Robert Krahn <robert.krahn@gmail.com>
2017-04-12 22:15:27 -0700

    doc page + complete build

--- tools/build.js
+++ tools/build.js
@@ -6,6 +6,21 @@ var rollup = require('rollup');
 var babel = require('rollup-plugin-babel');
 
 var targetFile = "dist/lively.vm.js";
+var targetFile2 = "dist/lively.vm_standalone.js";
+
+var placeholderSrc = "throw new Error('Not yet read')";
+
+var parts = {
+  "lively.notifications":    {source: placeholderSrc, path: require.resolve("lively.notifications/dist/lively.notifications.js")},
+  "lively.ast":              {source: placeholderSrc, path: require.resolve("lively.ast/dist/lively.ast.js")},
+  "lively.classes":          {source: placeholderSrc, path: require.resolve("lively.classes/dist/lively.classes.js")},
+  "lively.lang":             {source: placeholderSrc, path: require.resolve("lively.lang/dist/lively.lang.js")},
+  "lively.source-transform": {source: placeholderSrc, path: require.resolve("lively.source-transform/dist/lively.source-transform.js")},
+  "babel-regenerator":       {source: placeholderSrc, path: require.resolve("babel-regenerator-runtime/runtime.js")}
+}
+
+Object.keys(parts).forEach(name =>
+  parts[name].source = fs.readFileSync(parts[name].path));
 
 // output format - 'amd', 'cjs', 'es6', 'iife', 'umd'
 module.exports = Promise.resolve()
@@ -15,7 +30,12 @@ module.exports = Promise.resolve()
       babel({
         exclude: 'node_modules/**', sourceMap: false,
         "presets": [["es2015", {modules: false}]],
-        "plugins": ['transform-async-to-generator', "syntax-object-rest-spread", "transform-object-rest-spread", "external-helpers"],
+        "plugins": [
+          'transform-async-to-generator',
+          "syntax-object-rest-spread",
+          "transform-object-rest-spread",
+          "external-helpers"
+        ],
         babelrc: false
       })]
   }))
@@ -36,17 +56,35 @@ module.exports = Promise.resolve()
 
   // 3. massage code a little
   .then(bundled => {
-    return `(function() {
+  
+var noDeps = `(function() {
   var GLOBAL = typeof window !== "undefined" ? window :
       typeof global!=="undefined" ? global :
         typeof self!=="undefined" ? self : this;
   ${bundled.code}
   if (typeof module !== "undefined" && module.exports) module.exports = GLOBAL.lively.vm;
 })();`;
+
+var complete = [
+ "babel-regenerator",
+ "lively.lang",
+ "lively.notifications",
+ "lively.ast",
+ "lively.classes",
+ "lively.source-transform",
+].map(key => {
+  return `
+// INLINED ${parts[key].path}
+${parts[key].source}
+// INLINED END ${parts[key].path}`; }).join("\n") + "\n" + noDeps;
+
+return {noDeps: noDeps, complete: complete};
+
   })
 
   // 4. inject dependencies
-  .then(source => {
-    fs.writeFileSync(targetFile, source);
+  .then(sources => {
+    fs.writeFileSync(targetFile, sources.noDeps);
+    fs.writeFileSync(targetFile2, sources.complete);
   })
   .catch(err => { console.error(err.stack || err); throw err; })
