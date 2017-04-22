fd5aaa4e0abcdc7e45d3a6550b03c9f94a557008
Robert Krahn <robert.krahn@gmail.com>
2017-04-21 23:13:09 -0700

    use the "default" L2LClient, don't create one automatically.  For non-browser envs

--- interfaces/l2l-interface.js
+++ interfaces/l2l-interface.js
@@ -13,8 +13,13 @@ export class L2LCoreInterface extends RemoteCoreInterface {
 
   async runEval(source, options) {
     let {default: L2LClient} = await lively.modules.module("lively.2lively/client.js").load(),
-        l2lClient = L2LClient.forLivelyInBrowser(),
-        l2lEval = new L2LEvalStrategy(l2lClient, this.targetId);
+        l2lClient = L2LClient.default();
+
+    if (!l2lClient) {
+      throw new Error("No lively.2lively default client available!");
+    }
+
+    let l2lEval = new L2LEvalStrategy(l2lClient, this.targetId);
     return l2lEval.runEval(source, options);
   }
 
