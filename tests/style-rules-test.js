a04368226ee016b71e965b728b415b572b23e74a
Robin Schreiber <robin.schreiber@me.com>
2017-05-02 13:03:49 -0700

    add style sheets tests

--- tests/style-rules-test.js
+++ tests/style-rules-test.js
@@ -2,7 +2,7 @@
 import { StyleSheet } from "../style-rules.js";
 import { Morph, HorizontalLayout } from "../index.js";
 import { expect } from "mocha-es6";
-import { pt, Color, Rectangle } from "lively.graphics";
+import { pt, rect, Color, Rectangle } from "lively.graphics";
 import { Sizzle } from "../sizzle.js";
 
 describe("Sizzle", () => {
@@ -13,83 +13,179 @@ describe("Sizzle", () => {
 });
 
 describe("Style Rules", () => {
+  it("applies a style to a morph", () => {
+    const m1 = new Morph({
+      styleClasses: ["root"],
+      submorphs: [
+        {
+          styleClasses: ["bob"],
+          submorphs: [
+            {
+              name: "A",
+              styleClasses: ["alice"]
+            }
+          ]
+        }
+      ]
+    }),
+    m2 = m1.get('A');
+    m1.styleSheets = new StyleSheet({
+      ".root": {fill: Color.orange},
+      ".bob": {fill: Color.red},
+      ".alice": {fill: Color.yellow}
+    });
+    expect(m1._styleSheetsInScope).equals(m1.styleSheets);
+    expect(m1.fill).equals(Color.orange);
+    expect(m1._styleSheetProps.fill).not.undefined;
+    expect(m2.fill).equals(Color.yellow);
+  });
 
-   it("applies a style to a morph", () => {
-       const m1 = new Morph({styleClasses: ['root']});
-       m1.styleSheets = new StyleSheet({'.root': {fill: Color.orange}})
-       expect(m1._styleSheets).equals(m1.styleSheets)
-       expect(m1.fill).equals(Color.orange);
-       expect(m1._styleSheetProps.fill).not.undefined
-   });
+  it("applies a style once a morph is added to the hierarchy", () => {
+    const m1 = new Morph({styleClasses: ["root"]}), m2 = new Morph({styleClasses: ["child"]});
+    m1.styleSheets = new StyleSheet({
+      ".root": {fill: Color.orange},
+      ".child": {fill: Color.green}
+    });
+    m1.addMorph(m2);
+    expect(m2.fill).equals(Color.green);
+    m2.submorphs = [
+      {
+        styleClasses: ["child"],
+        submorphs: [{styleClasses: ["child"]}]
+      },
+      {styleClasses: ["child"]},
+      {styleClasses: ["root"]}
+    ];
+    expect(m2.submorphs.map(m => m.fill)).equals([Color.green, Color.green, Color.orange]);
+    expect(m2.submorphs[0].submorphs[0].fill).equals(Color.green);
+  });
 
-   it("applies a style once a morph is added to the hierarchy", () => {
-       const m1 = new Morph({styleClasses: ['root']}),
-             m2 = new Morph({styleClasses: ['child']})
-       m1.styleSheets = new StyleSheet({'.root': {fill: Color.orange}, 
-                                       '.child': {fill: Color.green}})
-       m1.addMorph(m2);
-       expect(m2.fill).equals(Color.green);
-       m2.submorphs = [{styleClasses: ['child'], 
-                        submorphs: [{styleClasses: ['child']}]}, 
-                       {styleClasses: ['child']},  {styleClasses: ['root']}]
-       expect(m2.submorphs.map(m => m.fill)).equals([Color.green, Color.green, Color.orange]);
-       expect(m2.submorphs[0].submorphs[0].fill).equals(Color.green);
-   });
+  it("updates the style once a morph changes morphClass", () => {
+    const m1 = new Morph({styleClasses: ["root"]}), m2 = new Morph({styleClasses: ["child"]});
+    m1.styleSheets = new StyleSheet({
+      ".root": {fill: Color.orange},
+      ".child": {fill: Color.green}
+    });
+    m1.addMorph(m2);
+    m1.styleClasses = ["child"];
+    m2.styleClasses = ["root"];
+    expect(m1.fill).equals(Color.green);
+    expect(m2.fill).equals(Color.orange);
+  });
 
-   it("updates the style once a morph changes morphClass", () => {
-       const m1 = new Morph({styleClasses: ['root']}),
-             m2 = new Morph({styleClasses: ['child']})
-       m1.styleSheets = new StyleSheet({'.root': {fill: Color.orange}, 
-                                       '.child': {fill: Color.green}})
-       m1.addMorph(m2);
-       m1.styleClasses = ['child'];
-       m2.styleClasses = ['root']
-       expect(m1.fill).equals(Color.green);
-       expect(m2.fill).equals(Color.orange);
-   });
+  it("updates the style once a morph changes name", () => {
+    const m1 = new Morph({styleClasses: ["root"]}), m2 = new Morph({styleClasses: ["child"]});
+    m1.styleSheets = new StyleSheet({
+      "[name=root]": {fill: Color.orange},
+      "[name=child]": {fill: Color.green}
+    });
+    m1.addMorph(m2);
+    m1.name = "child";
+    m2.name = "root";
+    expect(m1.fill).equals(Color.green);
+    expect(m2.fill).equals(Color.orange);
+  });
 
-   it("updates the style once a morph changes name", () => {
-       const m1 = new Morph({styleClasses: ['root']}),
-             m2 = new Morph({styleClasses: ['child']})
-       m1.styleSheets = new StyleSheet({'[name=root]': {fill: Color.orange}, 
-                                        '[name=child]': {fill: Color.green}})
-       m1.addMorph(m2);
-       m1.name = 'child';
-       m2.name = 'root';
-       expect(m1.fill).equals(Color.green);
-       expect(m2.fill).equals(Color.orange);
-   });
+  it("updates the style once a morph is added", () => {
+    const m0 = new Morph(),
+          m1 = new Morph({styleClasses: ["root"]}),
+          m2 = new Morph({styleClasses: ["child"]}),
+          m3 = new Morph({styleClasses: ["leaf"]});
+    m0.styleSheets = new StyleSheet({
+      ".root": {fill: Color.orange},
+      ".child": {fill: Color.brown},
+      ".leaf": {fill: Color.green}
+    });
+    m1.addMorph(m2);
+    m2.addMorph(m3);
+    expect(m2.fill).not.equals(Color.brown);
+    expect(m3.fill).not.equals(Color.green);
+    m0.addMorph(m1);
+    expect(m1.fill).equals(Color.orange);
+    expect(m2.fill).equals(Color.brown);
+    expect(m3.fill).equals(Color.green);
+  });
 
-   it("can be nested", () => {
-       const m1 = new Morph({name: "m1", styleClasses: ['root']}),
-             m2 = new Morph({name: "m2", styleClasses: ['child']}),
-             m3 = new Morph({name: "m3", styleClasses: ['child']})
-       m2.styleSheets = new StyleSheet({'.child': {fill: Color.black, borderColor: Color.red}})
-       m1.styleSheets = new StyleSheet({'.root': {fill: Color.orange}, 
-                                        '.child': {fill: Color.green}})
-       m2.addMorph(m3)
-       m1.addMorph(m2);
-       expect(m1.fill).equals(Color.orange);
-       expect(m2.fill).equals(Color.black);
-       expect(m3.fill).equals(Color.black);
-       expect(m3.borderColor).equals(Color.red);
-   });
+  it("can be nested", () => {
+    const m1 = new Morph({name: "m1", styleClasses: ["root"]}),
+          m2 = new Morph({name: "m2", styleClasses: ["child"]}),
+          m3 = new Morph({name: "m3", styleClasses: ["child"]});
+    m2.styleSheets = new StyleSheet({".child": {fill: Color.black, borderColor: Color.red}});
+    m1.styleSheets = new StyleSheet({
+      ".root": {fill: Color.orange},
+      ".child": {fill: Color.green}
+    });
+    m2.addMorph(m3);
+    m1.addMorph(m2);
+    expect(m1.fill).equals(Color.orange);
+    expect(m2.fill).equals(Color.black);
+    expect(m3.fill).equals(Color.black);
+    expect(m3.borderColor).equals(Color.red);
+  });
 
-   it("updates layouts on changing submorphs", () => {
-       const m1 = new Morph({name: "m1", styleClasses: ['root']}),
-             m2 = new Morph({name: "m2", styleClasses: ['child']}),
-             m3 = new Morph({name: "m3", styleClasses: ['child']})
-       m1.styleSheets = new StyleSheet({'.root': {layout: new HorizontalLayout({resizeContainer: true})}, 
-                                        '.child': {fill: Color.green},
-                                        '.new': {extent: pt(100, 0)}})
-       m1.addMorph(m3)
-       m1.addMorph(m2);
-       expect(m1.width).equals(m2.width + m3.width)
-       let l = m1.layout;
-       m2.styleClasses = ['new'];
-       expect(l).to.not.equals(m1.layout);
-       expect(m2.width).equals(100);
-       expect(m1.width).equals(100 + m3.width)
-   });
-   
+  it("updates layouts on changing submorphs", () => {
+    const m1 = new Morph({name: "m1", styleClasses: ["root"]}),
+          m2 = new Morph({name: "m2", styleClasses: ["child"]}),
+          m3 = new Morph({name: "m3", styleClasses: ["child"]});
+    m1.styleSheets = new StyleSheet({
+      ".root": {layout: new HorizontalLayout({resizeContainer: true})},
+      ".child": {fill: Color.green},
+      ".new": {extent: pt(100, 0)}
+    });
+    m1.addMorph(m3);
+    m1.addMorph(m2);
+    expect(m1.width).equals(m2.width + m3.width);
+    let l = m1.layout;
+    m2.styleClasses = ["new"];
+    expect(l).to.not.equals(m1.layout);
+    expect(m2.width).equals(100);
+    expect(m1.width).equals(100 + m3.width);
+  });
+
+  it("removes morphs from index when they are removed from the indexed context", () => {
+    const m = new Morph({
+      styleClasses: ["root", "child"],
+      submorphs: [{name: "bob", styleClasses: ["leaf"]}],
+      styleSheets: [
+        new StyleSheet({
+          ".leaf": {fill: Color.green}
+        })
+      ]
+    }),
+          sizzle = m.styleSheets[0].sizzle,
+          bob = m.get("bob");
+    expect(bob.fill).to.equal(Color.green);
+    expect(bob.id in sizzle.cachedExpressions[".leaf"].morphIndex).to.be
+      .true;
+    bob.remove();
+    expect(bob.id in sizzle.cachedExpressions[".leaf"].morphIndex).to.be
+      .false;
+  });
+
+  it('results in the same transform behaviors as if morphs where directly set', () => {
+    let hierarchy = new Morph({
+      styleSheets: [
+        new StyleSheet({
+          ".root": {position: pt(20, 20)},
+          ".bob": {position: pt(40, 40)},
+          ".alice": {extent: pt(30, 30)}
+        })
+      ],
+      styleClasses: ["root"],
+      submorphs: [
+        {
+          styleClasses: ["bob"],
+          submorphs: [
+            {
+              name: "A",
+              styleClasses: ["alice"]
+            }
+          ]
+        }
+      ]
+    });
+      expect(hierarchy.styleSheets[0].sizzle.context).equals(hierarchy);
+      expect(hierarchy.get('A').extent).equals(pt(30,30));
+      expect(hierarchy.get('A').globalBounds()).equals(rect(60,60,30,30))
+  })
 });
