5cab48b87d32a26d0122e40794f543ea1f2a9664
Robert Krahn <robert.krahn@gmail.com>
2017-05-15 17:17:27 -0700

    fixing tests, pre-script

--- tests/node-pre.js
+++ tests/node-pre.js
@@ -1,13 +1,5 @@
 // pre-loading the dependencies here so we don't have to configure SystemJS
 // with all the sub-sub-sub packages
 
-function requireFromPackage(moduleName, pkgName) {
-var path = require("path"),
-    module = require("module"),
-    lookupPaths = [path.join(require.resolve(pkgName), "../node_modules")],
-    id = module._resolveFilename(moduleName, {paths: lookupPaths});
-  return require(id);
-}
-
-requireFromPackage("socket.io", "lively.server");
+require("lively.server");
 require("socket.io-client");
