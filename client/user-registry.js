2bbe786b1f3ed88bf05813fab20892af96ea3b6b
Robert Krahn <robert.krahn@gmail.com>
2017-07-14 02:40:21 -0700

    user  registry

---
+++ client/user-registry.js
@@ -0,0 +1,71 @@
+import { User, GuestUser } from "./user.js";
+
+export default class UserRegistry {
+
+  static get current() {
+    return this._current || (this._current = new this());
+  }
+
+  hasUserStored() {
+    try {
+      return !!localStorage["lively.user"] || !!sessionStorage["lively.user"];
+    } catch (err) { return false; }
+  }
+
+  async login(user, password) {
+    if (!user.isGuestUser) {
+      let {error} = await user.login(password);
+      if (error) throw Error(error);
+    }
+    this.saveUserToLocalStorage(user);
+    return user;
+  }
+
+  async logout(user) {
+    try {
+      delete localStorage["lively.user"];
+      delete sessionStorage["lively.user"];
+    } catch (err) {}
+    return user && user.isGuestUser ? user : User.guest;
+  }
+
+  async register(user, password) {
+    if (!user || user.isGuestUser)
+      throw new Error("guest users cannot register");
+    let {error} = await user.register(password);
+    if (error) throw Error(error);
+    this.saveUserToLocalStorage(user);
+    return user;
+  }
+
+  loadUserFromLocalStorage(authServerURL) {
+    try {
+      let stored = localStorage["lively.user"] || sessionStorage["lively.user"];
+      if (stored) {
+        stored = JSON.parse(stored);
+        return stored.isGuest
+          ? GuestUser.named(stored.name, null)
+          : User.fromToken(stored.token, authServerURL);
+      }
+    } catch (err) {
+      console.warn(`Could not read user from localStorage: ${err}`);
+    }
+    return GuestUser.guest;
+  }
+
+  saveUserToLocalStorage(user) {
+    if (!user || (!user.isGuestUser && !user.token)) return false;
+    
+    try {
+      if (user.isGuestUser) {
+        sessionStorage["lively.user"] = JSON.stringify({isGuest: true, name: user.name});
+      } else {
+        localStorage["lively.user"] = JSON.stringify({token: user.token});
+      }
+      return true;
+    } catch (err) {
+      console.warn(`Could not save user into local/sessionStorage: ${err}`);
+      return false;
+    }
+  }
+}
