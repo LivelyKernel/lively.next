b59bf6a1b81b06460a9d3d9a2a88b4ac7eb5bc91
Robert Krahn <robert.krahn@gmail.com>
2017-07-17 11:13:50 -0700

    morphic DB

---
+++ objectdb.js
@@ -0,0 +1,28 @@
+/*global System*/
+import ObjectDB from "lively.objectdb";
+import { loadMorphFromSnapshot, createMorphSnapshot } from "./serialization.js";
+import { resource } from "lively.resources";
+
+let snapshotLocation = resource(System.normalizeSync("lively.morphic/objectdb/snapshots/"));
+
+export default class MorphicDB extends ObjectDB {
+
+  static get default() {
+    return this.named("default-morphic-db", {snapshotLocation});
+  }
+
+  async snapshotObject(type, name, object, snapshotOptions, commitSpec, ref, expectedPrevVersion) {    
+    snapshotOptions = snapshotOptions || {};
+    snapshotOptions.serializeFn = createMorphSnapshot;
+    return super.snapshotObject(
+      type, name, object, snapshotOptions,
+      commitSpec, ref, expectedPrevVersion);
+  }
+
+  async loadObject(type, name, loadOptions, commitIdOrCommit, ref) {
+    loadOptions = loadOptions || {}
+    loadOptions.deserializeFn = loadMorphFromSnapshot;
+    return super.loadObject(type, name, loadOptions, commitIdOrCommit, ref);
+  }
+
+}
