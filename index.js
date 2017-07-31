636139602f17cec5661fba35fb24c275027d3e15
Robin Schreiber <robin.schreiber@me.com>
2017-07-31 14:46:38 +0200

    add halo commands on import

---
+++ index.js
@@ -0,0 +1,38 @@
+import worldCommands from 'lively.morphic/world-commands.js';
+import { commands as morphCommands } from 'lively.morphic/morph.js';
+import Halo from "./morph.js";
+import { StatusMessage, StatusMessageForMorph, show } from "./markers.js";
+import { arr } from "lively.lang";
+
+export const haloCommands = [
+  {
+    name: 'open halo',
+    exec: (_, args) => {
+      return new Halo(args);
+    }
+  },
+   {
+    name: 'create status message',
+    exec: (world, args) => {
+      return new StatusMessage(args)
+    }
+  },
+  {
+    name: 'create status message for morph',
+    exec: (world, args) => {
+      return new StatusMessageForMorph(args)
+    }
+  },
+  {
+    name: 'show marker',
+    exec: (_, obj) => {
+       show(obj)
+    }
+  }
+];
+
+morphCommands.push(...arr.filter(haloCommands, haloCmd =>
+  !morphCommands.find(morphCmd => morphCmd.name == haloCmd.name)))
+
+worldCommands.push(...arr.filter(haloCommands, haloCmd =>
+  !worldCommands.find(worldCmd => worldCmd.name == haloCmd.name)))
