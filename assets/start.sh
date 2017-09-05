57020cb3f33031b75e051e5bd158ca46915406e2
Robert Krahn <robert.krahn@gmail.com>
2017-09-05 06:52:53 +0000

    add custom-npm-modules to flatn to support npm install tooling

--- assets/start.sh
+++ assets/start.sh
@@ -11,7 +11,7 @@ lv_next_dir=$PWD
 
 export PATH=$lv_next_dir/flatn/bin:$PATH
 export FLATN_PACKAGE_DIRS=
-export FLATN_PACKAGE_COLLECTION_DIRS=$lv_next_dir/lively.next-node_modules
+export FLATN_PACKAGE_COLLECTION_DIRS=$lv_next_dir/lively.next-node_modules:$lv_next_dir/custom-npm-modules
 eval $(node -p 'let PWD=process.cwd();let packages = JSON.parse(require("fs").readFileSync(PWD+"/lively.installer/packages-config.json")).map(ea => require("path").join(PWD, ea.name));`export FLATN_DEV_PACKAGE_DIRS=${packages.join(":")}`')
 
 cd lively.server;
