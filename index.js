35d595396191156db79be3837b28f90537fa47cc
Robin Schreiber <robin.schreiber@me.com>
2017-10-15 20:45:26 +0200

    fix index.js exports

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ export * from './menus.js';
 export * from './resizers.js';
 export * from './tree.js';
 export * from './iframe.js';
-export * from './window.js';
+import Window from './window.js';
 
 import * as prompts from './prompts';
 import * as widgets from './widgets.js';
 import LoadingIndicator from './loading-indicator.js';
-export { LoadingIndicator, prompts, widgets };
+export { Window, LoadingIndicator, prompts, widgets };
