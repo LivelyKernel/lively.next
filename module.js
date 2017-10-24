5f5034b49f0d3bf5bd3224732d5936ca70b60a12
Robert Krahn <robert.krahn@gmail.com>
2017-10-23 23:54:52 -0700

    use register format

--- module.js
+++ module.js
@@ -3,7 +3,13 @@ import { isURL } from "lively.modules/src/url-helpers.js";
 import { join, parent } from "lively.resources/src/helpers.js";
 import { parse, stringify, transform, nodes, query } from "lively.ast";
 import { findUniqJsName } from "./util.js";
-import { arr } from "lively.lang";
+import { arr, string } from "lively.lang";
+
+function exportCall(exportName, id) {
+  return stringify(
+    nodes.exprStmt(
+      nodes.funcCall(nodes.id("_export"), nodes.literal(exportName), id)))
+}
 
 export default class FreezerModule {
 
@@ -265,6 +271,126 @@ export default class FreezerModule {
          + `${exportGetters.map(stringify).join("\n")}`
          + `${replaced.source}\n`
          + `\n}`;
+  }
+
+
+
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  registerTranformsOfImports() {
+    let declsForImports = [], setters = [], qualifiedDependencyNames = [];
+    for (let [depModule, {imports}] of this.dependencies) {
+      // FIXME what if a toplevel var of `this` has the same name as the generated varName?
+      let varName = depModule.findUniqJsName([]),
+          setterStmts = [];
+      qualifiedDependencyNames.push(depModule.qualifiedName);
+      for (let i of imports) {
+        setterStmts.push(nodes.exprStmt(nodes.assign(i.local, nodes.member(nodes.id(varName), i.imported))));
+        declsForImports.push(nodes.varDecl(i.local));
+      }
+      setters.push(nodes.funcExpr({}, [nodes.id(varName)], ...setterStmts))
+    }
+    return {declsForImports, setters, qualifiedDependencyNames};
+  }
+
+  registerTranformsOfExports() {
+    let exported = [];
+
+    for (let node of this.parsed.body) {
+      let {type, declaration, specifiers} = node;
+      if (type !== "ExportNamedDeclaration" && type !== "ExportDefaultDeclaration")
+        continue;
+
+      let exportTransform = {node, replacementFunc: () => [], ids: []};
+      exported.push(exportTransform);
+
+      if (type === "ExportNamedDeclaration" && declaration) {
+
+        switch (declaration.type) {
+
+          case 'VariableDeclaration':
+            exportTransform.replacementFunc = (node, source, wasChanged) => {
+              source = source.replace(/^(\s*)export\s+/, "");
+              let ids = query.helpers.declIds(node.declaration.declarations.map(ea => ea.id)),
+                  exports = ids.map(id => exportCall(id.name, id)).join("\n")                  
+              return source + "\n" + exports;
+            }
+            for (let id of query.helpers.declIds(declaration.declarations.map(ea => ea.id)))
+              exportTransform.ids.push({local: id, exported: id});
+            break;
+
+          case 'ClassDeclaration': case 'FunctionDeclaration':
+            exportTransform.replacementFunc = (node, source, wasChanged) =>
+                source.replace(/^(\s*)export\s+/, "") + "\n"
+              + exportCall(node.declaration.id.name, node.declaration.id)
+            exportTransform.ids.push({local: declaration.id, exported: declaration.id});
+            break;
+        }
+        continue;
+      }
+
+      if (type === "ExportNamedDeclaration" && specifiers) {
+        for (let {local, exported} of specifiers)
+          exportTransform.ids.push({local, exported});
+        continue;
+      }
+
+      if (type === "ExportDefaultDeclaration" && declaration) {
+        let local;
+        switch (declaration.type) {
+
+          case 'Identifier':
+            exportTransform.replacementFunc = (node, source, wasChanged) =>
+                source.replace(/^(\s*)export\s+default\s+/, "") + "\n"
+              + exportCall("default", node)
+            local = declaration;
+            break;
+
+          case 'ClassDeclaration': case 'FunctionDeclaration':
+            exportTransform.replacementFunc = (node, source, wasChanged) =>
+                source.replace(/^(\s*)export\s+default\s+/, "")
+              + exportCall("default", node.id)
+            local = declaration.id;
+            break;
+
+          default:
+            throw new Error(`Strange default export declaration: ${declaration.type}`);
+        }
+
+        exportTransform.ids.push({local, exported: {type: "Identifier", name: "default"}});
+        continue;
+      }
+    }
+
+    return exported;
+  }
+  
+  transformToRegisterFormat() {
+    let {declsForImports, setters, qualifiedDependencyNames} = this.registerTranformsOfImports(),
+        exportTransformData = this.registerTranformsOfExports(),
+        replaced = transform.replaceNodes([
+          // remove import decls completely
+          ...this.parsed.body.filter(ea => ea.type === "ImportDeclaration")
+                                .map(target => ({target, replacementFunc: () => []})),
+
+          // // remove exports
+          ...exportTransformData.map(({node: target, replacementFunc}) => ({target, replacementFunc}))
+        ], this._source);
+
+    return `System.register("${this.qualifiedName}", `
+         + `[${qualifiedDependencyNames.map(ea => `"${ea}"`).join(", ")}], `
+         + `function(_export, _context) {\n`
+         + `  "use strict";\n`
+         + `  ${declsForImports.map(stringify)}\n`
+         + `  return {\n`
+         + `    setters: [\n`
+         + `${string.indent(setters.map(stringify).join(",\n"), "  ", 3)}\n`
+         + `    ],\n`
+         + `    execute: function() {\n`
+         + `${string.indent(replaced.source, "  ", 3)}\n`
+         + `    }\n`
+         + `  }\n`
+         + `});`
   }
 
 }
--- tests/bundle-test.js
+++ tests/bundle-test.js
@@ -1,4 +1,4 @@
-/*global declare, it, describe, beforeEach, afterEach, before, after,System*/
+/*global declare, it, describe, beforeEach, afterEach, before, after,System,xdescribe*/
 import { expect } from "mocha-es6";
 import { createFiles, resource } from "lively.resources";
 import FreezerPackage from "../package.js";
@@ -15,51 +15,42 @@ function buildPackage1() {
   });
 }
 
-async function createFiles(baseDir, fileSpec, opts) {
-    let base = resource(baseDir, opts).asDirectory();
-    await base.ensureExistance();
-    for (let name in fileSpec) {
-      if (!fileSpec.hasOwnProperty(name))
-        continue;
-      let resource = base.join(name);
-      typeof fileSpec[name] === "object" ? await createFiles(resource, fileSpec[name], opts) : await resource.write(fileSpec[name]);
-    }
-    return base;
-  }
+let baseDir = resource("local://freezer-tests/"), packages;
 
-let baseDir = resource("local://freezer-tests/");
-
-describe("freezer bundle", function () {
+describe("bundler", function () {
 
   this.timeout(6000);
+
+  beforeEach(async () => {
+    await buildPackage1();
+    packages = await FreezerPackage.buildPackageMap([baseDir.join("package1/")]);
+  });
 
   afterEach(async () => {
     await baseDir.remove();
   });
 
-  it("reads dependencies of simple package", async () => {
-    await buildPackage1();
-    let packages = await FreezerPackage.buildPackageMap([baseDir.join("package1/")]),
-        bundle = new Bundle(packages);
-    await bundle.build("file1.js", "package1");
+  describe("resolves modules", () => {
 
-    expect(bundle.report().trim()).equals([
-      `package1@1/file1.js`,
-      `  => y`,
-      `  <= package1@1/file2.js x`,
-      ``,
-      `package1@1/file2.js`,
-      `  => x`
-    ].join("\n"));
+    it("reads dependencies of simple package", async () => {
+      let bundle = new Bundle(packages);
+      await bundle.resolveDependenciesStartFrom("file1.js", "package1");
+      expect(bundle.report().trim()).equals([
+        `package1@1/file1.js`,
+        `  => y`,
+        `  <= package1@1/file2.js x`,
+        ``,
+        `package1@1/file2.js`,
+        `  => x`
+      ].join("\n"));
+    });
   });
 
-  describe("source transform", () => {
+  describe("module transforms", () => {
 
-    it("transforms module into function", async () => {
-      await buildPackage1();
-      let packages = await FreezerPackage.buildPackageMap([baseDir.join("package1/")]),
-          bundle = new Bundle(packages);
-      await bundle.build("file1.js", "package1");
+    it("into function", async () => {
+      let bundle = new Bundle(packages);
+      await bundle.resolveDependenciesStartFrom("file1.js", "package1");
 
       expect(bundle.entryModule.transformToModuleFunction()).equals(
 `function package1_1_file1_js(__imports__, __exports__) {
@@ -67,6 +58,44 @@ __exports__.__defineGetter__("y", () => y); var y = package1_1_file2_js.x + 2;
 
 }`);
 
-    })
+      expect(bundle.entryModule.transformToRegisterFormat()).equals(
+`System.register("package1@1/file1.js", ["package1@1/file2.js"], function(_export, _context) {
+  "use strict";
+  var x;
+  return {
+    setters: [
+      function (package1_1_file2_js) {
+        x = package1_1_file2_js.x;
+      }
+    ],
+    execute: function() {
+       var y = x + 2;
+      _export("y", y);
+    }
+  }
+});`);
+    });
+
   });
+
+  xdescribe("bundle", () => {
+
+    it("produces file spec for bundle", async () => {
+      let bundle = new Bundle(packages);
+      await bundle.resolveDependenciesStartFrom("file1.js", "package1");
+      let source = bundle.bundleToSource({bundleName: "package1"});
+      expect(source).equals(`
+
+;(function() {
+var G = typeof window !== "undefined" ? window :
+    typeof global!=="undefined" ? global :
+      typeof self!=="undefined" ? self : this;
+if (typeof G.lively !== "object") G.lively = {};
+if (!G.lively.bundled) G.lively.bundled = {};
+G.lively.bundled["package1"] = {}
+})();
+`)
+    });
+  });
+
 });
