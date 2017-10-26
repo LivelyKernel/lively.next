744a3eaea0bed74151961318c2c2caef09ad3fb9
Robert Krahn <robert.krahn@gmail.com>
2017-10-26 13:56:39 -0700

    tests

--- tests/bundle-test.js
+++ tests/bundle-test.js
@@ -4,18 +4,49 @@ import { createFiles, resource } from "lively.resources";
 import FreezerPackage from "../package.js";
 import Bundle from "../bundle.js";
 
-
 function buildPackage1() {
   return createFiles(baseDir, {
     "package1": {
       "package.json": `{"name": "package1", "version": "1"}`,
-      "file1.js": "import { x } from './file2.js'; export var y = x + 2;",
-      "file2.js": "export var x = 23;"
+      "file1.js": "import { x } from './file2.js'; export var y = x + 2; export { z } from './file3.js'",
+      "file2.js": "export var x = 23;",
+      "file3.js": "export var z = 99;",
     }
   });
 }
 
 let baseDir = resource("local://freezer-tests/"), packages;
+
+let expectedPackage1File1Code = `System.register("package1@1/file1.js", ["package1@1/file2.js", "package1@1/file3.js"], function(_export, _context) {
+  "use strict";
+  var x, z;
+  return {
+    setters: [
+      function (package1_1_file2_js) {
+        x = package1_1_file2_js.x;
+      },
+      function (package1_1_file3_js) {
+        z = package1_1_file3_js.z;
+      }
+    ],
+    execute: function() {
+      var y = x + 2;
+      _export("y", y);
+      _export("z", z);
+    }
+  }
+});`
+
+let expectedPackage1File2Code = `System.register("package1@1/file2.js", [], function(_export, _context) {
+  "use strict";
+  return {
+    setters: [],
+    execute: function() {
+      var x = 23;
+      _export("x", x);
+    }
+  }
+});`;
 
 describe("bundler", function () {
 
@@ -23,7 +54,7 @@ describe("bundler", function () {
 
   beforeEach(async () => {
     await buildPackage1();
-    packages = await FreezerPackage.buildPackageMap([baseDir.join("package1/")]);
+    packages = await FreezerPackage.buildPackageMap({package1: {path: baseDir.join("package1/")}});
   });
 
   afterEach(async () => {
@@ -37,11 +68,15 @@ describe("bundler", function () {
       await bundle.resolveDependenciesStartFrom("file1.js", "package1");
       expect(bundle.report().trim()).equals([
         `package1@1/file1.js`,
-        `  => y`,
+        `  => y, z`,
         `  <= package1@1/file2.js x`,
+        `  <= package1@1/file3.js z`,
         ``,
         `package1@1/file2.js`,
-        `  => x`
+        `  => x`,
+        ``,
+        `package1@1/file3.js`,
+        `  => z`
       ].join("\n"));
     });
   });
@@ -51,44 +86,23 @@ describe("bundler", function () {
     it("into function", async () => {
       let bundle = new Bundle(packages);
       await bundle.resolveDependenciesStartFrom("file1.js", "package1");
-
-      expect(bundle.entryModule.transformToRegisterFormat()).equals(
-`System.register("package1@1/file1.js", ["package1@1/file2.js"], function(_export, _context) {
-  "use strict";
-  var x;
-  return {
-    setters: [
-      function (package1_1_file2_js) {
-        x = package1_1_file2_js.x;
-      }
-    ],
-    execute: function() {
-       var y = x + 2;
-      _export("y", y);
-    }
-  }
-});`);
+      expect(bundle.entryModule.transformToRegisterFormat()).equals(expectedPackage1File1Code);
     });
 
   });
 
-  xdescribe("bundle", () => {
+  describe("bundle execution", () => {
 
-    it("produces file spec for bundle", async () => {
+    afterEach(() => delete lively.FreezerRuntime);
+
+    it("standalone", async () => {
       let bundle = new Bundle(packages);
       await bundle.resolveDependenciesStartFrom("file1.js", "package1");
-      let source = bundle.bundleToSource({bundleName: "package1"});
-      expect(source).equals(`
+      let source = bundle.standalone();
+      eval(source);
 
-;(function() {
-var G = typeof window !== "undefined" ? window :
-    typeof global!=="undefined" ? global :
-      typeof self!=="undefined" ? self : this;
-if (typeof G.lively !== "object") G.lively = {};
-if (!G.lively.bundled) G.lively.bundled = {};
-G.lively.bundled["package1"] = {}
-})();
-`)
+      expect(lively.FreezerRuntime.get("package1@1/file1.js").exports)
+        .deep.equals({y: 25,z: 99});
     });
   });
 
