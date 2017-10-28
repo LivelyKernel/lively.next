d9f44e5f3556f5504dc2364e4047c94bff7cfcf5
Robert Krahn <robert.krahn@gmail.com>
2017-10-27 17:24:57 -0700

    fix prompt import/export

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export * from './tree.js';
 export * from './iframe.js';
 import Window from './window.js';
 
-import * as prompts from './prompts';
+import * as prompts from './prompts.js';
 import * as widgets from './widgets.js';
 import LoadingIndicator from './loading-indicator.js';
 export { Window, LoadingIndicator, prompts, widgets };
