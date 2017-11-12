d46aa86089155aa0a55ebb5d3de03bdbe4032464
Robert Krahn <robert.krahn@gmail.com>
2017-11-12 02:38:15 +0000

    world loading: open graph meta tags + dynamic world preview images

--- plugins/world-loading.js
+++ plugins/world-loading.js
@@ -1,6 +1,11 @@
-/*global System*/
+/*global System,Buffer*/
 import LivelyServer from "../server.js";
 import { resource } from "lively.resources";
+import { ObjectDB } from "lively.storage";
+import { parse as parseUrl } from "url";
+
+const minute = 1000*60;
+const useCache = true;
 
 export default class WorldLoadingPlugin {
 
@@ -24,6 +29,25 @@ export default class WorldLoadingPlugin {
     return htmlResource.read()
   }
 
+  addMeta(html, worldName, parsedUrl) {
+    let encodedName = encodeURIComponent(worldName),
+        tags = [
+          {property: "og:title", content: `lively.next - ${worldName}`},
+          {property: "og:description", content: "lively.next is a personal programming kit. It emphasizes liveness, directness and interactivity."},
+          {property: "og:image", content: `https://lively-next.org/object-preview-by-name/world/${encodedName}`},
+          {property: "og:url", content: `https://lively-next.org/worlds/${encodedName}`},
+          // {name: "twitter:card", content: "summary_large_image"},
+        ], tagMarkup = [];
+
+    for (let spec of tags) {
+      let markup = "<meta"
+      for (let prop in spec) markup += ` ${prop}="${spec[prop]}"`;
+      markup += ">";
+      tagMarkup.push(markup);
+    }
+    return html.replace("<!--META-->", tagMarkup.join("\n"));
+  }
+
   async handleRequest(req, res, next) {
     let [url, query] = req.url.split("?");
     query = query ? "?" + query : "";
@@ -34,16 +58,81 @@ export default class WorldLoadingPlugin {
       return;
     }
 
-    if (!url.startsWith("/worlds")) return next();
+    if (url.startsWith("/object-preview-by-name/")) {
+      let [_, type, name] = url.match(/^\/object-preview-by-name\/([^\/]+)\/([^\/]+)$/) || [],
+          err;
+      if (!type || !name) {
+        err = {code: 404, message: `Cannot find thumbnail for ${url.split("/").slice(2).join("/")}`};
+      } else {
+        name = decodeURIComponent(name);
+        
+        {
+          // FIXME, support multiple DBs
+          // FIXME, move somewhere else
+          try {
+            let db = await ObjectDB.find("lively.morphic/objectdb/morphicdb"),
+                previewLoc = db.snapshotLocation.join(`../preview-cache/by-name/${type}/`).withRelativePartsResolved(),
+                defaultImageFile = previewLoc.join(`${name}.png`).beBinary();
+            
+            // exists and is recent?
+            if (await defaultImageFile.exists()) {
+              if (Date.now() - (await defaultImageFile.readProperties()).lastModified < minute*10) {
+                let buf = await defaultImageFile.read();
+                res.writeHead(200); res.end(buf);
+                return;
+              } else {
+                await defaultImageFile.remove();
+              }
+            }
 
-    let htmlFile = url === "/worlds" || url === "/worlds/" ?
-          "static-world-listing.html" : "morphic.html";
-    if (!this.cachedHTML[htmlFile]) {
-      this.cachedHTML[htmlFile] = await this.readFile(htmlFile);
+            // otherwise read from snapshot.preview
+            let commit = await db.getLatestCommit(type, name),
+                snapshotResource = db.snapshotResourceFor(commit),
+                {preview} = await snapshotResource.readJson(),
+                [_, ext, data] = preview.match(/^data:image\/([A-Za-z-+\/]+);base64,(.+)$/) || [],
+                imgFile = previewLoc.join(`${name}.${ext}`).beBinary(),
+                buf = new Buffer(data, 'base64');
+
+            // answer...
+            res.writeHead(200); res.end(buf);
+
+            // ...and cache
+            Promise.resolve().then(async () => {
+              await previewLoc.ensureExistance();
+              await imgFile.write(buf);
+            }).catch(err => console.error(`Error writing cached preview image for ${type}/${name}:\n${err}`));
+            return;
+
+          } catch (e) {
+            err = {code: 500, message: String(e)};
+          }
+        }
+
+        if (err) {
+          res.writeHead(err.code,  {});
+          res.end(err.message);
+          return;
+        }
+      }
+
+    } else if (url.startsWith("/worlds")) {
+
+      let parsedUrl = parseUrl(url, true),
+          [_, worldName] = parsedUrl.pathname.match(/^\/worlds\/?(.*)/),
+          htmlFile = worldName ? "morphic.html" : "static-world-listing.html",
+          html = (useCache && this.cachedHTML[htmlFile]) || (this.cachedHTML[htmlFile] = await this.readFile(htmlFile));
+      if (worldName) {
+        let withMeta = `${htmlFile}-for-${worldName}`;
+        html = (useCache && this.cachedHTML[withMeta]) || (this.cachedHTML[withMeta] = this.addMeta(html, worldName, parsedUrl));
+      }
+
+      res.setHeader('content-type', 'text/html');
+      res.end(html);
+
+    } else {
+      next();
     }
 
-    res.setHeader('content-type', 'text/html');
-    res.end(this.cachedHTML[htmlFile]);
   }
 
 }
