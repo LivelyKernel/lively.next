42b578bcf4cf6c580549cade1820289a730dc7b3
Robin Schreiber <robin.schreiber@me.com>
2018-05-10 15:36:59 +0900

    update buildscript for dependency bundle

--- build.js
+++ build.js
@@ -4,13 +4,20 @@ var livelyLang = require('lively.lang');
 var fs = require('fs');
 var path = require("path");
 
-var res = [
- fs.readFileSync(require.resolve("babel-standalone").replace('babel.js', 'babel.min.js')),
- fs.readFileSync(require.resolve("systemjs").replace("index.js", "/dist/system.src.js")),
- fs.readFileSync(require.resolve("../lively.modules/dist/lively.modules.min.js")),
- fs.readFileSync(require.resolve("../lively.graphics/dist/lively.graphics.js")),
- fs.readFileSync(require.resolve("../lively.bindings/dist/lively.bindings.js")),
- fs.readFileSync(require.resolve("../lively.serializer2/dist/lively.serializer2.js")),
- fs.readFileSync(require.resolve("../lively.storage/dist/lively.storage_with-pouch.js"))].join('\n');
+var res =  [
+uglifyjs.minify(fs.readFileSync(require.resolve("babel-standalone")).toString(), {
+  output: { ascii_only: true },
+}).code,
+uglifyjs.minify(fs.readFileSync(require.resolve("systemjs").replace("index.js", "dist/system.src.js")).toString()).code, 
+"//LIVELY.MODULES",
+fs.readFileSync(require.resolve("lively.modules/dist/lively.modules.min.js")),
+"//LIVELY.GRAPHICS",
+fs.readFileSync(require.resolve("lively.graphics/dist/lively.graphics.min.js")),
+"//LIVELY.BINDINGS",
+fs.readFileSync(require.resolve("lively.bindings/dist/lively.bindings.min.js")),
+"//LIVELY.SERIALIZER",
+fs.readFileSync(require.resolve("lively.serializer2/dist/lively.serializer2.min.js")),
+"//LIVELY.MORPHIC",
+fs.readFileSync(require.resolve("lively.morphic/dist/lively.morphic_no-deps.min.js"))].join('\n');
 
-fs.writeFileSync('./runtime-dependencies.js', res);
+fs.writeFileSync('./runtime-deps.js', res);
