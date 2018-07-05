5d953892189568e94c79002bd0965d8f3b846eee
Robert Krahn <robert.krahn@gmail.com>
2018-07-05 09:35:34 +0000

    add log fetching

--- src/repository.js
+++ src/repository.js
@@ -1,12 +1,113 @@
-import { mixins, promisify } from "../dist/js-git-browser.js"
+import { mixins, promisify, modes } from "../dist/js-git-browser.js"
 import JsGitWebDAVFs from "./js-git-webdav-fs.js";
+import { Database } from "lively.storage";
 
+// await lively.modules.module("https://lively-next.org/js-git-browser/js-git-browser.js").unload()
+
+class FetchLogProcess {
+
+  constructor(repo) {
+    this.repo = repo;
+    this.canceled = false;
+    this.cancelRequested = false;
+    this.running = false;
+    this.error = null;
+    this.logStream = null;
+    this.commits = [];
+    this.whenDone = Promise.resolve(null);
+  }
+
+  stop() { this.cancelRequested = true; };
+  
+  run(fromRef, limit, onUpdate) {
+
+    this.whenDone = this.logWalkWithLocalDb(fromRef, limit, onUpdate)
+      .catch(err => {
+        console.error(`fetchLog error: ${err}`)
+        this.running = false;
+        this.error = err;
+        throw err;
+      });
+
+    return this;
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // We can also loop through all the files of each commit version.
+    // var treeStream = await repo.treeWalk(commit.tree);
+    // while (object = await treeStream.read(), object !== undefined) {
+    //   console.log(object);
+    // }
+  }
+
+  async logWalkWithLocalDb(ref, limit = Infinity, onUpdate) {
+    let seen = {},
+        repo = this.repo,
+        last = await repo.readRef("shallow"),
+        refsToLoad = [ref];
+
+    repo.fetchProcesses.push(this);
+    this.running = true;
+
+    while (true) {
+      if (this.cancelRequested) this.canceled = true;
+      if (this.canceled || !this.running) {
+        repo.fetchProcesses.splice(repo.fetchProcesses.indexOf(this), 1);
+        break;
+      }
+
+      let ref = refsToLoad.shift(),
+          commit = await repo.localDb.get(ref),
+          hash = commit ? commit.hash : null,
+          saveInDb = false;
+
+      if (!commit) {
+        hash = await repo.resolveRef(ref);
+        commit = await repo.loadAs("commit", hash);
+        if (commit) commit.hash = hash;
+        if (hash === last) {
+          commit.last = true;
+          this.running = false;
+        }
+        saveInDb = true;
+      }
+
+      if (!commit) {
+        let err = new Error(`Cannot resolve ref ${ref}`);
+        this.error = err;
+        this.running = false;
+        throw err;
+      }
+
+      seen[hash] = true;    
+      this.commits.push(commit);
+      typeof onUpdate === "function" && onUpdate(this);
+
+      if (saveInDb)
+        await repo.localDb.set(commit.hash, {...commit, type: "commit"});
+
+      refsToLoad.push(...commit.parents.filter(hash => !seen[hash]));
+      if (!refsToLoad.length || this.commits.length > limit)
+        this.running = false;
+    }
+
+    return this;
+  }
+
+
+  toString() {
+    return this.error ?
+      `<commit fetcher errored: ${this.error} >` :
+      `<commit fetcher ${this.running ? `is running` : this.canceled ? "was canceled" : "finished"} found ${this.commits.length} from ${this.fromRef}>`;
+  }
+
+}
 
 export default class WebDAVRepository {
 
   constructor(url) {
     this.rootPath = url;
     this.fs = new JsGitWebDAVFs();
+    this.localDb = Database.ensureDB("lively.git-" + url.replace(/^https?:\/\//, ""));
     mixins.fsDb(this, this.fs);
     mixins.createTree(this);
     mixins.formats(this);
@@ -14,12 +115,41 @@ export default class WebDAVRepository {
     mixins.packOps(this);
     mixins.walkers(this);
     // jsgit.mixins.memCache(repo);
+    this.fetchProcesses = [];
     promisify(this);
   }
 
   get url() { return this.rootPath }
 
-  get master() { return this.readRef("refs/heads/master"); }
+  get master() { return this.resolveRef("refs/heads/master"); }
+
+  async resolveRef(hashish, seen = {}) {
+    if (seen[hashish] || /^[0-9a-f]{40}$/.test(hashish)) return hashish;
+    seen[hashish] = true;
+    let hash = await this.readRef(hashish);
+    if (!hash) throw new Error("Bad ref " + hashish);
+    if (hash.startsWith("ref: ")) hash = hash.slice(5);
+    return hash === hashish ? hash : this.resolveRef(hash, seen);
+  }
+
+  commits(fromRef, limit, onUpdate) {
+    let fetchProc = new FetchLogProcess(this).run(fromRef, limit, onUpdate);
+    return fetchProc.whenDone.then(() => fetchProc.commits);
+  }
+    
+  async readFileObject(object, readAs = "text") {
+    // object {hash, mode, path}
+    // readAs = "text"|"blob"
+    if (object.mode !== modes.file) return undefined;
+    let content
+    try {
+      content = await this.loadAs(readAs, object.hash);
+    } catch (err) {
+      content = await this.loadAs("blob", object.hash);
+    }
+    return {content, ...object};
+  }
+
 }
 
 
