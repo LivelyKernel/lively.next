4f656a2833184cda5f6351e08d534897bb1bced3
Robin Schreiber <robin.schreiber@me.com>
2018-08-16 21:56:48 +0000

    file browser: use labels for improved performance

--- http-file-browser.js
+++ http-file-browser.js
@@ -1,6 +1,6 @@
 import { Morph, Label, InputLine } from "lively.morphic";
 import { Tree, TreeData } from "lively.components";
-import { arr, promise, num, date, string } from "lively.lang";
+import { arr, fun, promise, num, date, string } from "lively.lang";
 import { pt, Rectangle, Color } from "lively.graphics";
 import { connect } from "lively.bindings";
 import { resource } from "lively.resources";
@@ -391,7 +391,7 @@ export default class HTTPFileBrowser extends Morph {
             }),
 
             {
-              type: "text", name: "selectedFileName",
+              type: "label", name: "selectedFileName",
               fontSize: 14, fontFamily: "Inconsolata, monospace",
               readOnly: true, clipMode: "hidden"
             },
@@ -567,7 +567,7 @@ export default class HTTPFileBrowser extends Morph {
   }
 
   showSelectedFile() {
-    let sel = this.selectedFile;
+    let sel = this.selectedFile;       
     this.get("selectedFileName").textString =  sel ? sel.url : "";
   }
 
