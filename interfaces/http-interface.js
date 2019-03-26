ee46db70ba25f4e73fc369bb19c52673fdf82a3a
Robert Krahn <robert.krahn@gmail.com>
2019-03-26 04:22:20 -0700

    Build does expose remote functionality as well.

--- interfaces/http-interface.js
+++ interfaces/http-interface.js
@@ -1,21 +1,23 @@
-import { HttpEvalStrategy } from "lively.vm/lib/eval-strategies.js";
-import { RemoteCoreInterface } from "./interface.js";
+import {evalStrategies} from 'lively.vm';
+import {RemoteCoreInterface} from './interface.js';
 
 export class HTTPCoreInterface extends RemoteCoreInterface {
-
   constructor(url) {
     super();
     this.currentEval = null;
     this.url = url;
-    this.server = new HttpEvalStrategy(url);
+    this.server = new evalStrategies.HttpEvalStrategy(url);
   }
 
-  get name() { return this.url; }
+  get name() {
+    return this.url;
+  }
 
-  get description() { return this.url.replace(/^https?:\/\//, ""); }
+  get description() {
+    return this.url.replace(/^https?:\/\//, '');
+  }
 
   runEval(source, options) {
     return this.server.runEval(source, options);
   }
-
 }
