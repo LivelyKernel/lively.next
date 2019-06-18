cc0794a5c2a2fc545fb408b98e60f9dd96c59b1f
Robert Krahn <robert.krahn@gmail.com>
2019-06-18 16:18:38 +0200

    testing travis setup

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ if [[ ! -d "${lv_next_dir}/${target_package}" ]]; then
   exit 1
 fi
 
-. $lv_next_dir/lively.installer/assets/lively-next-flatn-env.sh
+. $lv_next_dir/scripts/lively-next-flatn-env.sh
 lively_next_flatn_env $lv_next_dir
 
 ln -sf $lv_next_dir/mocha-es6/bin/{mocha-es6.js,mocha-es6}
