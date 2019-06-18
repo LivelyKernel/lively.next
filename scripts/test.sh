da4bc1d543d9f5be16bb17c2716de4ef01a71d17
Robert Krahn <robert.krahn@gmail.com>
2019-06-18 20:47:54 +0200

    travis: fix ci setup

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,9 +11,14 @@ fi
 . $lv_next_dir/scripts/lively-next-flatn-env.sh
 lively_next_flatn_env $lv_next_dir
 
-pushd $lv_next_dir/mocha-es6/bin/
+pushd $lv_next_dir/mocha-es6/
+if [[ ! -d node_modules ]]; then
+    npm install
+fi
+pushd bin/
 ln -sf mocha-es6.js mocha-es6
 export PATH=$PWD:$PATH
+popd
 popd
 
 cd $target_package;
