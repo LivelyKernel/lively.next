fa68e6977c03368ea456215a54dc35bf7e723c8b
Robin Schreiber <robin.schreiber@me.com>
2019-12-25 09:43:21 +0000

    loading indicator: improve progress bar, overall styling

--- lively.components/loading-indicator.js
+++ lively.components/loading-indicator.js
@@ -1,8 +1,9 @@
 /* global System */
-import { promise } from "lively.lang";
-import { Icon, morph, Morph, StyleSheet, Image } from "lively.morphic";
-import { pt, Rectangle, Color } from "lively.graphics";
+import { promise, num } from "lively.lang";
+import { Icon, VerticalLayout, ShadowObject, HorizontalLayout, morph, Morph, StyleSheet, Image } from "lively.morphic";
+import { pt, RadialGradient, rect, Rectangle, Color } from "lively.graphics";
 import { connect } from "lively.bindings";
+import { Line } from 'https://raw.githubusercontent.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js'
 
 export default class LoadingIndicator extends Morph {
 
@@ -34,7 +35,6 @@ export default class LoadingIndicator extends Morph {
       },
       ".LoadingIndicator [name=closeButton]": {
         extent: pt(20, 20),
-        fill: Color.transparent,
         borderWidth: 0,
         fontColor: Color.white,
       }
@@ -42,7 +42,8 @@ export default class LoadingIndicator extends Morph {
   }
 
   static open(label, props) {
-    return new this({...props, label}).openInWorld();
+    return morph({...loadingIndicatorSpec, ...props, label}).openInWorld();
+    //return new this({...props, label}).openInWorld();
   }
 
   static forPromise(p, label, props) {
@@ -63,8 +64,27 @@ export default class LoadingIndicator extends Morph {
       name:       {defaultValue: "LoadingIndicator"},
       borderRadius: {defaultValue: 10},
       styleClasses: {defaultValue: ['Halo']},
+      layout: {
+        initialize() {
+          this.layout = new VerticalLayout({
+            align: 'center', spacing: 10
+          });
+        }
+      },
+      ui: {
+        derived: true,
+        get() {
+          return {
+            progressBar: this.getSubmorphNamed('progressBar'),
+            closeButton: this.getSubmorphNamed('closeButton'),
+            spinner: this.getSubmorphNamed('spinner'),
+            label: this.getSubmorphNamed('label'),
+            status: this.getSubmorphNamed('status')
+          }
+        }
+      },
       progress: {
-        defaultValue: false,
+        derived: true,
         set(p) {
           this.animateProgress(p);
         }
@@ -73,6 +93,22 @@ export default class LoadingIndicator extends Morph {
         derived: true, after: ["submorphs"],
         get() { return this.getSubmorphNamed("label").value; },
         set(val) { this.getSubmorphNamed("label").value = val; }
+      },
+      status: {
+        derived: true, after: ['submorphs'], defaultValue: false,
+        get() { return this.getSubmorphNamed('status').value; },
+        set(val) { 
+          let { status, label } = this.ui;
+          if (val) {
+            status.visible = true;
+            status.top = label.height;
+            status.value = val;
+          } else {
+            status.visible = false;
+            status.bottom = label.height;
+            status.value = '';
+          }
+        }
       },
 
       fontFamily: {
@@ -98,7 +134,7 @@ export default class LoadingIndicator extends Morph {
           let spinner;
           this.submorphs = [
             spinner = new Image({
-              extent: pt(100, 104),
+              extent: pt(140, 104),
               imageUrl: this.loadingImage,
               name: "spinner"
             }),
@@ -106,19 +142,16 @@ export default class LoadingIndicator extends Morph {
               type: "label",
               name: "label",
               value: "",
-              styleClasses: ["center-text"]
+              fontColor: Color.white,
+              fontFamily: 'Nunito'
             },
             {
               type: "button",
               name: "closeButton",
+              isLayoutable: false,
               label: Icon.textAttribute("times")
             }
           ];
-          // if (bowser.name == 'Firefox') {
-          //   spinner.extent = pt(55,55);
-          //   spinner.origin = pt(25,25);
-          //   spinner.scale = 2;
-          // }
         }
       }
     };
@@ -126,14 +159,56 @@ export default class LoadingIndicator extends Morph {
 
   constructor(props = {}) {
     super(props);
-    this.relayout();
+    let { label, closeButton, progressBar } = this.ui;
+    if (progressBar) {
+      progressBar.opacity = 0;
+      progressBar.isLayoutable = false;
+    }
     connect(this, 'extent', this, 'relayout');
-    connect(this.get("label"), 'extent', this, "relayout");
-    connect(this.get("label"), 'value', this, "updateLabel");
-    connect(this.get("label"), 'fontSize', this, "updateLabel");
-    connect(this.get("label"), 'fontFamily', this, "updateLabel");
-    connect(this.get("closeButton"), 'fire', this, "remove");
-    this.get("closeButton").fit();
+    connect(label, 'extent', this, "relayout");
+    connect(label, 'value', this, "updateLabel");
+    connect(label, 'fontSize', this, "updateLabel");
+    connect(label, 'fontFamily', this, "updateLabel");
+    connect(closeButton, 'fire', this, "remove");
+    closeButton.fit();
+    this.relayout();
+  }
+
+  async animateProgress(p) {
+    let pb = this.getSubmorphNamed('progressBar');
+    if (!pb) {
+      this.addMorph({
+        name: 'progressBar', type: 'html', fill: Color.transparent, height: 5, borderRadius: 5,
+        clipMode: 'hidden', opacity: 0
+      });
+      await pb.whenRendered();
+    }
+    if (!this.progressBar) {
+      this.progressBar = new Line(pb.domNode, {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: Color.orange,
+        trailColor: Color.darkGray,
+        trailWidth: 4,
+        svgStyle: {position: 'absolute', width: '100%', height: '100%'}
+      });
+    }
+    let { progressBar } = this.ui;
+    let center = this.center;
+    if (p == false) {
+      progressBar.isLayoutable = false;
+      progressBar.opacity = 0;
+    } else {
+      progressBar.isLayoutable = true;
+      progressBar.opacity = 1; 
+    }
+    if (this.progressBar) {
+      if (this.progressBar.value() > p) this.progressBar.set(p);
+      else this.progressBar.animate(p);
+    }
+    await this.whenRendered();
+    this.center = center;
   }
 
   get isEpiMorph() { return true; }
@@ -143,30 +218,209 @@ export default class LoadingIndicator extends Morph {
     this.center = center;
   }
 
-  animateProgress(p /* between 0-1*/) {
-    let pb = this.get('progressBar') || this.addMorph({
-      name: 'progressBar', submorphs: [{name: 'progress', width: 0}]
-    })
-    pb.get('progress').animate({width: p * pb.width, duration: 300});
-    this.relayout();
-  }
-
   relayout() {
     var padding = Rectangle.inset(20, 12),
-        [spinner, label, closeButton, progressBar] = this.submorphs,
-        w = Math.max(spinner.width, label.width, 120) + padding.left() + padding.right(),
-        h = ((progressBar && progressBar.height + 5) || 0 )
-            + spinner.bounds().height + label.height + padding.top() + padding.bottom();
-    this.extent = pt(w,h);
-    spinner.topCenter = this.innerBounds().topCenter().addXY(0, padding.top());
-    label.topCenter = spinner.bottomCenter.addXY(0, 4);
-    if (progressBar) {
-      progressBar.topCenter = label.bottomCenter.addXY(0, 4);
-      progressBar.width = this.width - 20;
-    }
-    closeButton.right = w;
+        {spinner, label, closeButton, progressBar} = this.ui;
+    closeButton.topRight = this.innerBounds().insetBy(2).topRight();
   }
   
-  onHoverIn(evt) { this.get("closeButton").visible = true; }
-  onHoverOut(evt) { this.get("closeButton").visible = false; }
+  onHoverIn(evt) { this.ui.closeButton.visible = true; }
+  onHoverOut(evt) { this.ui.closeButton.visible = false; }
 }
+
+let loadingIndicatorSpec = {
+  dropShadow: new ShadowObject({
+    distance:2,
+    rotation:45,
+    color:Color.rgba(0,0,0,0.62),
+    inset:false,
+    blur:28,
+    spread:0,
+    fast:true
+  }),
+  clipMode: 'hidden',
+  extent: pt(309.625,89.96256321724826),
+  fill: Color.rgba(0,0,0,0.65),
+  layout: new VerticalLayout({
+    spacing:15,
+    resizeSubmorphs:false,
+    autoResize:true,
+    align:"left",
+    direction:"topToBottom",
+    reactToSubmorphAnimations:false
+  }),
+  name: "loading indicator",
+  position: pt(193.6527343749999,341.3359375),
+  submorphs: [{
+      borderRadius: 6,
+      clipMode: "hidden",
+      extent: pt(279.625,6.99609375),
+      fill: Color.rgba(255,255,255,0),
+      html: "<svg viewBox=\"0 0 100 4\" preserveAspectRatio=\"none\" style=\"position: absolute; width: 100%; height: 100%;\"><path d=\"M 0,2 L 100,2\" stroke=\"#eee\" stroke-width=\"4\" fill-opacity=\"0\"></path><path d=\"M 0,2 L 100,2\" stroke=\"rgb(255,153,0)\" stroke-width=\"4\" fill-opacity=\"0\" style=\"stroke-dasharray: 100, 100; stroke-dashoffset: 100;\"></path></svg>",
+      name: "progressBar",
+      isLayoutable: false,
+      position: pt(15,67.96646946724826),
+      type: "html"
+    }, {
+      draggable: false,
+      extent: pt(153.1222655103119,37.96646946724826),
+      fill: Color.rgba(46,75,223,0),
+      layout: new HorizontalLayout({"spacing":0,"resizeSubmorphs":false,"autoResize":true,"align":"center","direction":"leftToRight","reactToSubmorphAnimations":false}),
+      name: "rectangle",
+      position: pt(15,15),
+      submorphs: [{
+          cssDeclaration: `
+           .lds-spinner {
+            color: official;
+            display: inline-block;
+            position: relative;
+            width: 64px;
+            height: 64px;
+          }
+          .lds-spinner div {
+            transform-origin: 32px 32px;
+            animation: lds-spinner .6s linear infinite;
+          }
+          .white-spinner div:after {
+            content: " ";
+            display: block;
+            position: absolute;
+            top: 3px;
+            left: 29px;
+            width: 5px;
+            height: 14px;
+            border-radius: 20%;
+            background: white;
+          }
+          .lds-spinner div:nth-child(1) {
+            transform: rotate(0deg);
+            animation-delay: -.55s;
+          }
+          .lds-spinner div:nth-child(2) {
+            transform: rotate(30deg);
+            animation-delay: -.5s;
+          }
+          .lds-spinner div:nth-child(3) {
+            transform: rotate(60deg);
+            animation-delay: -0.45s;
+          }
+          .lds-spinner div:nth-child(4) {
+            transform: rotate(90deg);
+            animation-delay: -0.4s;
+          }
+          .lds-spinner div:nth-child(5) {
+            transform: rotate(120deg);
+            animation-delay: -0.35s;
+          }
+          .lds-spinner div:nth-child(6) {
+            transform: rotate(150deg);
+            animation-delay: -0.3s;
+          }
+          .lds-spinner div:nth-child(7) {
+            transform: rotate(180deg);
+            animation-delay: -0.25s;
+          }
+          .lds-spinner div:nth-child(8) {
+            transform: rotate(210deg);
+            animation-delay: -0.2s;
+          }
+          .lds-spinner div:nth-child(9) {
+            transform: rotate(240deg);
+            animation-delay: -0.15s;
+          }
+          .lds-spinner div:nth-child(10) {
+            transform: rotate(270deg);
+            animation-delay: -0.1s;
+          }
+          .lds-spinner div:nth-child(11) {
+            transform: rotate(300deg);
+            animation-delay: -0.05s;
+          }
+          .lds-spinner div:nth-child(12) {
+            transform: rotate(330deg);
+            animation-delay: 0s;
+          }
+          @keyframes lds-spinner {
+            0% {
+              opacity: 1;
+            }
+            100% {
+              opacity: 0;
+            }
+          }`,
+          extent: pt(86.24453102062381,70.22525286899308),
+          fill: Color.rgba(255,255,255,0),
+          html: "<div class=\"white-spinner lds-spinner\"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>",
+          name: "spinner",
+          position: pt(0,1.4269215163758595),
+          scale: 0.5,
+          type: 'html'
+        }, {
+          extent: pt(110,22),
+          fill: Color.rgba(255,255,255,0),
+          fontColor: Color.rgb(253,254,254),
+          fontFamily: "Nunito",
+          fontSize: 16,
+          fontWeight: "bold",
+          name: "label",
+          nativeCursor: "pointer",
+          position: pt(43.122265510311905,3.552713678800501e-15),
+          submorphs: [{
+              extent: pt(83,16),
+              fill: Color.rgba(255,255,255,0),
+              fontColor: Color.rgb(253,254,254),
+              fontFamily: "Nunito",
+              fontWeight: "bold",
+              name: "status",
+              nativeCursor: "pointer",
+              opacity: 0.65,
+              position: pt(0.3464844896881232,21.96646946724826),
+              textAndAttributes: ["Loading World", null],
+              type: 'label'
+            }],
+          textAndAttributes: ["Loading World", null],
+          type: 'label'
+        }],
+    }, {
+      borderRadius: 22,
+      borderWidth: 0,
+      dropShadow: new ShadowObject({
+        distance:3,
+        rotation:45,
+        color:Color.rgba(0,0,0,0.26),
+        inset:false,
+        blur:10,
+        spread:0,
+        fast:true
+      }),
+      extent: pt(21.603125000000006,21.0703125),
+      fill: new RadialGradient({
+        stops: [{
+          offset: 0, 
+          color: Color.rgb(153,163,164)
+        }, {
+          offset: 1,
+          color: Color.rgb(112,123,124)
+        }],
+        bounds: rect(0,0,20,20),
+        focus: pt(0.5,0.5)
+      }),
+      fontColor: Color.rgb(253,254,254),
+      isLayoutable: false,
+      name: "closeButton",
+      position: pt(276.8734375,8.8515625),
+      submorphs: [{
+          extent: pt(8,16),
+          fontColor: Color.rgb(253,254,254),
+          fontWeight: "bolder",
+          name: "label",
+          position: pt(6.801562500000003,2.53515625),
+          reactsToPointer: false,
+          textAndAttributes: Icon.textAttribute("times"),
+          type: "label"
+        }],
+      type: "button",
+      visible: false
+    }],
+  type: LoadingIndicator
+};
