92cc763c60b836c5becafce2de05137bf6668409
Robin Schreiber <robin.schreiber@me.com>
2020-04-13 14:16:32 +0000

    halos: remove svg style halo

--- lively.halos/index.js
+++ lively.halos/index.js
@@ -2,13 +2,11 @@ import Halo, { MorphHighlighter, InteractiveMorphSelector } from "./morph.js";
 import { StatusMessage, StatusMessageForMorph, show, showConnector } from "./markers.js";
 import { ProportionalLayoutHalo, GridLayoutHalo, FlexLayoutHalo } from "./layout.js";
 import { showAndSnapToGuides, removeSnapToGuidesOf } from "./drag-guides.js";
-import { SvgStyleHalo } from './vertices.js';
 
 export {
   StatusMessage, StatusMessageForMorph, 
   show, showConnector,
   Halo, MorphHighlighter, InteractiveMorphSelector,
   ProportionalLayoutHalo, GridLayoutHalo, FlexLayoutHalo,
-  SvgStyleHalo, // rms 26.08.19: deprecated, to be removed
   showAndSnapToGuides, removeSnapToGuidesOf
 };
