3bb759051faa3c4a711bb50f80fa474f0f957021
Robin Schreiber <robin.schreiber@me.com>
2020-10-08 10:12:04 +0200

    install: remove custom-npm-modules dir on init install

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ lv_next_dir=$PWD
 
 export PATH=$lv_next_dir/flatn/bin:$PATH
 export FLATN_PACKAGE_DIRS=
-export FLATN_PACKAGE_COLLECTION_DIRS=$lv_next_dir/lively.next-node_modules:$lv_next_dir/custom-npm-modules
+export FLATN_PACKAGE_COLLECTION_DIRS=$lv_next_dir/lively.next-node_modules
 eval $(node -p 'let PWD=process.cwd();let packages = JSON.parse(require("fs").readFileSync(PWD+"/lively.installer/packages-config.json")).map(ea => require("path").join(PWD, ea.name));`export FLATN_DEV_PACKAGE_DIRS=${packages.join(":")}`')                                                              
 mkdir lively.next-node_modules
-flatn --C lively.next-node_modules install brotli
-flatn --C lively.next-node_modules install node-fetch@2.6.0
+flatn --C ./lively.next-node_modules install brotli
+flatn --C ./lively.next-node_modules install node-fetch@2.6.0
 
 node lively.installer/install-with-node.js $PWD
