0fcce61198f295b6d39c913923faf36ce1398079
Robin Schreiber <robin.schreiber@me.com>
2020-10-10 15:51:09 +0000

    world save: world save dialogs rememver file names

--- lively.morphic/world-loading.js
+++ lively.morphic/world-loading.js
@@ -221,9 +221,9 @@ export async function interactivelySaveWorld(world, options) {
       }
       i.visible = true;
     } else await resourceHandle.ensureExistance();
-    
+
+    world.changeMetaData('file', jsonStoragePath, false, false);
     await resourceHandle.writeJson(await createMorphSnapshot(world));
-    resource(System.baseURL).join('/subservers/ComponentBrowser/refresh').join(name).read();
     i.remove();
     world.setStatusMessage(`saved world ${name} to file: ${resourceHandle.url}`);
     return;
@@ -267,8 +267,6 @@ export async function interactivelySaveWorld(world, options) {
           path = pathForBrowserHistory(name, queryString);
       window.history.pushState({}, "lively.next", path);
     }
-
-    resource(System.baseURL).join('/subserver/ComponentsBrowser/refresh').join(name).read();
 
     world.setStatusMessage(`saved world ${name}`);
     world.get("world-list") && world.get("world-list").onWorldSaved(name);
