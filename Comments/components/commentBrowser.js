4fc6ad378d700626bf114d6fabde5cc35766b8da
frcroth <frcroth@gmail.com>
2020-11-18 12:29:55 +0100

    Toggle color in topbar

--- Comments/components/commentBrowser.js
+++ Comments/components/commentBrowser.js
@@ -1,48 +1,73 @@
-import { Window } from "lively.components";
-import { CommentMorph } from "./commentMorph.js";
-import { VerticalLayout, Morph } from "lively.morphic";
-import { pt } from "lively.graphics";
+import { Window } from 'lively.components';
+import { CommentMorph } from './commentMorph.js';
+import { VerticalLayout, Morph } from 'lively.morphic';
+import { pt } from 'lively.graphics';
+
+let instance;
 
 export class CommentsBrowser extends Window {
-
-  constructor() {
-    super();
-    this.container = new Morph({
-        layout: new VerticalLayout(),
-      })
-    this.addMorph(this.container);
-    this.updateCommentMorphs();
-    this.height = ($world.height - $world.getSubmorphNamed("lively top bar").height) * 0.8;
-    this.width = 200;
-    this.position = pt($world.width - 200, $world.getSubmorphNamed("lively top bar").height + 100);
-    this.relayoutWindow();
+  static close () {
+    instance.close();
   }
 
-  getCommentsInWorld() {
-    let comments = [];
+  static isOpen () {
+    return instance;
+  }
+
+  static get instance () {
+    return instance;
+  }
+
+  constructor () {
+    if (!instance) {
+      super();
+      this.container = new Morph({
+        layout: new VerticalLayout()
+      });
+      this.addMorph(this.container);
+      this.updateCommentMorphs();
+      this.height = ($world.height - $world.getSubmorphNamed('lively top bar').height) * 0.8;
+      this.width = 200;
+      this.position = pt($world.width - 200, $world.getSubmorphNamed('lively top bar').height + 100);
+      this.relayoutWindow();
+      $world.addMorph(this);
+      instance = this;
+    }
+    return instance;
+  }
+
+  close () {
+    super.close();
+    const topbar = $world.getSubmorphNamed('lively top bar');
+    if (topbar) {
+      topbar.uncolorCommentBrowserButton();
+    }
+    instance = undefined;
+  }
+
+  getCommentsInWorld () {
+    const comments = [];
     $world.withAllSubmorphsDo((morph) => {
-      
       comments.push(...morph.comments);
-    })
+    });
     return comments;
   }
 
-  getCommentMorphs(commentList) {
-    let commentMorphs = [];
+  getCommentMorphs (commentList) {
+    const commentMorphs = [];
     commentList.forEach((comment) => {
       commentMorphs.push(new CommentMorph(comment));
-    })
+    });
     return commentMorphs;
   }
 
-  updateCommentMorphs() {
-    let commentMorphs = this.getCommentMorphs(this.getCommentsInWorld());
+  updateCommentMorphs () {
+    const commentMorphs = this.getCommentMorphs(this.getCommentsInWorld());
     this.container.submorphs = commentMorphs;
   }
-  
-  // to not block responseToVisibleWindow
-  relayoutWindow() {
+
+  // to not block respondsToVisibleWindow
+  relayoutWindow () {
     this.relayoutWindowControls();
   }
-  
 }
