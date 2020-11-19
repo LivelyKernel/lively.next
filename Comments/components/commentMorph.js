b709ebb9b0ef88c2735a8bee080c759992c7b028
Silvan Verhoeven <SilvanVerhoeven@github.com>
2020-11-19 01:30:02 +0100

    Update CommentMorph

--- Comments/components/commentMorph.js
+++ Comments/components/commentMorph.js
@@ -11,22 +11,31 @@ export class CommentMorph extends Morph {
     };
   }
 
+  constructor () {
+    super();
+    this.ui = {
+      dateLabel: this.get('date label'),
+      commentTextLabel: this.get('comment text field'),
+      deleteButton: this.get('delete button')
+    };
+  }
+
   initialize (comment, referenceMorph) {
     this.comment = comment;
     this.referenceMorph = referenceMorph;
     const [date, time] = new Date(this.comment.timestamp).toLocaleString('de-DE', { hour12: false }).split(', ');
-    this.get('dateLabel').textString = ' ' + date + ' ' + time;
-    this.get('commentTextField').textString = this.comment.text;
+    this.get('date label').textString = date + ' ' + time;
+    // TODO: Fix that width doesn't need to be increased at runtime
+    this.get('date label').width += 20;
+    this.get('comment text field').textString = this.comment.text;
   }
 
   onMouseDown (evt) {
     super.onMouseDown(evt);
 
-    const deleteButton = this.get('deleteButton');
-
-    if (evt.targetMorph === deleteButton) {
+    if (evt.targetMorph === this.get('delete button')) {
       this.referenceMorph.removeComment(this.comment);
-    } else {
+    } else if (this.referenceMorph) {
       this.referenceMorph.show();
     }
   }
