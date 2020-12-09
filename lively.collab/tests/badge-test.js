72a9239a2774730f95f9613ad475975df7c8a58b
frcroth <frcroth@gmail.com>
2020-12-09 14:33:16 +0100

    Rename test files so CI can find them
    Co-authored-by: Paula-Kli <Paula-Kli@users.noreply.github.com>

---
+++ lively.collab/tests/badge-test.js
@@ -0,0 +1,6 @@
+/* global System, declare, done, it, xit, describe, xdescribe, beforeEach, afterEach, before, after */
+import { expect } from 'mocha-es6';
+
+describe('badge', function () {
+
+});
--- lively.collab/tests/comment-class-test.js
+++ lively.collab/tests/comment-class-test.js
@@ -33,6 +33,7 @@ describe('comment object', function () {
 describe('morph', function () {
   let morph;
   const exampleText = 'Example text';
+  let comment;
 
   beforeEach(function () {
     morph = new Morph();
@@ -43,13 +44,13 @@ describe('morph', function () {
   });
 
   it('a comment may be added', async function () {
-    await morph.addComment(exampleText);
+    comment = await morph.addComment(exampleText);
     expect(morphHasNumberOfComments(morph, 1));
     expect(morph.comments[0].text).equals(exampleText);
   });
 
   it('a comment may be removed', async function () {
-    const comment = await morph.addComment(exampleText);
+    comment = await morph.addComment(exampleText);
     expect(morph.comments[0].equals(comment)).to.be.ok;
     await morph.removeComment(comment);
     expect(morphHasNoComments(morph)).to.be.ok;
@@ -64,9 +65,14 @@ describe('morph', function () {
   });
 
   it('can be cloned to with equal comment', async function () {
-    await morph.addComment(exampleText);
+    comment = await morph.addComment(exampleText);
     const morph2 = morph.copy();
     expect(morph.comments[0].equals(morph2.comments[0])).to.be.ok;
+  });
+
+  afterEach(async function () {
+    morph.emptyComments();
+    morph.remove();
   });
 
   function morphHasNoComments (morph) {
