48345c5b04e26b2d15cf9841e5b5ecb82c2fbea0
Robin Schreiber <robin.schreiber@me.com>
2021-04-09 10:37:54 +0000

    morph: lint morph to image

--- lively.morphic/rendering/morph-to-image.js
+++ lively.morphic/rendering/morph-to-image.js
@@ -1,5 +1,5 @@
-/*global System,Map*/
-import domToImage from "https://dev.jspm.io/dom-to-image-more";
+/* global System,Map */
+import domToImage from 'https://dev.jspm.io/dom-to-image-more';
 
 /*
 
@@ -22,36 +22,36 @@ import domToImage from "https://dev.jspm.io/dom-to-image-more";
 
 */
 
-function fixNodejs(document, window) {
+function fixNodejs (document, window) {
   // 2017-03-18 FIXME
   // The dom-to-image does not support nodejs or options for jsdom. For now we
   // install globals so that it works but it would be better to fix the lib...!
 
-  let global = System.global,
-      globals = [
-        "HTMLCanvasElement",
-        "Element",
-        "HTMLTextAreaElement",
-        "HTMLInputElement",
-        "SVGElement",
-        "SVGRectElement",
-        "XMLSerializer",
-        "Blob",
-        "Image",
-        "XMLHttpRequest",
-        "FileReader",
-        "CSSRule",
-        "HTMLImageElement",
-      ];
+  const global = System.global;
+  const globals = [
+    'HTMLCanvasElement',
+    'Element',
+    'HTMLTextAreaElement',
+    'HTMLInputElement',
+    'SVGElement',
+    'SVGRectElement',
+    'XMLSerializer',
+    'Blob',
+    'Image',
+    'XMLHttpRequest',
+    'FileReader',
+    'CSSRule',
+    'HTMLImageElement'
+  ];
 
   if (!window.XMLSerializer) {
-    window.XMLSerializer = function XMLSerializer_hack() {}
+    window.XMLSerializer = function XMLSerializer_hack () {};
     window.XMLSerializer.prototype.serializeToString = el => el.outerHTML;
   }
 
   if (global.window !== window) global.window = window;
   if (global.document !== document) global.document = document;
-  for (let glob of globals) {
+  for (const glob of globals) {
     if (!window[glob]) {
       console.log(`${glob} is not available!`);
       window[glob] = eval(`(function ${glob}() { throw new Error("${glob} not supported"); })`);
@@ -62,14 +62,14 @@ function fixNodejs(document, window) {
 }
 
 const typeMethodMap = {
-  "blob": "toBlob",
-  "jpeg": "toJpeg",
-  "pixeldata": "toPixelData",
-  "png": "toPng",
-  "svg": "toSvg",
-}
+  blob: 'toBlob',
+  jpeg: 'toJpeg',
+  pixeldata: 'toPixelData',
+  png: 'toPng',
+  svg: 'toSvg'
+};
 
-export async function renderMorphToDataURI(morph, opts = {}) {
+export async function renderMorphToDataURI (morph, opts = {}) {
   // Takes a morph and options like opts = {width: NUMBER, height: NUMBER,
   // center: BOOL}, then calls morph.renderPreview() which creates a DOM node
   // looking like the morph.  This node is then fed to dom-to-image where it is
@@ -78,23 +78,22 @@ export async function renderMorphToDataURI(morph, opts = {}) {
   // Nodes of HTML morphs are identity-based, ie the preview render will move
   // those to another parent.  To properly restore the HTML morphs, we move their
   // nodes back to their original parent
-  let htmlMorphRestoreMap = new Map();
+  const htmlMorphRestoreMap = new Map();
   morph.withAllSubmorphsDo(ea =>
     ea.isHTMLMorph && htmlMorphRestoreMap.set(
-      ea, {domNode: ea.domNode, parentNode: ea.domNode.parentNode}));
+      ea, { domNode: ea.domNode, parentNode: ea.domNode.parentNode }));
 
-  let actualRenderOnGPU = morph.renderOnGPU;
+  const actualRenderOnGPU = morph.renderOnGPU;
   morph.renderOnGPU = true; // needs to be promoted to own layer
   await morph.whenRendered();
 
-  let {document, window} = morph.env.domEnv,
-      node = morph.renderPreview({...opts, asNode: true}), dataURI,
-      wrapper = document.createElement("div"),
-      canvas = document.createElement("div"),
-      {width, height, type = 'svg'} = opts;
+  const { document, window } = morph.env.domEnv;
+  const node = morph.renderPreview({ ...opts, asNode: true }); let dataURI;
+  const wrapper = document.createElement('div');
+  const canvas = document.createElement('div');
+  const { width, height, type = 'svg' } = opts;
 
-  if (System.get("@system-env").node)
-    fixNodejs(document, window);
+  if (System.get('@system-env').node) { fixNodejs(document, window); }
 
   // In order for dom-to-image to render the node we need to put it into the DOM.
   // To make sure the specified width and height into which the node is
@@ -105,32 +104,36 @@ export async function renderMorphToDataURI(morph, opts = {}) {
   // up
 
   try {
-    wrapper.style.overflow = "hidden"
-    wrapper.style.width = "1px";
-    wrapper.style.height = "1px";
-    wrapper.style.position = "absolute";
+    wrapper.style.overflow = 'hidden';
+    wrapper.style.width = '1px';
+    wrapper.style.height = '1px';
+    wrapper.style.position = 'absolute';
 
     document.body.appendChild(wrapper);
     wrapper.appendChild(canvas);
     canvas.appendChild(node);
 
-    let domBounds = width && height ? null : node.getBoundingClientRect();
-    if (!width) canvas.style.width = Math.ceil(domBounds.width + morph.borderWidth*2) + "px";
-    if (!height) canvas.style.height = Math.ceil(domBounds.height + morph.borderWidth*2) + "px";
+    const domBounds = width && height ? null : node.getBoundingClientRect();
+    if (!width) canvas.style.width = Math.ceil(domBounds.width + morph.borderWidth * 2) + 'px';
+    if (!height) canvas.style.height = Math.ceil(domBounds.height + morph.borderWidth * 2) + 'px';
 
-    canvas.style.width = width + "px";
-    canvas.style.height = height + "px";
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
 
-    let method = typeMethodMap[type] || "toSvg";
-    return await domToImage[method](canvas); // returns data URI
-
+    const method = typeMethodMap[type] || 'toSvg';
+    const dataUrl = await domToImage[method](canvas, {
+      style: {
+        transform: 'scale(.7)',
+        'transform-origin': 'center'
+      }
+    }); // returns data URI
+    return dataUrl;
   } finally {
     morph.renderOnGPU = actualRenderOnGPU;
     wrapper.parentNode && wrapper.parentNode.removeChild(wrapper);
 
-    for (let [htmlMorph, {domNode, parentNode}] of htmlMorphRestoreMap) {
-      if (domNode && parentNode && domNode.parentNode !== parentNode)
-        parentNode.appendChild(domNode)
+    for (const [htmlMorph, { domNode, parentNode }] of htmlMorphRestoreMap) {
+      if (domNode && parentNode && domNode.parentNode !== parentNode) { parentNode.appendChild(domNode); }
     }
   }
 }
