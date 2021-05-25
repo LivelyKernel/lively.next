cc50b9534bee1503a0d54d20ceab70414abb50ca
Robin Schreiber <robin.schreiber@me.com>
2021-05-25 15:54:39 +0000

    dav: fix uncaught promise rejection on caching files server side

--- lively.server/plugins/dav.js
+++ lively.server/plugins/dav.js
@@ -1,91 +1,91 @@
-/*global System, process*/
+/* global System, process */
 
-import { obj } from "lively.lang";
-import { resource } from "lively.resources";
-import { string } from "lively.lang";
+import { obj } from 'lively.lang';
+import { resource } from 'lively.resources';
+import { string } from 'lively.lang';
 
 // FIXME...
-var DavHandler, FsTree, jsDavPlugins = {};
-(function loadJsDAV() {
+let DavHandler; let FsTree;
+const jsDavPlugins = {};
+(function loadJsDAV () {
   // jsDAV shows unimportant console logs while loading, hide those...
-  let log = console.log;
-  console.log = () => {}
+  const log = console.log;
+  console.log = () => {};
   try {
-    DavHandler = System._nodeRequire(`jsDAV/lib/DAV/handler`);
-    FsTree = System._nodeRequire(`jsDAV/lib/DAV/backends/fs/tree`);
-    jsDavPlugins.browser = System._nodeRequire(`jsDAV/lib/DAV/plugins/browser.js`);
-  }
-  catch (err) { console.error(`cannot load jsdav:` , err); }
-  finally { console.log = log; }  
+    DavHandler = System._nodeRequire('@pylonide/jsdav/lib/DAV/handler');
+    FsTree = System._nodeRequire('@pylonide/jsdav/lib/DAV/backends/fs/tree');
+    jsDavPlugins.browser = System._nodeRequire('@pylonide/jsdav/lib/DAV/plugins/browser.js');
+  } catch (err) { console.error('cannot load jsdav:', err); } finally { console.log = log; }
 })();
 
 export default class LivelyDAVPlugin {
-
-  constructor() {
+  constructor () {
     this.options = {
       rootDirectory: process.cwd(),
       excludedDirectories: [],
       excludedFiles: ['.DS_Store'],
-      includedFiles: undefined/*allow all*/,
-    }
+      includedFiles: undefined/* allow all */
+    };
   }
 
-  setOptions(opts) { Object.assign(this.options, opts); }
+  setOptions (opts) { Object.assign(this.options, opts); }
 
-  get pluginId() { return "jsdav"; }
+  get pluginId () { return 'jsdav'; }
 
-  toString() { return `<${this.pluginId}>`; }
+  toString () { return `<${this.pluginId}>`; }
 
-  get after() { return ["cors", "socketio", "eval"]; }
+  get after () { return ['cors', 'socketio', 'eval']; }
 
-  setup({server}) {
-    server.once("close", () => this.server = null);
+  setup ({ server }) {
+    server.once('close', () => this.server = null);
     this.server = server;
     this.patchServerForJsDAV(server);
     this.fileHashes = {};
   }
 
-  patchServerForJsDAV(server, thenDo) {
+  patchServerForJsDAV (server, thenDo) {
     // this is what jsDAV expects...
     server.tree = FsTree.new(this.options.rootDirectory);
     server.tmpDir = './tmp'; // httpPut writes tmp files
     server.options = {};
     // for showing dir contents
-    server.plugins = {...jsDavPlugins};
+    server.plugins = { ...jsDavPlugins };
 
     // https server has slightly different interface
     if (!server.baseUri) server.baseUri = '/';
     if (!server.getBaseUri) server.getBaseUri = () => server.baseUri;
   }
 
-  handleRequest(req, res, next) {
-    var path = '';
+  handleRequest (req, res, next) {
+    let path = '';
     // Fix URL to allow non-root installations
     // In Apache config, set:
     //   RequestHeader set x-lively-proxy-path /[your-path]
     if (req.headers['x-lively-proxy-path']) {
       path = req.headers['x-lively-proxy-path'];
-      if (path.endsWith("/")) path = path.slice(0, -1);
+      if (path.endsWith('/')) path = path.slice(0, -1);
     }
     this.server.baseUri = path + '/';
     req.url = path + req.url;
 
     if (req.url == '/__JS_FILE_HASHES__') {
-      res.writeHead(200, {"content-type": "application/json"});
+      res.writeHead(200, { 'content-type': 'application/json' });
       res.end(JSON.stringify(this.fileHashes));
       return;
     }
-    
+
     if (req.url.endsWith('.js')) {
       if (req.method == 'PUT' || !this.fileHashes[req.url]) {
-        resource("file://" + this.options.rootDirectory).join(decodeURIComponent(req.url)).read().then(source => {
-          this.fileHashes[req.url] = string.hashCode(source);   
+        resource('file://' + this.options.rootDirectory).join(decodeURIComponent(req.url)).read().then(source => {
+          this.fileHashes[req.url] = string.hashCode(source);
+        }).catch(err => {
+          // ignore
         });
       }
     }
-    
-    if (req.method == 'GET' && 
-        req.url.endsWith('.json') && 
+
+    if (req.method == 'GET' &&
+        req.url.endsWith('.json') &&
         req.headers['accept-encoding'].includes('br') &&
         // fixme
         req.url.includes('lively.morphic/styleguides')) {
@@ -93,23 +93,22 @@ export default class LivelyDAVPlugin {
       req.url = req.url.replace('.json', '.br.json');
       res.setHeader('content-encoding', 'br');
     }
+
     this.callDAV(req, res);
   }
 
-  callDAV(req, res) {
-    var handler = new DavHandler(this.server, req, res);
+  callDAV (req, res) {
+    const handler = new DavHandler(this.server, req, res);
     if (handler.plugins.browser) {
-      var origGetAssetUrl = handler.plugins.browser.getAssetUrl;
+      const origGetAssetUrl = handler.plugins.browser.getAssetUrl;
       handler.plugins.browser.getAssetUrl = function (assetName) {
-        if (assetName === "favicon.ico")
-          return this.handler.server.getBaseUri() + assetName;
-        return origGetAssetUrl.call(this, assetName)
-      }
+        if (assetName === 'favicon.ico') { return this.handler.server.getBaseUri() + assetName; }
+        return origGetAssetUrl.call(this, assetName);
+      };
     }
   }
 
-  close() {
+  close () {
     this.server = null;
   }
-
 }
