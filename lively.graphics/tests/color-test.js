ca03ae87c8b53b277206c54bf1249528398f193c
Robin Schreiber <robin.schreiber@me.com>
2021-06-01 13:39:47 +0200

    graphics: lint files

--- lively.graphics/tests/color-test.js
+++ lively.graphics/tests/color-test.js
@@ -1,52 +1,51 @@
-/*global describe, it*/
+/* global describe, it */
 
-import { expect } from "mocha-es6";
+import { expect } from 'mocha-es6';
 
-import { Color } from "../color.js";
-import { LinearGradient } from "lively.graphics";
+import { Color } from '../color.js';
+import { LinearGradient } from 'lively.graphics';
 
-describe("lively.graphics", () => {
-
-  it("parses a hex string", () => {
-    expect(Color.rgbHex("#FFFFFF")).stringEquals(Color.white);
-    expect(Color.rgbHex("#000000")).stringEquals(Color.black);
+describe('lively.graphics', () => {
+  it('parses a hex string', () => {
+    expect(Color.rgbHex('#FFFFFF')).stringEquals(Color.white);
+    expect(Color.rgbHex('#000000')).stringEquals(Color.black);
   });
 
-  it("interpolates colors", () => {
-    expect(Color.white.interpolate(.5, Color.black).equals(Color.fromTuple([.5,.5,.5,1]))).equals(true);
+  it('interpolates colors', () => {
+    expect(Color.white.interpolate(0.5, Color.black).equals(Color.fromTuple([0.5, 0.5, 0.5, 1]))).equals(true);
   });
 
-  it("interpolates linear gradient", () => {
+  it('interpolates linear gradient', () => {
     let g1 = new LinearGradient({
       stops: [
         {
           color: Color.white,
           offset: 0
-        },{
+        }, {
           color: Color.black,
           offset: 1
         }
       ]
-    }),
-        g2 = new LinearGradient({
-          stops: [
+    });
+    let g2 = new LinearGradient({
+      stops: [
         {
           color: Color.black,
           offset: 0.2
-        },{
-          color: Color.fromTuple([.2,.4,.6]),
-          offset: .8
+        }, {
+          color: Color.fromTuple([0.2, 0.4, 0.6]),
+          offset: 0.8
         }
       ]
     });
-    expect(g1.interpolate(.5, g2).equals(new LinearGradient({
+    expect(g1.interpolate(0.5, g2).equals(new LinearGradient({
       stops: [
         {
-          color: Color.fromTuple([.5, .5, .5]),
-          offset: .1
-        },{
-          color: Color.fromTuple([.1, .2, .3]),
-          offset: .9
+          color: Color.fromTuple([0.5, 0.5, 0.5]),
+          offset: 0.1
+        }, {
+          color: Color.fromTuple([0.1, 0.2, 0.3]),
+          offset: 0.9
         }
       ]
     }))).equals(true);
