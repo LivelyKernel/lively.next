bd5d1f06d3a8417efcf4b945788a0d0d620167a8
Robin Schreiber <robin.schreiber@me.com>
2021-06-02 12:50:57 +0200

    headless: lint files

--- lively.headless/tests/test.js
+++ lively.headless/tests/test.js
@@ -1,6 +1,6 @@
-import { expect } from "mocha-es6";
-describe("lively.headless", () => {
-  it("works", () => {
+import { expect } from 'mocha-es6';
+describe('lively.headless', () => {
+  it('works', () => {
     expect(1 + 2).equals(3);
   });
 });
