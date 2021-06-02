f9db446700e42b05ad7ede2146433ac7144131fd
Robin Schreiber <robin.schreiber@me.com>
2021-06-02 13:24:52 +0200

    notifications: lint files

--- lively.notifications/index.js
+++ lively.notifications/index.js
@@ -1,6 +1,6 @@
-/*global System*/
+/* global System */
 
-import { events, obj } from "lively.lang";
+import { events, obj } from 'lively.lang';
 
 // type EventType = string
 // type EventTime = number
@@ -12,13 +12,13 @@ import { events, obj } from "lively.lang";
 
 let env = null;
 
-function getEnv(_System) { // System? -> Env
+function getEnv (_System) { // System? -> Env
   if (_System === undefined) {
-    if (typeof System === "undefined") {
+    if (typeof System === 'undefined') {
       // fallback if not System is available
       if (env !== undefined) return env;
       return env || (env = {
-        emitter: events.makeEmitter({}, {maxListenerLimit: 10000}),
+        emitter: events.makeEmitter({}, { maxListenerLimit: 10000 }),
         notifications: []
       });
     }
@@ -26,61 +26,59 @@ function getEnv(_System) { // System? -> Env
     _System = System;
   }
 
-  const livelyEnv = _System.get("@lively-env");
-  if (!livelyEnv)
-    _System.set("@lively-env", _System.newModule({options: {}}));
+  const livelyEnv = _System.get('@lively-env');
+  if (!livelyEnv) { _System.set('@lively-env', _System.newModule({ options: {} })); }
 
   let options = livelyEnv.options;
 
-  if (!options)
-    throw new Error("@lively-env registered read-only");
+  if (!options) { throw new Error('@lively-env registered read-only'); }
 
   if (!options.emitter) {
     Object.assign(options, {
-      emitter: _System["__lively.notifications_emitter"] ||
-              (_System["__lively.notifications_emitter"] = events.makeEmitter({}, {maxListenerLimit: 10000})),
-      notifications: _System["__lively.notifications_notifications"] ||
-                    (_System["__lively.notifications_notifications"] = []),
+      emitter: _System['__lively.notifications_emitter'] ||
+              (_System['__lively.notifications_emitter'] = events.makeEmitter({}, { maxListenerLimit: 10000 })),
+      notifications: _System['__lively.notifications_notifications'] ||
+                    (_System['__lively.notifications_notifications'] = [])
     });
   }
-  const {emitter, notifications} = options;
-  return {emitter, notifications};
+  const { emitter, notifications } = options;
+  return { emitter, notifications };
 }
 
-export function subscribe(type, handler, system) {
+export function subscribe (type, handler, system) {
   // EventType, Handler, System? -> Handler
   getEnv(system).emitter.on(type, handler);
   return handler;
 }
 
-export function subscribeOnce(type, handler, system) {
+export function subscribeOnce (type, handler, system) {
   // EventType, Handler, System? -> Handler
   getEnv(system).emitter.once(type, handler);
   return handler;
 }
 
-export function emit(type, data = {}, time = Date.now(), system) {
+export function emit (type, data = {}, time = Date.now(), system) {
   // EventType, Notification?, EventTime?, System? -> Notification
-  const notification = Object.assign({type, time}, data),
-        {emitter, notifications} = getEnv(system);
+  const notification = Object.assign({ type, time }, data);
+  const { emitter, notifications } = getEnv(system);
   emitter.emit(type, notification);
   if (emitter.isLogging) log(notification);
   if (emitter.isRecording) record(notifications, notification);
   return notification;
 }
 
-export function unsubscribe(type, handler, system) {
+export function unsubscribe (type, handler, system) {
   // EventType, Handler, System? -> Handler
   getEnv(system).emitter.removeListener(type, handler);
   return handler;
 }
 
-export function unsubscribeAll(type, system) {
+export function unsubscribeAll (type, system) {
   // EventType, System? -> ()
   getEnv(system).emitter.removeAllListeners(type);
 }
 
-function record(notifications, notification) {
+function record (notifications, notification) {
   // Array<Notification>, Notification -> ()
   notifications.push(notification);
   if (notifications.limit) {
@@ -88,32 +86,32 @@ function record(notifications, notification) {
   }
 }
 
-export function startRecording(system) { // System? -> ()
+export function startRecording (system) { // System? -> ()
   getEnv(system).emitter.isRecording = true;
 }
 
-export function stopRecording(system) { // System? -> ()
+export function stopRecording (system) { // System? -> ()
   getEnv(system).emitter.isRecording = false;
 }
 
-export function clearRecord(system) { // System? -> ()
-  const {notifications} = getEnv(system);
+export function clearRecord (system) { // System? -> ()
+  const { notifications } = getEnv(system);
   notifications.splice(0, notifications.length);
 }
 
-export function getRecord(system) { // System? -> Notifications
+export function getRecord (system) { // System? -> Notifications
   return getEnv(system).notifications;
 }
 
-function log(notification) { // Notification -> ()
-  const padded = notification.type + " ".repeat(Math.max(0, 32 - notification.type.length));
-  console.log(padded + " " + obj.inspect(notification, {maxDepth: 2}));
+function log (notification) { // Notification -> ()
+  const padded = notification.type + ' '.repeat(Math.max(0, 32 - notification.type.length));
+  console.log(padded + ' ' + obj.inspect(notification, { maxDepth: 2 }));
 }
 
-export function startLogging(system) { // System? -> ()
+export function startLogging (system) { // System? -> ()
   getEnv(system).emitter.isLogging = true;
 }
 
-export function stopLogging(system) { // System? -> ()
+export function stopLogging (system) { // System? -> ()
   getEnv(system).emitter.isLogging = false;
 }
--- lively.notifications/tests/record-test.js
+++ lively.notifications/tests/record-test.js
@@ -1,11 +1,10 @@
-/*global beforeEach, afterEach, describe, it*/
+/* global beforeEach, afterEach, describe, it */
 
-import { expect } from "mocha-es6";
+import { expect } from 'mocha-es6';
 
-import { startRecording, stopRecording, clearRecord, emit, getRecord } from "../index.js";
+import { startRecording, stopRecording, clearRecord, emit, getRecord } from '../index.js';
 
-describe("record", () => {
-  
+describe('record', () => {
   beforeEach(() => {
     getRecord().limit = 10;
     clearRecord();
@@ -15,28 +14,28 @@ describe("record", () => {
   afterEach(() => {
     stopRecording();
   });
-  
-  it("notifications", () => {
-    emit("@test", {payload: 23});
-    expect(getRecord()).to.containSubset([{type: "@test", payload: 23}]);
+
+  it('notifications', () => {
+    emit('@test', { payload: 23 });
+    expect(getRecord()).to.containSubset([{ type: '@test', payload: 23 }]);
   });
-  
-  it("notifications unless stopped", () => {
+
+  it('notifications unless stopped', () => {
     stopRecording();
-    emit("@test", {payload: 23});
+    emit('@test', { payload: 23 });
     expect(getRecord().length).to.deep.equal(0);
   });
 
-  it("multiple notifications", () => {
-    emit("@test", {payload: 23});
-    emit("@test", {payload: 42});
+  it('multiple notifications', () => {
+    emit('@test', { payload: 23 });
+    emit('@test', { payload: 42 });
     expect(getRecord().length).to.deep.equal(2);
   });
 
-  it("multiple notifications unless limit", () => {
+  it('multiple notifications unless limit', () => {
     getRecord().limit = 1;
-    emit("@test", {payload: 23});
-    emit("@test", {payload: 42});
+    emit('@test', { payload: 23 });
+    emit('@test', { payload: 42 });
     expect(getRecord().length).to.deep.equal(1);
   });
 });
