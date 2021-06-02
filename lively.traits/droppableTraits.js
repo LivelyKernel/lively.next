d55e43cd174158b9ddbac3f0e03c7e9e7d72530f
Robin Schreiber <robin.schreiber@me.com>
2021-06-02 18:14:00 +0200

    traits: lint files

--- lively.traits/droppableTraits.js
+++ lively.traits/droppableTraits.js
@@ -1,43 +1,39 @@
-import { copy } from "lively.serializer2";
-import { pt } from "lively.graphics";
-import ObjectPackage, { addScript } from "lively.classes/object-classes.js";
+import { copy } from 'lively.serializer2';
+import { pt } from 'lively.graphics';
+import ObjectPackage, { addScript } from 'lively.classes/object-classes.js';
 
-function onDropOn(target){
-  console.log(target)
-  return
+function onDropOn (target) {
+  console.log(target);
 }
 
-function onDragStart(evt){
-  console.log(evt)
-  return
-  var newMorph = copy(this)
+function onDragStart (evt) {
+  console.log(evt);
+  return;
+  let newMorph = copy(this);
   newMorph.openInWorld();
-  var hand = evt.hand
-  hand.grab(newMorph)  
-  newMorph.position = pt(0,0)
+  let hand = evt.hand;
+  hand.grab(newMorph);
+  newMorph.position = pt(0, 0);
 }
 
-function onDragEnd(evt){
-  super.onDragEnd(evt)
-  var hand = evt.hand
-  hand.onDrop(evt)
+function onDragEnd (evt) {
+  // super.onDragEnd(evt)
+  let hand = evt.hand;
+  hand.onDrop(evt);
 }
 
-export async function makeDroppable(aMorph){
-   var functionsToApply = [onDragStart,onDragEnd,onDropOn]
-   let obj = $world.get(aMorph)   
+export async function makeDroppable (aMorph) {
+  let functionsToApply = [onDragStart, onDragEnd, onDropOn];
+  let obj = $world.get(aMorph);
 
-   if (!ObjectPackage.lookupPackageForObject(obj)) {
-     let pkg = ObjectPackage.withId("DroppedMethods");
-     await pkg.adoptObject(obj);
-   }  
-   
-   functionsToApply.forEach(async function(fn){     
-     var name = fn.name
-     console.log(name)
-     let {methodName} = await addScript(obj, fn, name);
-   })
-   
-   
-   
+  if (!ObjectPackage.lookupPackageForObject(obj)) {
+    let pkg = ObjectPackage.withId('DroppedMethods');
+    await pkg.adoptObject(obj);
+  }
+
+  functionsToApply.forEach(async function (fn) {
+    let name = fn.name;
+    console.log(name);
+    let { methodName } = await addScript(obj, fn, name);
+  });
 }
--- lively.traits/tests/test.js
+++ lively.traits/tests/test.js
@@ -1,6 +1,6 @@
-import { expect } from "mocha-es6";
-describe("lively.traits", () => {
-  it("works", () => {
+import { expect } from 'mocha-es6';
+describe('lively.traits', () => {
+  it('works', () => {
     expect(1 + 2).equals(3);
   });
 });
