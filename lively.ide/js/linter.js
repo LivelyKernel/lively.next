319f33270e93c7ee5f0201cb2e5560563a8f6e18
Robin Schreiber <robin.schreiber@me.com>
2021-10-04 09:00:30 +0000

    linter: remove async return

--- lively.ide/js/linter.js
+++ lively.ide/js/linter.js
@@ -1,14 +1,20 @@
 import { importModuleViaNative } from 'lively.resources';
 import config from 'https://jspm.dev/eslint-config-standard';
 
+let eslint;
+
+(async () => {
+  // fixme: this can become a normal import once we update babel to babel
+  eslint = await importModuleViaNative('https://jspm.dev/eslint');
+})();
+
 Object.assign(config.rules, {
   semi: ['error', 'always'],
   'no-extra-semi': 'error',
   'prefer-const': 'off'
 });
 
-export default async function lint (code) {
-  const eslint = await importModuleViaNative('https://jspm.dev/eslint');
+export default function lint (code) {
   const linter = new eslint.Linter();
   return linter.verifyAndFix(code, config).output;
 }
