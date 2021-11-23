f7370d4f2e8a4acb917903a27a6f167a51e3ab1b
Tarik <talnawa@outlook.de>
2021-11-23 10:24:25 +0100

    Fix error with styleClass comparison

--- lively.merger/differ.js
+++ lively.merger/differ.js
@@ -1,12 +1,13 @@
+import { Color } from 'lively.graphics';
 export class Differ {
   constructor (morph1id, morph2id) {
-    const morph1 = $world.submorphs.filter(morph => morph.id === morph1id);
-    const morph2 = $world.submorphs.filter(morph => morph.id === morph2id);
+    const morph1 = $world.submorphs.filter(morph => morph.id === morph1id)[0];
+    const morph2 = $world.submorphs.filter(morph => morph.id === morph2id)[0];
     this.diff(morph1, morph2);
   }
 
   diff (morph1, morph2) {
-    if (!morph1 || !morph2 && morph1.styleClasses !== morph2.styleClasses) {
+    if (!morph1 || !morph2 || JSON.stringify(morph1.styleClasses) != JSON.stringify(morph2.styleClasses)) {
       $world.setStatusMessage('Cant diff morphs, classes differ or morphs not found');
     } else {
       $world.setStatusMessage('Diffing');
