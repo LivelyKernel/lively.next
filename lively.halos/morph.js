4beab04c5bca76db22f7d97827c327ff487b7f2d
Robin Schreiber <robin.schreiber@me.com>
2021-12-08 16:20:42 +0100

    halo: fix halo disappearing on renaming morph

--- lively.halos/morph.js
+++ lively.halos/morph.js
@@ -562,7 +562,7 @@ export default class Halo extends Morph {
     }
     if (evtTarget == this && this.target.isWorld) { return this.remove(); }
 
-    if (evtTarget == this || evtTarget && !evtTarget.isHaloItem) {
+    if (evtTarget == this || evtTarget && !evtTarget.isHaloItem && !this.nameHalo().nameHolders.includes(evtTarget.owner)) {
       if (![this.target, ...this.target.ownerChain()].includes(this.morphBeneath(evt.position))) { this.remove(); }
     }
   }
