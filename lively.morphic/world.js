2e8b2fe71fddaefafa709708309560a91bbb22ce
Linus Hagemann <mail@linushagemann.de>
2022-01-06 17:57:38 +0100

    morphic: respect scrollbars for visible bounds of world

--- lively.morphic/world.js
+++ lively.morphic/world.js
@@ -332,8 +332,15 @@ export class World extends Morph {
 
   visibleBounds () {
     // the bounds call seems to slow down halos...
-    if (!this.env.renderer) return this.innerBounds();
-    return this.windowBounds().intersection(this.innerBounds());
+    let visibleBounds;
+    if (!this.env.renderer) visibleBounds = this.innerBounds();
+    else visibleBounds = this.windowBounds().intersection(this.innerBounds());
+    // if not the whole world is visible, we have to adjust for scrollbars on Win and Linux (on Mac they are transparent)
+    if (bowser.mac) return visibleBounds;
+    // it does not matter in which direction the space is too small, since we always get both scroll bars
+    if (visibleBounds.height < this.extent.y || visibleBounds.width < this.extent.x) visibleBounds.width = visibleBounds.width - this.scrollbarOffset.x;
+
+    return visibleBounds;
   }
 
   visibleBoundsExcludingTopBar () {
