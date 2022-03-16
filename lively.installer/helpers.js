def5c96cbd3fb223253d27395b7221e20a0e66e5
Linus Hagemann <mail@linushagemann.de>
2022-03-16 15:28:42 +0100

    installer: lint helpers.js

--- lively.installer/helpers.js
+++ lively.installer/helpers.js
@@ -1,23 +1,21 @@
-import { exec } from "./shell-exec.js";
-
-export function join() {
-  var args = Array.prototype.slice.call(arguments);
+export function join () {
+  let args = Array.prototype.slice.call(arguments);
   return args.reduce(function (path, ea) {
-      return typeof ea === 'string' ? path.replace(/\/*$/, '') + '/' + ea.replace(/^\/*/, '') : path;
+    return typeof ea === 'string' ? path.replace(/\/*$/, '') + '/' + ea.replace(/^\/*/, '') : path;
   });
 }
 
-export function normalizeProjectSpec(spec) {
+export function normalizeProjectSpec (spec) {
   return Object.assign({}, spec, {
     dir: spec.dir || join(spec.parentDir, spec.name)
   });
 }
 
-export function getPackageSpec() {
-  return System.decanonicalize("lively.installer/packages-config.json");
+export function getPackageSpec () {
+  return System.decanonicalize('lively.installer/packages-config.json');
 }
 
-export async function readPackageSpec(pkgSpec) {
-  if (pkgSpec.startsWith("/")) pkgSpec = "file://" + pkgSpec;
+export async function readPackageSpec (pkgSpec) {
+  if (pkgSpec.startsWith('/')) pkgSpec = 'file://' + pkgSpec;
   return JSON.parse(await lively.resources.resource(pkgSpec).read());
 }
