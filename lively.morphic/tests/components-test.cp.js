3e567db6e95978d4dd6a725545f284e9d4924b51
Robin Schreiber <robin.schreiber@me.com>
2022-04-08 10:27:57 +0200

    components: fix tests

--- lively.morphic/tests/components-test.cp.js
+++ lively.morphic/tests/components-test.cp.js
@@ -5,8 +5,6 @@ import { Color } from 'lively.graphics';
 import { obj } from 'lively.lang';
 import { serialize } from 'lively.serializer2';
 
-let c1, c2, c3, c4, d1, d2, d3;
-
 const TLA = component({
   name: 'tla',
   fill: Color.orange,
@@ -41,51 +39,49 @@ class TestViewModel extends ViewModel {
   }
 }
 
+const d1 = component({ name: 'd1', fill: Color.purple });
+const d2 = component({ mame: 'd2', fill: Color.black });
+const c1 = component({
+  name: 'c1',
+  fill: Color.red
+});
+const c2 = component({
+  name: 'c2',
+  defaultViewModel: TestViewModel,
+  fill: Color.green,
+  submorphs: [part(c1, {
+    name: 'alice',
+    borderColorTop: Color.red,
+    submorphs: [add(part(d1, { name: 'bob' }))]
+  })]
+});
+const d3 = component(c2, {
+  name: 'd3',
+  fill: Color.cyan,
+  submorphs: [
+    {
+      name: 'alice',
+      borderRadiusTopLeft: 42,
+      master: d1
+    }
+  ]
+});
+const c3 = component({
+  name: 'c3',
+  fill: Color.orange,
+  submorphs: [part(c2, {
+    name: 'foo',
+    submorphs: [{ name: 'alice', master: d2 }]
+  })]
+});
+const c4 = component(c3, {
+  name: 'c4',
+  submorphs: [
+    { name: 'foo', master: d3 }
+  ]
+});
+
 describe('components', () => {
-  beforeEach(() => {
-    d1 = component({ name: 'd1', fill: Color.purple });
-    d2 = component({ mame: 'd2', fill: Color.black });
-    c1 = component({
-      name: 'c1',
-      fill: Color.red
-    });
-    c2 = component({
-      name: 'c2',
-      defaultViewModel: TestViewModel,
-      fill: Color.green,
-      submorphs: [part(c1, {
-        name: 'alice',
-        borderColorTop: Color.red,
-        submorphs: [add(part(d1, { name: 'bob' }))]
-      })]
-    });
-    d3 = component(c2, {
-      name: 'd3',
-      fill: Color.cyan,
-      submorphs: [
-        {
-          name: 'alice', 
-          borderRadiusTopLeft: 42,
-          master: d1 
-        }
-      ] 
-    });
-    c3 = component({
-      name: 'c3',
-      fill: Color.orange,
-      submorphs: [part(c2, {
-        name: 'foo',
-        submorphs: [{ name: 'alice', master: d2 }]
-      })] 
-    });
-    c4 = component(c3, {
-      name: 'c4',
-      submorphs: [
-        { name: 'foo', master: d3 }
-      ]  
-    });
-  });
-  
   it('prevents accumulation of overridden props', () => {
     // let's create a couple of derived components from c1
     const A = component(c1, {
@@ -93,36 +89,36 @@ describe('components', () => {
       borderWidth: 5,
       borderRadius: 10
     });
-    
+
     const B = component(A, {
       fill: Color.orange,
       borderWidth: 0
     });
 
-    expect(A.master._overriddenProps.get(A)).to.eql({
+    expect(A.getComponent().master._overriddenProps.get(A.getComponent())).to.eql({
       fill: true, borderWidth: true, borderRadius: true
     });
 
-    expect(B.master._overriddenProps.get(B)).to.eql({
+    expect(B.getComponent().master._overriddenProps.get(B.getComponent())).to.eql({
       fill: true, borderWidth: true
     });
   });
-  
+
   it('properly adheres to overridden masters', () => {
     const inst3 = part(c3);
     const alice = inst3.getSubmorphNamed('alice');
     const foo = inst3.getSubmorphNamed('foo');
-    const masterAlice = c3.getSubmorphNamed('alice');
-    const masterFoo = c3.getSubmorphNamed('foo');
+    const masterAlice = c3.getComponent().getSubmorphNamed('alice');
+    const masterFoo = c3.getComponent().getSubmorphNamed('foo');
 
     // check if master component overridden props match
-    expect(masterAlice.master._appliedMaster).to.equal(d2);
+    expect(masterAlice.master._appliedMaster).to.equal(d2.getComponent());
     expect(masterFoo.master._overriddenProps.get(masterAlice)).to.eql({ master: true });
     // check if overridden props in part match
     alice.master.applyIfNeeded(true);
     expect(alice.master._appliedMaster).to.equal(masterAlice.master);
     expect(alice.master._overriddenProps.get(alice)).to.eql({});
-    
+
     expect(foo.master._overriddenProps.get(alice)).to.eql({});
 
     // check the appearance is according to the expectation
@@ -133,8 +129,8 @@ describe('components', () => {
     const inst4 = part(c4);
     const alice = inst4.getSubmorphNamed('alice');
     const foo = inst4.getSubmorphNamed('foo');
-    expect(foo.master.auto).to.equal(c4.get('foo').master, 'policy of foo is applied');
-    expect(alice.master.auto).to.equal(c4.get('alice').master, 'policy of alice is applied');
+    expect(foo.master.auto).to.equal(c4.getComponent().get('foo').master, 'policy of foo is applied');
+    expect(alice.master.auto).to.equal(c4.getComponent().get('alice').master, 'policy of alice is applied');
 
     expect(foo.master._overriddenProps.get(foo)).to.eql({}); // all overridden stuff is kept in the inline policies
     expect(foo.master._overriddenProps.get(alice)).to.eql({});
@@ -149,15 +145,16 @@ describe('components', () => {
           submorphs: [
             {
               name: 'bob',
-              master: d2 
+              master: d2
             }
           ]
         }
       ]
     });
+    const bc = b.getComponent();
     const inst = part(b);
-    expect(b.get('bob').master._overriddenProps.get(b.get('bob'))).to.eql({ master: true });
-    expect(inst.get('bob').master.auto).to.equal(b.get('bob').master);
+    expect(bc.get('bob').master._overriddenProps.get(bc.get('bob'))).to.eql({ master: true });
+    expect(inst.get('bob').master.auto).to.equal(bc.get('bob').master);
     expect(inst.get('bob').master._overriddenProps.get(inst.get('bob'))).to.eql({});
     expect(inst.get('alice').master._overriddenProps.get(inst.get('bob'))).to.eql({});
   });
@@ -176,14 +173,15 @@ describe('components', () => {
         }
       ]
     });
-    expect(t1.master.derivedMorph).not.to.be.undefined;
-    expect(t1.get('bob').master).not.to.be.undefined;
-    expect(t1.get('bob').master._overriddenProps.get(t1.get('bob')).fill).to.be.true;
+    const t1c = t1.getComponent();
+    expect(t1c.master.derivedMorph).not.to.be.undefined;
+    expect(t1c.get('bob').master).not.to.be.undefined;
+    expect(t1c.get('bob').master._overriddenProps.get(t1c.get('bob')).fill).to.be.true;
     const inst = part(c2, { name: 'troller', master: t1 }); // now we protocol the overridden props within the t1 hierarchy, but these adjusted masters are not yet carried over. Again, we need to mark the master components as "altered" so that the traversal of the master component hierarchy happens further and also reaches down to the bob morph. If this info is not saved within the components directly, the policies have no way of knowing they have to further traverse the hierarchy.
-    expect(inst.get('alice').master.managesMorph(inst.get('bob'))).to.be.true; 
-    expect(inst.get('alice').master.auto).to.eql(t1.get('alice').master);
+    expect(inst.get('alice').master.managesMorph(inst.get('bob'))).to.be.true;
+    expect(inst.get('alice').master.auto).to.eql(t1c.get('alice').master);
     expect(inst.master.auto, 'correctly overrides top level master').to.eql(t1);
-    expect(inst.get('bob').master.auto, 'preserves the masters that have not been altered').to.eql(t1.get('bob').master); // the inline overridden property in the master needs to take precedence here
+    expect(inst.get('bob').master.auto, 'preserves the masters that have not been altered').to.eql(t1c.get('bob').master); // the inline overridden property in the master needs to take precedence here
     expect(inst.get('bob').fill, 'styles the morphs according to the overridden properties').equals(Color.red);
   });
 
@@ -217,15 +215,14 @@ describe('components', () => {
       ]
     });
 
-    expect(t1.get('bob').master.auto).equals(d1);
+    expect(t1.getComponent().get('bob').master.auto).equals(d1);
   });
 
   it('serializes inline properties to symbolic expressions', () => {
     const inst = part(TLB);
     const snap = serialize(inst);
-    expect(snap.snapshot[snap.snapshot[inst.get('alice').id].props.master.value.id].props.auto.value).to.include('TLB.get("alice").master');
+    expect(snap.snapshot[snap.snapshot[inst.get('alice').id].props.master.value.id].props.auto.value).to.include('(TLB.isComponentDescriptor ? TLB.getComponent() : TLB).get("alice").master');
   });
-
   it('does not serialize bindings but reconstructs them reliably', () => {
     // instantiate a component with a view model and ensure that the
     // bindings are not put into the snapshot
