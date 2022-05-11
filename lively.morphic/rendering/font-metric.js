e2cc2ea78475b80b3f4f999327649bfe05310d88
Linus Hagemann <mail@linushagemann.de>
2022-05-11 11:51:45 +0200

    🎨: fix linter warnings in `font-measure.js`

--- lively.morphic/rendering/font-metric.js
+++ lively.morphic/rendering/font-metric.js
@@ -1,10 +1,6 @@
 /* global System, global, self */
-import { string, arr, obj } from 'lively.lang';
+import { arr, obj } from 'lively.lang';
 import FontDetector from './font-detector.js';
-import { cumulativeOffset } from './dom-helper.js';
-import config from '../config.js';
-
-const debug = !!config.onloadURLQuery['debug-font-metric'];
 
 const checkTimeout = 1000;
 
@@ -61,7 +57,7 @@ export default class FontMetric {
     this.element.name = 'fontMetric';
     this.setMeasureNodeStyles(this.element.style, true);
     parentEl.appendChild(this.element);
-    this._domMeasure = new DOMTextMeasure().install(doc, parentEl, debug);
+    this._domMeasure = new DOMTextMeasure().install(doc, parentEl, debug); // eslint-disable-line no-use-before-define
   }
 
   uninstall () {
@@ -88,7 +84,6 @@ export default class FontMetric {
       textStyleClasses, transform
     } = style;
     const el = this.element;
-    const rect = null;
     if (transform) transform = transform.inverse();
     el.textContent = text;
     Object.assign(el.style, {
@@ -105,7 +100,6 @@ export default class FontMetric {
     try {
       ({ width, height } = el.getBoundingClientRect());
     } catch (e) { return { width: 0, height: 0 }; }
-
     return { height, width };
   }
 
@@ -247,7 +241,6 @@ export default class FontMetric {
   }
 }
 
-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // font measuring inside text
 
@@ -278,18 +271,6 @@ function textlayerNodeForFontMeasure (morph) {
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // new text measure implementation
-
-// Unicode characters that are considered "extending", i.e. treated as a single
-// unit. The list below is based on
-// https://github.com/codemirror/CodeMirror/blob/master/src/util/misc.js#L122
-const extendingChars = /[\u0300-\u036f\u0483-\u0489\u0591-\u05bd\u05bf\u05c1\u05c2\u05c4\u05c5\u05c7\u0610-\u061a\u064b-\u065e\u0670\u06d6-\u06dc\u06de-\u06e4\u06e7\u06e8\u06ea-\u06ed\u0711\u0730-\u074a\u07a6-\u07b0\u07eb-\u07f3\u0816-\u0819\u081b-\u0823\u0825-\u0827\u0829-\u082d\u0900-\u0902\u093c\u0941-\u0948\u094d\u0951-\u0955\u0962\u0963\u0981\u09bc\u09be\u09c1-\u09c4\u09cd\u09d7\u09e2\u09e3\u0a01\u0a02\u0a3c\u0a41\u0a42\u0a47\u0a48\u0a4b-\u0a4d\u0a51\u0a70\u0a71\u0a75\u0a81\u0a82\u0abc\u0ac1-\u0ac5\u0ac7\u0ac8\u0acd\u0ae2\u0ae3\u0b01\u0b3c\u0b3e\u0b3f\u0b41-\u0b44\u0b4d\u0b56\u0b57\u0b62\u0b63\u0b82\u0bbe\u0bc0\u0bcd\u0bd7\u0c3e-\u0c40\u0c46-\u0c48\u0c4a-\u0c4d\u0c55\u0c56\u0c62\u0c63\u0cbc\u0cbf\u0cc2\u0cc6\u0ccc\u0ccd\u0cd5\u0cd6\u0ce2\u0ce3\u0d3e\u0d41-\u0d44\u0d4d\u0d57\u0d62\u0d63\u0dca\u0dcf\u0dd2-\u0dd4\u0dd6\u0ddf\u0e31\u0e34-\u0e3a\u0e47-\u0e4e\u0eb1\u0eb4-\u0eb9\u0ebb\u0ebc\u0ec8-\u0ecd\u0f18\u0f19\u0f35\u0f37\u0f39\u0f71-\u0f7e\u0f80-\u0f84\u0f86\u0f87\u0f90-\u0f97\u0f99-\u0fbc\u0fc6\u102d-\u1030\u1032-\u1037\u1039\u103a\u103d\u103e\u1058\u1059\u105e-\u1060\u1071-\u1074\u1082\u1085\u1086\u108d\u109d\u135f\u1712-\u1714\u1732-\u1734\u1752\u1753\u1772\u1773\u17b7-\u17bd\u17c6\u17c9-\u17d3\u17dd\u180b-\u180d\u18a9\u1920-\u1922\u1927\u1928\u1932\u1939-\u193b\u1a17\u1a18\u1a56\u1a58-\u1a5e\u1a60\u1a62\u1a65-\u1a6c\u1a73-\u1a7c\u1a7f\u1b00-\u1b03\u1b34\u1b36-\u1b3a\u1b3c\u1b42\u1b6b-\u1b73\u1b80\u1b81\u1ba2-\u1ba5\u1ba8\u1ba9\u1c2c-\u1c33\u1c36\u1c37\u1cd0-\u1cd2\u1cd4-\u1ce0\u1ce2-\u1ce8\u1ced\u1dc0-\u1de6\u1dfd-\u1dff\u200c\u200d\u20d0-\u20f0\u2cef-\u2cf1\u2de0-\u2dff\u302a-\u302f\u3099\u309a\ua66f-\ua672\ua67c\ua67d\ua6f0\ua6f1\ua802\ua806\ua80b\ua825\ua826\ua8c4\ua8e0-\ua8f1\ua926-\ua92d\ua947-\ua951\ua980-\ua982\ua9b3\ua9b6-\ua9b9\ua9bc\uaa29-\uaa2e\uaa31\uaa32\uaa35\uaa36\uaa43\uaa4c\uaab0\uaab2-\uaab4\uaab7\uaab8\uaabe\uaabf\uaac1\uabe5\uabe8\uabed\udc00-\udfff\ufb1e\ufe00-\ufe0f\ufe20-\ufe26\uff9e\uff9f]/;
-function isExtendingChar (ch) { return ch.charCodeAt(0) >= 768 && extendingChars.test(ch); }
-
-// Returns a number from the range [`0`; `str.length`] unless `pos` is outside that range.
-function skipExtendingChars (str, pos, dir) {
-  while ((dir < 0 ? pos > 0 : pos < str.length) && isExtendingChar(str.charAt(pos))) { pos = pos + dir; }
-  return pos;
-}
 
 class DOMTextMeasure {
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -436,7 +417,6 @@ class DOMTextMeasure {
         // render in once go, then read, not intermixed!
         const lineNodes = new Array(lines.length);
         const results = new Array(lines.length);
-        const { doc: document } = this;
         for (let i = 0; i < lines.length; i++) {
           if (results[i]) continue;
           const lineNode = renderLineFn(lines[i]);
@@ -474,16 +454,15 @@ class DOMTextMeasure {
       (textNode, textNodeOffsetLeft, textNodeOffsetTop) => {
         const lineNode = renderLineFn(line);
         const _ = textNode.appendChild(lineNode);
-        const { doc: document } = this;
         const result = (line.stringSize > 10000 &&
 
-                   charBoundsOfBigMonospacedLine(
+                   charBoundsOfBigMonospacedLine( // eslint-disable-line no-use-before-define
                      morph, fontMetric, line, lineNode,
                      offsetX,
                      offsetY,
                      styleOpts, renderTextLayerFn)) ||
 
-                charBoundsOfLine(line, lineNode,
+                charBoundsOfLine(line, lineNode, // eslint-disable-line no-use-before-define
                   offsetX - textNodeOffsetLeft,
                   offsetY - textNodeOffsetTop);
 
@@ -521,7 +500,7 @@ class DOMTextMeasure {
 
     const tfm = morph.getGlobalTransform().inverse();
     if (morph.env.renderer && morph.env.renderer.getNodeForMorph(morph) &&
-        (tfm.getScale() != 1 || tfm.getRotation() != 0)) {
+        (tfm.getScale() !== 1 || tfm.getRotation() !== 0)) {
       tfm.e = tfm.f = 0;
       textNode.style.transform = tfm.toString();
     }
@@ -553,6 +532,7 @@ const GLOBAL = typeof System !== 'undefined'
 const getComputedFontFamily = typeof GLOBAL.getComputedStyle === 'function'
   ? node => GLOBAL.getComputedStyle(node).fontFamily
   : () => '';
+
 const getComputedMarginLeft = typeof GLOBAL.getComputedStyle === 'function'
   ? node => parseInt(GLOBAL.getComputedStyle(node).marginLeft) || 0
   : () => 0;
@@ -563,15 +543,13 @@ function charBoundsOfBigMonospacedLine (
   styleOpts, directRenderTextLayerFn
 ) {
   const textLength = line.text.length;
-  const index = 0;
 
   if (textLength < 500 || fontMetric.isProportional(getComputedFontFamily(lineNode))) { return null; }
 
   let lineWidth = Infinity;
-  let lineHeight = Infinity;
   const { defaultTextStyle, lineWrapping } = styleOpts;
 
-  if (lineWrapping) { ({ width: lineWidth, height: lineHeight } = lineNode.getBoundingClientRect()); }
+  if (lineWrapping) { ({ width: lineWidth } = lineNode.getBoundingClientRect()); }
 
   const { width, height } = fontMetric._domMeasure.defaultCharExtent(
     morph, { defaultTextStyle }, directRenderTextLayerFn);
@@ -589,7 +567,7 @@ function charBoundsOfBigMonospacedLine (
 }
 
 function charBoundsOfLine (line, lineNode, offsetX = 0, offsetY = 0) {
-  const { ELEMENT_NODE, TEXT_NODE, childNodes } = lineNode;
+  const { ELEMENT_NODE, TEXT_NODE } = lineNode;
   const maxLength = Infinity;
   const document = lineNode.ownerDocument;
   const result = [];
@@ -606,7 +584,6 @@ function charBoundsOfLine (line, lineNode, offsetX = 0, offsetY = 0) {
   } else {
     const realLineNode = lineNode.getElementsByClassName('line')[0];
     node = realLineNode.childNodes[0];
-    const offsetNode = node;
 
     offsetX = offsetX - lineNode.offsetLeft + getComputedMarginLeft(lineNode);
     offsetY = offsetY - lineNode.offsetTop;
@@ -630,7 +607,7 @@ function charBoundsOfLine (line, lineNode, offsetX = 0, offsetY = 0) {
         // "right" bias for rect means that if we get multiple rects for a
         // single char (if it comes after a line break caused by wrapping, we
         // prefer the bounds on the next (the wrapped) line)
-        ({ left, top, width, height } = measureCharInner(document, textNode, i, 'right'));
+        ({ left, top, width, height } = measureCharInner(document, textNode, i, 'right')); // eslint-disable-line no-use-before-define
         x = left + offsetX;
         y = top + offsetY;
 
@@ -653,10 +630,10 @@ function charBoundsOfLine (line, lineNode, offsetX = 0, offsetY = 0) {
 
 function measureCharInner (document, node, index, bias = 'left') {
   let rect; let start = index; let end = index + 1;
-  if (node.nodeType == 3) { // If it is a text node, use a range to retrieve the coordinates.
+  if (node.nodeType === 3) { // If it is a text node, use a range to retrieve the coordinates.
     for (let i = 0; i < 4; i++) { // Retry a maximum of 4 times when nonsense rectangles are returned
-      rect = getUsefulRect(range(document, node, start, end).getClientRects(), bias);
-      if (rect.left || rect.right || start == 0) break;
+      rect = getUsefulRect(range(document, node, start, end).getClientRects(), bias); // eslint-disable-line no-use-before-define
+      if (rect.left || rect.right || start === 0) break;
       end = start;
       start = start - 1;
     }
@@ -675,13 +652,13 @@ function range (document, node, start, end, endNode) {
 
 function getUsefulRect (rects, bias) {
   let rect = { left: 0, right: 0, top: 0, bottom: 0 };
-  if (bias == 'left') {
+  if (bias === 'left') {
     for (let i = 0; i < rects.length; i++) {
-      if ((rect = rects[i]).left != rect.right) break;
+      if ((rect = rects[i]).left !== rect.right) break;
     }
   } else {
     for (let i = rects.length - 1; i >= 0; i--) {
-      if ((rect = rects[i]).left != rect.right) break;
+      if ((rect = rects[i]).left !== rect.right) break;
     }
   }
   return rect;
