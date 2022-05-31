8f5fb8a2e65537609c7d8934d455ad3ddd1c1a74
Robin Schreiber <robin.schreiber@me.com>
2022-05-31 12:27:22 +0200

    freezer: add utils

--- lively.freezer/src/util/helpers.js
+++ lively.freezer/src/util/helpers.js
@@ -1,13 +1,13 @@
-/* global require */
-/* global System,global,process,Buffer */
-import { toJsIdentifier } from 'lively.classes/util.js';
+/* global System,global,process,Buffer,require */
 import * as classes from 'lively.classes';
+import { toJsIdentifier } from 'lively.classes/util.js';
 import { resource } from 'lively.resources';
-import { runCommand } from 'lively.ide/shell/shell-interface.js';
 import { arr, Path, fun, promise } from 'lively.lang';
 import { es5Transpilation, stringifyFunctionWithoutToplevelRecorder } from 'lively.source-transform';
 
-const isNode = typeof System !== 'undefined'
+export const ROOT_ID = '\0__rootModule__';
+
+export const isNode = typeof System !== 'undefined'
   ? System.get('@system-env').node
   : (typeof global !== 'undefined' && typeof process !== 'undefined');
 
@@ -30,7 +30,7 @@ export function findUniqJsName (name, boundNames = []) {
 // problem: when we encode the compressed string in utf8 it gets fucked up during transmission
 // solution: when compressed string is sent between client and server translate to base64
 
-export async function gzip (blob, inflate = true) {
+export async function gzip (blob, inflate = true, useBrotli = false) {
   // if this is not node, perform a remote eval instead
   // blob is a string in utf8 format, convert it to base64 for safe transmission
   if (!isNode) {
@@ -41,10 +41,10 @@ export async function gzip (blob, inflate = true) {
       .map(backend => backend.coreInterface)
       .find(coreInterface => coreInterface instanceof RemoteCoreInterface);
     let res = await remoteInterface.runEvalAndStringify(`
-        let { gzip } = await System.import('lively.freezer/util.js');
+        let { gzip } = await System.import('lively.freezer/src/util/helpers.js');
         await gzip(${
            JSON.stringify(blob) // JSON.stringify(inflate ? blob : btoa(blob))
-        }, ${inflate});`);
+        }, ${inflate}, ${useBrotli});`);
       // if (inflate) {
       //   // if we instructed the server to compress, it sends the response in
       //   // base64, which we have to convert to utf8 such that we can
@@ -53,13 +53,20 @@ export async function gzip (blob, inflate = true) {
       // }
     return res;
   }
-  if (!System.get('@system-env').node) return;
+  if (!isNode) return;
   let zlib = require('zlib');
-  let gzipFunc = (inflate ? zlib.gzipSync : zlib.gunzipSync);
+  let compressionFn = useBrotli
+    ? (inflate ? zlib.brotliCompressSync : zlib.brotliDecompressSync)
+    : (inflate ? zlib.gzipSync : zlib.gunzipSync);
   blob = inflate ? blob : new Buffer(blob, 'base64');
-  let compressed = gzipFunc(blob);
-  return compressed.toString('utf8');
-  // return compressed.toString(inflate ? 'base64' : 'utf8');
+  let compressed = compressionFn(blob);
+  // return compressed.toString('utf8');
+  return compressed.toString(inflate ? 'base64' : 'utf8');
+}
+
+// await brotli(await brotli('hello'), false)
+export async function brotli (blob, inflate = true) {
+  return await gzip(blob, inflate, true);
 }
 
 /**
@@ -99,7 +106,7 @@ export function fixSourceForBugsInGoogleClosure (id, source) {
 }
 
 export async function evalOnServer (code) {
-  if (System.get('@system-env').node) {
+  if (isNode) {
     return eval(code);
   }
   const { default: EvalBackendChooser } = await System.import('lively.ide/js/eval-backend-ui.js');
@@ -120,68 +127,57 @@ export async function getConfig () {
     min: resource(System.decanonicalize('lively.freezer/tmp.min.js')),
     presetPath: await evalOnServer('System.decanonicalize(\'@babel/preset-env\').replace(\'file://\', \'\')'),
     babelPath: await evalOnServer('System.decanonicalize(\'@babel/cli/bin/babel.js\').replace(System.baseURL, \'../\')'),
-    babelConfig: resource(System.baseURL).join('lively.freezer/.babelrc'),
+    babelConfig: System.normalizeSync('lively.freezer/.babelrc'),
     pathToGoogleClosure: System.decanonicalize(`google-closure-compiler-${os === 'darwin' ? 'osx' : 'linux'}/compiler`).replace(System.baseURL, '../')
   };
 }
 
-export async function compileOnServer (code, loadingIndicator, fileName = 'data', useTerser) {
+export async function compileOnServer (code, resolver, useTerser) {
   const transpilationSpeed = 100000;
   const compressionSpeed = 150000;
   const { cwd, tmp, min, presetPath, babelPath, babelConfig, pathToGoogleClosure } = await getConfig();
   tmp.onProgress = (evt) => {
     // set progress of loading indicator
     const p = evt.loaded / evt.total;
-    loadingIndicator.progress = p;
-    loadingIndicator.status = `Sending ${fileName} to Google Closure: ` + (100 * p).toFixed() + '%';
+    resolver.setStatus({ progress: p, status: 'Sending code to Google Closure: ' + (100 * p).toFixed() + '%' });
   };
   min.onProgress = (evt) => {
     // set progress of loading indicator
     const p = evt.loaded / evt.total;
-    loadingIndicator.progress = p;
-    loadingIndicator.status = 'Retrieving compiled code...' + (100 * p).toFixed() + '%';
+    resolver.setStatus({ progress: p, status: 'Retrieving compiled code...' + (100 * p).toFixed() + '%' });
   };
-  await tmp.write(code);
+  await tmp.write(code); // write the file to the filesystem for working in the shell
   let c; const res = {};
-  if (System.get('@system-env').node) {
-    const { default: ServerCommand } = await System.import('lively.shell/server-command.js');
-    const cmd = new ServerCommand().spawn({
+  if (useTerser) {
+    // Terser fails to convert class definitions into functions, so we need to
+    // preprocess with babel transform
+    await babelConfig.writeJson({
+      presets: [[presetPath, { modules: false }]]
+    });
+    c = await resolver.spawn({ command: `${babelPath} -o tmp.es5.js tmp.js`, cwd });
+    resolver.setStatus({ status: 'Transpiling source...', progress: 0.01 });
+    for (const i of arr.range(0, code.length / transpilationSpeed)) {
+      await promise.delay(400);
+      if (c.status.startsWith('exited')) break;
+      resolver.setStatus({ progress: (i + 1) / (code.length / transpilationSpeed) });
+    }
+    await promise.waitFor(100 * 1000, () => c.status.startsWith('exited'));
+    c = await resolver.spawn({ command: 'terser --compress --mangle --comments false --ecma 5 --output tmp.min.js -- tmp.es5.js', cwd });
+  } else {
+    c = await resolver.spawn({
       command: `${pathToGoogleClosure} tmp.js > tmp.min.js --warning_level=QUIET`,
       cwd
     });
-    c = { status: 'started' };
-    cmd.on('stdout', stdout => c.status = 'exited');
-  } else {
-    if (useTerser) {
-      // Terser fails to convert class definitions into functions, so we need to
-      // preprocess with babel transform
-      await babelConfig.writeJson({
-        presets: [[presetPath, { modules: false }]]
-      });
-      c = await runCommand(`${babelPath} -o tmp.es5.js tmp.js`, { cwd });
-      loadingIndicator.status = `Transpiling ${fileName}...`;
-      loadingIndicator.progress = 0.01;
-      for (const i of arr.range(0, code.length / transpilationSpeed)) {
-        await promise.delay(400);
-        if (c.status.startsWith('exited')) break;
-        loadingIndicator.progress = (i + 1) / (code.length / transpilationSpeed);
-      }
-      await promise.waitFor(100 * 1000, () => c.status.startsWith('exited'));
-      c = await runCommand('terser --compress --mangle --comments false --ecma 5 --output tmp.min.js -- tmp.es5.js', { cwd });
-    } else {
-      c = await runCommand(`${pathToGoogleClosure} tmp.js > tmp.min.js --warning_level=QUIET`, { cwd });
-    }
   }
-  loadingIndicator.status = `Compressing ${fileName}...`;
-  loadingIndicator.progress = 0.01;
+  resolver.setStatus({ status: 'Compressing source files...', progress: 0.01 });
   for (const i of arr.range(0, code.length / compressionSpeed)) {
     await promise.delay(400);
     if (c.status.startsWith('exited')) break;
-    loadingIndicator.progress = (i + 1) / (code.length / compressionSpeed);
+    resolver.setStatus({ progress: (i + 1) / (code.length / compressionSpeed) });
   }
   await promise.waitFor(100 * 1000, () => c.status.startsWith('exited'));
   if (c.stderr && c.exitCode !== 0) {
-    loadingIndicator && loadingIndicator.remove();
+    resolver.finish();
     throw new Error(c.stderr);
   }
   res.code = code;
@@ -253,4 +249,82 @@ export function instrumentStaticSystemJS (system) {
   system.get('@lively-env').loadedModules = lively.FreezerRuntime.registry;
   system.baseURL = lively.FreezerRuntime.baseURL;
   system.trace = false;
+}
+
+/**
+ * Generates the html file that will serve as the index.html for loading the
+ * frozen morph. Note that this convenience method only works when we freeze
+ * instantiated morphs, it does not yet work when we bundle a library that
+ * auto executes.
+ * @param { Morph } part - The morph that is supposed to be frozen.
+ * @returns { string } The html code of the index.html.
+ */
+export async function generateLoadHtml (htmlConfig, importMap, resolver, modules) {
+  const htmlTemplate = await resource(await resolver.decanonicalizeFileName('lively.freezer/src/util/load-template.html')).read();
+  // fixme: what to do when we receive a module?
+  const loadCode = `
+    window.frozenPart = {
+      renderFrozenPart: (domNode, baseURL) => {
+        if (baseURL) System.config( { baseURL });
+        if (!baseURL) baseURL = './';
+        System.config({
+          meta: {
+           ${
+            modules.map(snippet =>
+              `[baseURL + '${snippet.fileName}']: {format: "system"}`
+            ).join(',\n') // makes sure that compressed modules are still recognized as such
+            }
+          }
+        });
+        System.import("./${ROOT_ID}").then(m => { System.trace = false; m.renderFrozenPart(domNode); });
+      }
+    }
+  `;
+  let title = htmlConfig.title || 'lively.next app';
+  let head = htmlConfig.head || '';
+  let load = htmlConfig.load || '';
+  let crawler = htmlConfig.crawler || '';
+  // extract stuff from the source code
+  if (importMap) {
+    head += importMap;
+  }
+  head += '<script>' + loadCode + '</script>';
+  return htmlTemplate
+    .replace('__TITLE_TAG__', title)
+    .replace('__HEAD_HTML__', head)
+    .replace('__LOADING_HTML__', load)
+    .replace('__CRAWLER_HTML__', crawler);
+}
+
+/**
+ * Handles the writing of all the files of a finished frozen bundle.
+ * @param { object } frozen - A finished build.
+ * @param { LoadingIndicator } li - Indicator to visualize the progress of writing the files.
+ * @param { object } handles - Resource handles that will be utilized to write to the proper directory.
+ * @param { HTTPResource } handles.dir - A http resource handle.
+ * @param { ShellResource } handles.shell - A shell resource handle for sending shell commands to the server in order to compress files.
+ */
+export async function writeFiles (frozen, li, { dir, shell, resolver }) {
+  const target = frozen.part || frozen.rootModule;
+  let currentFile = '';
+  dir.onProgress = (evt) => {
+    // set progress of loading indicator
+    const p = evt.loaded / evt.total;
+    li.progress = p;
+    li.status = 'Writing file ' + currentFile + ' ' + (100 * p).toFixed() + '%';
+  };
+
+  for (let file of frozen.output) {
+    let handle = dir.join(file.fileName);
+    if (await handle.exists()) await handle.remove();
+    await handle.ensureExistance(file.code || file.source);
+  }
+
+  li.remove();
+  const { StatusMessageConfirm } = await System.import('lively.halos/components/messages.cp.js');
+  $world.setStatusMessage(['Published. Click ', null, 'here', {
+    textDecoration: 'underline',
+    fontWeight: 'bold',
+    doit: { code: `window.open("${dir.join('index.html').url}")` }
+  }, ' to view.'], StatusMessageConfirm, false);
 }
---
+++ lively.freezer/src/util/main-module.js
@@ -0,0 +1,23 @@
+/* global main, prepare, WORLD_CLASS, TITLE */
+/**
+ * This module is a template to be used for any of the synthesized entry modules
+ * that are created when freezing a particular module as an entrypoint.
+ */
+import { MorphicEnv } from 'lively.morphic';
+import { World } from 'lively.morphic';
+import { pt } from 'lively.graphics';
+
+// fixme: what to do to make IDE worlds load? They require components before $world is a thing yet....
+export async function renderFrozenPart (node = document.body) {
+  prepare();
+  if (!MorphicEnv.default().world) {
+    let world = window.$world = window.$$world = new WORLD_CLASS({
+      extent: pt(window.innerWidth, window.innerHeight),
+      name: TITLE
+    });
+    world.stealFocus = true;
+    MorphicEnv.default().setWorldRenderedOn(world, node, window.prerenderNode);
+  }
+  // if IDE world required import that now and require that stuff too!
+  main();
+}
