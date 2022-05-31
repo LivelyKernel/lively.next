7622450cb9b231871799831c844d8bbe7b969a42
Robin Schreiber <robin.schreiber@me.com>
2022-05-31 11:27:32 +0200

    classes: lint code and support static class field definitions

--- lively.classes/class-to-function-transform.js
+++ lively.classes/class-to-function-transform.js
@@ -1,4 +1,4 @@
-import { arr, Path } from 'lively.lang';
+import { Path } from 'lively.lang';
 import { parse, stringify, query, nodes, BaseVisitor as Visitor } from 'lively.ast';
 
 let {
@@ -31,20 +31,33 @@ function ensureIdentifier (name) {
     .replace(trailingIdRe, '_');
 }
 
+function classFieldsInitialization (nodes) {
+  return nodes.map(({ key, value }) => {
+    let name = key.name;
+    if (key.type === 'PrivateIdentifier') name = '_' + name;
+    return assign(member('this', name), value);
+  });
+}
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-function constructorTemplate (name) {
-  // Creates a function like
-  // function CLASS() {
-  //   var firstArg = arguments[0];
-  //   if (firstArg && firstArg[Symbol.for("lively-instance-restorer")]) {
-  //     // for deserializing instances just do nothing
-  //   } else {
-  //     // automatically call the initialize method
-  //     this[Symbol.for("lively-instance-initialize")].apply(this, arguments);
-  //   }
-  // }
-
+/**
+ * Transforms a class definition to a function like:
+ * function CLASS() {
+ *   var firstArg = arguments[0];
+ *   if (firstArg && firstArg[Symbol.for("lively-instance-restorer")]) {
+ *     // for deserializing instances just do nothing
+ *   } else {
+ *     this._privateField1 = field1Value;
+ *     this._field2 = field2Value;
+ *     // automatically call the initialize method
+ *     this[Symbol.for("lively-instance-initialize")].apply(this, arguments);
+ *   }
+ * }
+ * @param {string} name - The name of the class that is being defined.
+ * @param {object[]} fields - The set of custom class fields that were defined by the code.
+ */
+function constructorTemplate (name, fields) {
   return funcExpr({ id: name ? id(name) : null }, ['__first_arg__'],
     ifStmt(
       binaryExpr(
@@ -53,6 +66,7 @@ function constructorTemplate (name) {
         member('__first_arg__', funcCall(member('Symbol', 'for'), literal('lively-instance-restorer')), true)),
       block(),
       block(
+        ...classFieldsInitialization(fields),
         exprStmt(
           funcCall(
             member(
@@ -67,40 +81,13 @@ const methodKindSymbol = Symbol();
 const tempLivelyClassVar = '__lively_class__';
 const tempLivelyClassHolderVar = '__lively_classholder__';
 
-class ClassReplaceVisitor extends Visitor {
-  accept (node, state, path) {
-    if (isFunctionNode(node)) {
-      state = {
-        ...state,
-        classHolder: objectLiteral([]),
-        currentMethod: node[methodKindSymbol] ? node : state.currentMethod
-      };
-    }
-
-    if (node.type === 'ClassExpression' || node.type === 'ClassDeclaration') { node = replaceClass(node, state, path, state.options); }
-
-    if (node.type === 'Super') { node = replaceSuper(node, state, path, state.options); }
-
-    if (node.type === 'MemberExpression' && node.object && node.object.type === 'Super') { node = replaceSuperGetter(node, state, path, state.options); }
-
-    if (node.type === 'AssignmentExpression' && node.left.type === 'MemberExpression' && node.left.object.type === 'Super') { node = replaceSuperSetter(node, state, path, state.options); }
-
-    if (node.type === 'CallExpression' && node.callee.type === 'Super') { node = replaceDirectSuperCall(node, state, path, state.options); }
-
-    if (node.type === 'CallExpression' && node.callee.object && node.callee.object.type === 'Super') { node = replaceSuperMethodCall(node, state, path, state.options); }
-
-    node = super.accept(node, state, path);
-
-    if (node.type === 'ExportDefaultDeclaration') { return splitExportDefaultWithClass(node, state, path, state.options); }
-
-    return node;
-  }
-
-  static run (parsed, options) {
-    let v = new this();
-    let classHolder = options.classHolder || objectLiteral([]);
-    return v.accept(parsed, { options, classHolder }, []);
-  }
+function splitExportDefaultWithClass (node, classHolder, path, options) {
+  return !node.declaration || !node.declaration[isTransformedClassVarDeclSymbol]
+    ? node
+    : [node.declaration, {
+        declaration: node.declaration.declarations[0].id,
+        type: 'ExportDefaultDeclaration'
+      }];
 }
 
 function replaceSuper (node, state, path, options) {
@@ -110,7 +97,6 @@ function replaceSuper (node, state, path, options) {
   let { currentMethod } = state;
   if (!currentMethod) {
     console.warn(`[lively.classes] Trying to transform es6 class but got super call outside a method! ${stringify(node)} in ${path.join('.')}`);
-    // return node;
   }
 
   let [parentReferencedAs, referencedAs] = path.slice(-2);
@@ -122,8 +108,6 @@ function replaceSuper (node, state, path, options) {
 
   return methodHolder;
 }
-
-// parse("class Foo extends Bar { get x() { return super.x; }}").body[0]
 
 function replaceSuperMethodCall (node, state, path, options) {
   // like super.foo()
@@ -190,22 +174,26 @@ function replaceClass (node, state, path, options) {
   let evalId = options.evalId;
   let sourceAccessorName = options.sourceAccessorName;
   let loc = node['x-lively-object-meta'] || { start, end };
+  const validMemberIdentifiers = ['Literal', 'Identifier', 'PrivateIdentifier', 'MemberExpression', 'CallExpression'];
 
-  let { inst, clazz } = body.reduce((props, propNode) => {
-    let decl; let { key, kind, value, static: classSide } = propNode;
+  let { inst, clazz, fields } = body.reduce((props, propNode) => {
+    let decl; let { key, kind, value, static: classSide, type } = propNode;
 
-    if (key.type !== 'Literal' && key.type !== 'Identifier') {
-      console.warn(`Unexpected key in classToFunctionTransform! ${JSON.stringify(key)}`);
+    if (!validMemberIdentifiers.includes(key.type)) {
+      console.warn(`Unexpected key in classToFunctionTransform! ${JSON.stringify(key)} -> ${stringify(propNode)}`);
     }
 
+    // all of the cases below may contain code blocks, that potentially reference private identifiers
+    // which we translate to underscore vars. These need to be transformed as well.
     if (kind === 'method') {
       // The name is just for debugging purposes when it appears in
       // native debuggers. We have to be careful about it b/c it shadows
       // outer functions / vars, something that is totally not apparent for a user
       // of the class syntax. That's the reason for making it a little cryptic
-      let methodId = id(className + '_' + ensureIdentifier(key.name || key.value || Path('property.name').get(key)) + '_');
+      let methodName = key.name || key.value || Path('property.name').get(key);
+      let methodId = id(className + '_' + ensureIdentifier(methodName || Path('arguments.0.value').get(key)) + '_');
       let props = [
-        'key', literal(key.name || key.value || Path('property.name').get(key)),
+        'key', methodName ? literal(methodName) : key,
         'value', { ...value, id: methodId, [methodKindSymbol]: classSide ? 'static' : 'proto' }];
 
       decl = objectLiteral(props);
@@ -218,17 +206,23 @@ function replaceClass (node, state, path, options) {
         'key', funcCall(member('Symbol', 'for'), literal('lively-instance-initialize')),
         'value', { ...value, id: id(className + '_initialize_'), [methodKindSymbol]: 'proto' }];
       decl = objectLiteral(props);
+    } else if (type === 'PropertyDefinition') {
+      // collect these for class field initializiation
+      props.fields.push(propNode);
     } else {
-      console.warn(`[lively.classes] classToFunctionTransform encountered unknown class property with kind ${kind}, ignoring it, ${JSON.stringify(propNode)}`);
+      console.warn(`[lively.classes] classToFunctionTransform encountered unknown class property with kind ${kind}, ignoring it, ${JSON.stringify(propNode)} -> ${stringify(propNode)}`);
     }
-    (classSide ? props.clazz : props.inst).push(decl);
+    if (decl) (classSide ? props.clazz : props.inst).push(decl);
     return props;
   }, {
     inst: [],
-    clazz: addClassNameGetter ? [
-      // explicitly add in a static property to ensure the class name is accessible also in google closure env
-      parse(`({ key: Symbol.for("__LivelyClassName__"), get: function get() { return "${className}"; } })`).body[0].expression
-    ] : []
+    fields: [],
+    clazz: addClassNameGetter
+      ? [
+          // explicitly add in a static property to ensure the class name is accessible also in google closure env
+          parse(`({ key: Symbol.for("__LivelyClassName__"), get: function get() { return "${className}"; } })`).body[0].expression
+        ]
+      : []
   });
 
   if (inst.length) instanceProps = { type: 'ArrayExpression', elements: inst };
@@ -285,9 +279,9 @@ function replaceClass (node, state, path, options) {
                 consequent: member(tempLivelyClassHolderVar, classId),
                 alternate: assign(
                   member(tempLivelyClassHolderVar, classId),
-                  constructorTemplate(classId.name))
+                  constructorTemplate(classId.name, fields))
               }
-            : classId ? constructorTemplate(classId.name) : constructorTemplate(null)),
+            : classId ? constructorTemplate(classId.name, fields) : constructorTemplate(null, fields)),
         returnStmt(
           funcCall(
             options.functionNode,
@@ -321,34 +315,68 @@ function replaceClass (node, state, path, options) {
   return result;
 }
 
-function splitExportDefaultWithClass (node, classHolder, path, options) {
-  return !node.declaration || !node.declaration[isTransformedClassVarDeclSymbol]
-    ? node
-    : [node.declaration, {
-        declaration: node.declaration.declarations[0].id,
-        type: 'ExportDefaultDeclaration'
-      }];
+function replacePrivateIdentifier (node) {
+  return { ...node, type: 'Identifier', name: '_' + node.name };
 }
 
-// var opts = {classHolder: {type: "Identifier", name: "_rec"}, functionNode: {type: "Identifier", name: "createOrExtendClass"}};
-// stringify(classToFunctionTransform(parse("class Foo extends Bar {m() { super.m(); }}"), opts))
-// stringify(classToFunctionTransform(parse("class Foo extends Bar {m() { super.m(arguments[1]); }}"), opts))
-// stringify(classToFunctionTransform(parse("class Foo {constructor() {}}"), opts))
+/**
+ * Custom AST transform visitor that applies all the different transformations
+ * we need to implement the custom lively class notation.
+ */
+class ClassReplaceVisitor extends Visitor {
+  accept (node, state, path) {
+    if (isFunctionNode(node)) {
+      state = {
+        ...state,
+        classHolder: objectLiteral([]),
+        currentMethod: node[methodKindSymbol] ? node : state.currentMethod
+      };
+    }
 
+    if (node.type === 'ClassExpression' || node.type === 'ClassDeclaration') { node = replaceClass(node, state, path, state.options); }
+
+    if (node.type === 'PrivateIdentifier') node = replacePrivateIdentifier(node);
+
+    if (node.type === 'Super') { node = replaceSuper(node, state, path, state.options); }
+
+    if (node.type === 'MemberExpression' && node.object && node.object.type === 'Super') { node = replaceSuperGetter(node, state, path, state.options); }
+
+    if (node.type === 'AssignmentExpression' && node.left.type === 'MemberExpression' && node.left.object.type === 'Super') { node = replaceSuperSetter(node, state, path, state.options); }
+
+    if (node.type === 'CallExpression' && node.callee.type === 'Super') { node = replaceDirectSuperCall(node, state, path, state.options); }
+
+    if (node.type === 'CallExpression' && node.callee.object && node.callee.object.type === 'Super') { node = replaceSuperMethodCall(node, state, path, state.options); }
+
+    node = super.accept(node, state, path);
+
+    if (node.type === 'ExportDefaultDeclaration') { return splitExportDefaultWithClass(node, state, path, state.options); }
+
+    return node;
+  }
+
+  static run (parsed, options) {
+    let v = new this();
+    let classHolder = options.classHolder || objectLiteral([]);
+    return v.accept(parsed, { options, classHolder }, []);
+  }
+}
+
+/**
+ * From
+ *   class Foo extends SuperFoo { m() { return 2 + super.m() }}
+ * produces something like
+ *   createOrExtend({}, {referencedAs: "SuperFoo", value: SuperFoo}, "Foo2", [{
+ *     key: "m",
+ *     value: function m() {
+ *       return 2 + this.constructor[superclassSymbol].prototype.m.call(this);
+ *     }
+ *   }])
+ * @param {string|object} sourceOrAst - The sourcecode or AST to apply the transformation to.
+ * @param { object } options
+ * @parma { object } options.functionNode
+ * @param { string } options.classHolder
+ */
 export function classToFunctionTransform (sourceOrAst, options) {
-  // required: options = {functionNode, classHolder}
-  // From
-  //   class Foo extends SuperFoo { m() { return 2 + super.m() }}
-  // produces something like
-  //   createOrExtend({}, {referencedAs: "SuperFoo", value: SuperFoo}, "Foo2", [{
-  //     key: "m",
-  //     value: function m() {
-  //       return 2 + this.constructor[superclassSymbol].prototype.m.call(this);
-  //     }
-  //   }])
-
-  // console.log(typeof sourceOrAst === "string" ? sourceOrAst : stringify(sourceOrAst))
-
   let parsed = typeof sourceOrAst === 'string' ? parse(sourceOrAst) : sourceOrAst;
   options.scope = query.resolveReferences(query.scopes(parsed));
 
