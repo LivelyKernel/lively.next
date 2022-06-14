ff9024d8f45a078744a39f26d451553e18c21f83
Robin Schreiber <robin.schreiber@me.com>
2022-06-14 11:16:30 +0200

    freezer: fix node.js resolver

--- lively.freezer/src/resolvers/node.cjs
+++ lively.freezer/src/resolvers/node.cjs
@@ -1,8 +1,11 @@
-/* global process, require */
+/* global process, require, module */
 const { findPackageConfig } = require('flatn/flatn-cjs.js');
-const { flatnResolve } = require('flatn/module-resolver.js');
+const babel = require('@babel/core');
+const { flatnResolve, findPackagePathForModule } = require('flatn/module-resolver.js');
 const path = require('node:path');
 const { builtinModules } = require('node:module');
+const child_process = require("node:child_process");
+
 
 // Problem: Just defering to rollup seems to bypass the flatn resolution mechanism
 // flatn 
@@ -18,13 +21,18 @@ function ensureFileFormat(url) {
   return url && url.startsWith('/') ? 'file://' + url : url;
 }
 
-function resolveModuleId (moduleName, importer) {
-  if (isAlreadyResolved(moduleName) && moduleName.startsWith('/')) return moduleName; // already fully resolved name
+// fixme: if we are bundling from a node.js script but targeting the browser, we need to properly resolve /npm- urls
+function resolveModuleId (moduleName, importer, context = 'node') {
   if (moduleName.startsWith('esm://cache/')) return moduleName.replace('esm://cache/', 'https://jspm.dev/'); // for now
+  if (importer && importer.startsWith('https://jspm.dev/')) {
+    if (moduleName.startsWith('/')) 'https://jspm.dev' + moduleName;
+    if (moduleName.startsWith('https://jspm.dev')) return moduleName;
+  }
+  if (isAlreadyResolved(moduleName) || moduleName.startsWith('/')) return moduleName; // already fully resolved name
   if (moduleName.startsWith('./') || moduleName.startsWith('../'))
     return null; // not our job to resolve relative imports?
   // still this needs to take into account the importer since we are node.js and package.json is IMPORTANT!
-  return flatnResolve(moduleName, importer);
+  return flatnResolve(moduleName, importer, context);
 }
 
 function detectFormatFromSource (source) {
@@ -37,10 +45,16 @@ async function normalizeFileName (fileName) {
   return require.resolve(fileName);
 }
 
-async function decanonicalizeFileName (fileName) {
+function decanonicalizeFileName (fileName) {
   if (isAlreadyResolved(fileName)) return fileName;
   // return await System.decanonicalize(fileName);
-  return require.resolve(fileName);
+  let url = require.resolve(fileName);
+  if (fileName.endsWith('.js') &&
+      !fileName.endsWith('index.js') &&
+      url.endsWith('index.js')) {
+    return url.replace('index.js', fileName.split('/').slice(-1)[0])
+  }
+  return url;
 }
 
 function resolvePackage (moduleName) {
@@ -49,13 +63,13 @@ function resolvePackage (moduleName) {
   return findPackageConfig(moduleName);
 }
 
-function dontTransform (moduleId) {
-  // return module(moduleId).dontTransform;
-  return [];
+function dontTransform (moduleId, knownGlobals) {
+  return knownGlobals;
 }
 
 function pathInPackageFor (moduleId) {
-  // return module(moduleId).pathInPackage();
+  const pkgPath = findPackagePathForModule(moduleId);
+  return moduleId.replace(pkgPath, '.');
 }
 
 function detectFormat (moduleId) {
@@ -76,15 +90,29 @@ function whenReady () {
 
 function spawn ({ command, cwd }) {
   // handle this natively...
-  // const cmd = new ServerCommand().spawn({ command, cwd });
-  // const c = { status: 'started' };
-  // cmd.on('stdout', stdout => c.status = 'exited');
-  // return c;
+  const c = child_process.exec(command, { cwd });
+  const res = { status: 'running' };
+  c.on('close', () => {
+    res.status = 'exited'
+  });
+  return res;
 }
 
 async function load(url) {
+  if (url === '@empty') return '';
   const { resource } = await import('lively.resources');
-  return await resource(ensureFileFormat(url)).read()
+  // also transpile the source code if this is a js file...
+  // in the future we should restrict it to cp.js or some
+  // more general lv.js files...
+  const code = await resource(ensureFileFormat(url)).read();
+  if (url.endsWith('.js') || url.endsWith('.cjs'))
+    return babel.transform(code, {
+      plugins: [
+        require('@babel/plugin-proposal-class-properties')
+      ]
+      // systemjs transform is not really needed
+    }).code;
+  return code;
 }
 
 const NodeResolver = {
@@ -101,6 +129,7 @@ const NodeResolver = {
   whenReady,
   spawn,
   builtinModules,
+  ensureFileFormat,
   load
 };
 
