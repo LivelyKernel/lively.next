59b82eff6f05731196a2187df329d668e976bc74
Linus Hagemann <mail@linushagemann.de>
2022-07-08 10:49:25 +0200

    🌳: add bundle

---
+++ lively.ast/build.js
@@ -0,0 +1,36 @@
+/* global process */
+import { rollup } from 'rollup';
+import jsonPlugin from '@rollup/plugin-json';
+import { lively } from 'lively.freezer/src/plugins/rollup';
+import resolver from 'lively.freezer/src/resolvers/node.cjs';
+
+const build = await rollup({
+  input: './index.js',
+  plugins: [
+    lively({ // we just need this one to apply the source transform to our lively classes stuff as well as utilizing the flatn resolution mechanism encapsulated in the resolver.
+      resolver,
+      minify: false,
+      includeLivelyAssets: false,
+      captureModuleScope: false,
+      asBrowserModule: false,
+      compress: false, // this should be disabled by default on node.js      
+      excludedModules: [
+	      'mocha-es6', 'mocha', // references old lgtg that breaks the build
+	      'rollup', // has a dist file that cant be parsed by rollup
+	      'lively.morphic',
+        'lively.components',
+        'lively.ide' // contains code not required for this install script
+      ],
+      map: {
+        astq: 'astq/src/astq.js' 
+      },
+    }),
+    jsonPlugin(),
+  ],
+});
+
+await build.write({
+  inlineDynamicImports: true,
+  file: 'astbundledverzimportant.cjs',
+  format: 'cjs'
+});
