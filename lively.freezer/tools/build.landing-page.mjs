e7ff8eb650a5ade0878ce44d1029028f3c47a2ad
Robin Schreiber <robin.schreiber@me.com>
2022-08-29 14:28:25 +0200

    ❄️: add build scripts for loading-screen and landing-page

---
+++ lively.freezer/tools/build.landing-page.mjs
@@ -0,0 +1,45 @@
+/* global process */
+import { rollup } from 'rollup';
+import jsonPlugin from '@rollup/plugin-json';
+import { babel } from '@rollup/plugin-babel';
+import { lively } from 'lively.freezer/src/plugins/rollup';
+import resolver from 'lively.freezer/src/resolvers/node.cjs';
+import PresetEnv from '@babel/preset-env';
+
+const build = await rollup({
+  input: './src/landing-page.cp.js',
+  shimMissingExports: true,  
+  plugins: [
+    lively({
+      autoRun: {
+        title: 'lively.next',
+        head: `<script> window.AUTH_SERVER_URL; </script>`
+      },
+      minify: true,
+      asBrowserModule: true,
+      excludedModules: [
+	'lively.collab',
+        'mocha-es6','mocha', // references old lgtg that breaks the build
+        'rollup', // has a dist file that cant be parsed by rollup
+        // other stuff that is only needed by rollup
+        '@babel/preset-env',
+        '@babel/plugin-syntax-import-meta',
+        '@rollup/plugin-json', 
+        '@rollup/plugin-commonjs',
+        'rollup-plugin-polyfill-node',
+        'babel-plugin-transform-es2015-modules-systemjs'
+      ],
+      resolver
+    }),
+    jsonPlugin(),
+    babel({
+     babelHelpers: 'bundled', 
+     presets: [PresetEnv]
+    })
+   ]
+});
+
+await build.write({
+  format: 'system',
+  dir: 'landing-page'
+});
