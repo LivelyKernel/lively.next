e26f55e83b8e1b3bba1665b8b80e26997b3080f7
Linus Hagemann <mail@linushagemann.de>
2022-09-05 14:33:11 +0200

    ❄️🎨: remove all references to Nunito font

--- lively.freezer/src/loading-screen.cp.js
+++ lively.freezer/src/loading-screen.cp.js
@@ -30,7 +30,6 @@ export class WorldLoadingScreen extends Morph {
   get __head_html__ () {
     return `
 <script> window.AUTH_SERVER_URL; </script>
-<style type="text/css">@import url("https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap");</style>`;
   }
 
   async onLoad () {
