daf273b97c30c72a588b279a5b19352910082464
Robin Schreiber <robin.schreiber@me.com>
2022-09-08 15:00:24 +0200

    🧰: fix number widget tests

--- lively.ide/tests/widget-test.js
+++ lively.ide/tests/widget-test.js
@@ -1,10 +1,10 @@
 /* global System, it, describe, beforeEach, afterEach */
-
-import { World, MorphicEnv } from 'lively.morphic';
+import { World, part, MorphicEnv } from 'lively.morphic';
 import { pt } from 'lively.graphics';
 import { expect } from 'mocha-es6';
 import { NumberWidget } from '../value-widgets.js';
 import { createDOMEnvironment } from 'lively.morphic/rendering/dom-helper.js';
+import { DefaultNumberWidget } from '../value-widgets.cp.js';
 
 let env, field;
 async function createMorphicEnv () {
@@ -29,40 +29,41 @@ describe('number widget', () => {
   afterEach(() => destroyMorphicEnv());
 
   it('it fits bounds to content', async () => {
-    field = new NumberWidget({
+    field = part(DefaultNumberWidget, {
       autofit: true, number: 0.5, min: 0, max: 1, floatingPoint: true
     });
+    field.openInWorld();
     let valueContainer = field.getSubmorphNamed('value');
     env.world.addMorph(field);
     await field.whenRendered();
     expect(valueContainer.fixedWidth).to.be.true;
-    expect(field.bounds()).equals(field.submorphBounds());
     expect(field.width).equals(field.getSubmorphNamed('value').width + 20);
-    expect(field.height).equals(field.getSubmorphNamed('value').height);
-    expect(field.width).equals(field.getSubmorphNamed('up').right);
-    expect(field.width).equals(field.getSubmorphNamed('down').right);
+    expect(field.height).greaterThan(field.getSubmorphNamed('value').height);
+    expect(field.width).approximately(field.getSubmorphNamed('up').right, .5);
+    expect(field.width).approximately(field.getSubmorphNamed('down').right, .5);
   });
 
   it('it allows to resize widgets', async () => {
-    field = new NumberWidget({
-      autofit: false, number: 0.5, min: 0, max: 1, floatingPoint: true, width: 100, height: 25
+    field = part(DefaultNumberWidget, {
+      autofit: false, number: 0.5, min: 0, max: 1, floatingPoint: true, width: 90, height: 25
     });
     let valueContainer = field.getSubmorphNamed('value');
     env.world.addMorph(field);
+    await field.whenRendered();
+    field.width += 10;
     await field.whenRendered();
     expect(field.width).equals(100, 'field has width');
     expect(field.height).equals(25, 'filed has height');
     expect(valueContainer.scaleToBounds).to.be.true;
     expect(valueContainer.fixedWidth).to.be.true;
-    field.layout.forceLayout();
     // all the other invariants apply as well
-    expect(field.width).equals(field.getSubmorphNamed('value').width + 20, 'value container stretches across field');
+    expect(field.width).approximately(field.getSubmorphNamed('value').width + 20, .5, 'value container stretches across field');
     expect(field.width).equals(field.getSubmorphNamed('up').right, 'up button aligned correctly');
     expect(field.width).equals(field.getSubmorphNamed('down').right, 'down button aligned correctly');
   });
 
   it('keeps bounds on copy', async () => {
-    field = new NumberWidget({
+    field = part(DefaultNumberWidget, {
       autofit: false,
       number: 0.5,
       min: 0,
