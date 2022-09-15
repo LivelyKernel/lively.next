68a45d2edbe8601b05c3792138a3be21d43ee67a
Robin Schreiber <robin.schreiber@me.com>
2022-09-15 15:52:33 +0200

    🧰: move reconciliation tests file

--- lively.ide/tests/components/reconciliation-test.cp.js
+++ lively.ide/tests/components/reconciliation-test.cp.js
@@ -1,9 +1,12 @@
-/* global describe, it, xit */
+/* global describe, it */
 import { expect } from 'mocha-es6';
 import { ComponentDescriptor, part, add, component } from 'lively.morphic';
 import { Color, pt } from 'lively.graphics';
+import { InteractiveComponentDescriptor } from '../components/editor.js';
 
-const e1 = ComponentDescriptor.for(() => component({
+component.DescriptorClass = InteractiveComponentDescriptor;
+
+const e1 = component({
   name: 'e1',
   fill: Color.red,
   submorphs: [
@@ -15,9 +18,9 @@ const e1 = ComponentDescriptor.for(() => component({
     },
     { name: 'bob', fill: Color.orange }
   ]
-}), {});
+});
 
-const e2 = ComponentDescriptor.for(() => component(e1, {
+const e2 = component(e1, {
   name: 'e2',
   fill: Color.yellow,
   submorphs: [
@@ -25,21 +28,20 @@ const e2 = ComponentDescriptor.for(() => component(e1, {
       name: 'alice',
       fill: Color.black
     },
-    { name: 'bob', master: e1 }, // DOES NOT INHERIT STRUCTURE!
+    { name: 'bob', master: e1 },
     add(part(e1, { name: 'foo', fill: Color.gray, submorphs: [{ name: 'bob', fill: Color.green }] })),
     add({ name: 'bar', borderRadius: 5, borderColor: Color.black, borderWidth: 2 })
   ]
-}), {});
+});
+
+component.DescriptorClass = ComponentDescriptor;
 
 describe('component definition reconciliation', () => {
-  // FIXME: These tests should go into lively.ide, since they are testing
-  //        tooling specific behavior
   it('allows to create a component proxy for editing the spec', async () => {
     // define an ad hoc component
     const c = await e2.edit(); // => returns a component morph from the spec that is auto mapping changes to the spec
-    expect(c.isComponent).to.be.true;
     c.get('alice').fill = Color.green;
-    expect(c.spec.get('alice').fill).to.eql(Color.green);
+    expect(e2.stylePolicy.getSubSpecFor('alice').fill).to.eql(Color.green);
   });
 
   it('allows to reify source code based on changes applied to its spec', () => {
@@ -55,8 +57,8 @@ describe('component definition reconciliation', () => {
     { name: 'bob', fill: Color.orange }
   ]
 })`);
-    e1.spec.get('alice').fill = Color.magenta;
-    e1.spec.get('alice').position = pt(100, 100);
+    e1.stylePolicy.getSubSpecFor('alice').fill = Color.magenta;
+    e1.stylePolicy.getSubSpecFor('alice').position = pt(100, 100);
     expect(e1.getSourceCode()).to.equal(`component({
   name: 'e1',
   fill: Color.red,
@@ -73,7 +75,7 @@ describe('component definition reconciliation', () => {
   });
 
   it('allows to instantiate a morph from the spec', () => {
-    const m = e1.spec.instantiate();
-    expect(m.master.auto).equals(e1); // better to reference the descritptor instead of the policy for auto update mechanism when components are directly manipulated or rewritten via codee.
+    const m = part(e1);
+    expect(m.master.parent).equals(e1.stylePolicy); // better to reference the descritptor instead of the policy for auto update mechanism when components are directly manipulated or rewritten via codee.
   });
 });
