eb39675328adfc0fd17875f518aa0cb4971315d7
Linus Hagemann <mail@linushagemann.de>
2022-09-26 19:46:56 +0200

    ❄️: fix buildscripts for landing page and loading screen for Linux

---
+++ lively.freezer/tools/build-landing-page.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. ../scripts/lively-next-flatn-env.sh
+lively_next_flatn_env "$(dirname "$(pwd)")"
+export FLATN_DEV_PACKAGE_DIRS=$FLATN_DEV_PACKAGE_DIRS:$(pwd);
+node --no-experimental-fetch --no-warnings --experimental-import-meta-resolve --experimental-loader ../flatn/resolver.mjs ./tools/build.landing-page.mjs
