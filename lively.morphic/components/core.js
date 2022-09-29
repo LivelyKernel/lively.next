cbdd5746508d5b6d04138017d148c1f8a506c175
Robin Schreiber <robin.schreiber@me.com>
2022-09-29 19:15:22 +0200

    🎨: support proper marking of added submorphs in components

--- lively.morphic/components/core.js
+++ lively.morphic/components/core.js
@@ -574,7 +574,10 @@ export function without (removedSiblingName) {
 export function add (props, before = null) {
   return {
     COMMAND: 'add',
-    props,
+    props: {
+      __wasAddedToDerived__: true,
+      ...props
+    },
     before
   };
 }
