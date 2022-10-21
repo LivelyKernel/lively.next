d303293b4465303e49f0f018be9b5ab4632fb25f
Robin Schreiber <robin.schreiber@me.com>
2022-10-21 18:15:01 +0200

    🧰: implement embedded morph reconciliation

--- lively.ide/components/helpers.js
+++ lively.ide/components/helpers.js
@@ -71,6 +71,14 @@ export function convertToExpression (aMorph, opts = {}) {
   };
 }
 
+export function getTextAttributesExpr (textMorph) {
+  const expr = convertToExpression(textMorph);
+  const rootPropNode = parse('(' + expr.__expr__ + ')').body[0].expression;
+  const { start, end } = getProp(rootPropNode, 'textAndAttributes').value; // eslint-disable-line no-use-before-define
+  expr.__expr__ = expr.__expr__.slice(start - 1, end);
+  return expr;
+}
+
 /**
  * Converts a certain value to a serializable expression. Requires a name of the property
  * it belongs to, in order to properly convert nested properties.
