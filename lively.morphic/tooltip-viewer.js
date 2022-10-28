7e5095f8d7abe7708a80c01b119685ad1c4f5e89
linusha <mail@linushagemann.de>
2022-10-28 15:39:13 +0200

    🧰👼🎛️: migrate `SystemTooltip` to `ViewModel` architecture
    tooltip

--- lively.morphic/tooltip-viewer.js
+++ lively.morphic/tooltip-viewer.js
@@ -1,55 +1,8 @@
-import { pt } from 'lively.graphics';
-import { Morph } from './morph.js';
-import { morph, touchInputDevice } from './helpers.js';
+
+import { touchInputDevice } from './helpers.js';
 import config from './config.js';
-
-export class Tooltip extends Morph {
-  static get properties () {
-    return {
-      hasFixedPosition: { defaultValue: true },
-      reactsToPointer: { defaultValue: false },
-      isEpiMorph: { defaultValue: true },
-      description: {
-        after: ['submorphs'],
-        derived: true,
-        get () {
-          const [descriptor] = this.submorphs;
-          return descriptor.value;
-        },
-        set (stringOrAttributes) {
-          const [descriptor] = this.submorphs;
-          descriptor.value = stringOrAttributes;
-          descriptor.fit();
-        }
-      },
-      submorphs: {
-        initialize () {
-          this.submorphs = [
-            new morph({
-              reactsToPointer: false,
-              type: 'label',
-              name: 'label',
-              width: 200
-            })
-          ];
-        }
-      }
-    };
-  }
-
-  update (target) {
-    this.position = target.globalBounds().bottomCenter().subPt(target.world().scroll).addPt(pt(0, 7));
-    const visibleWorldBounds = target.world().visibleBoundsExcludingTopBar().insetBy(10);
-    const adjustedLabelBounds = visibleWorldBounds.translateForInclusion(this.bounds());
-    this.setBounds(adjustedLabelBounds.translatedBy(target.world().scroll.negated()));
-  }
-
-  async softRemove (cb) {
-    await this.animate({ opacity: 0, duration: 300 });
-    cb && cb(this);
-    this.remove();
-  }
-}
+import { SystemTooltip } from './tooltips.cp.js';
+import { part } from 'lively.morphic';
 
 export class TooltipViewer {
   constructor (world) {
@@ -146,13 +99,9 @@ export class TooltipViewer {
     }
     this.clearCurrentTooltip();
     const position = hand ? hand.position.addXY(10, 7) : morph.globalBounds().bottomRight();
-    this.currentTooltip = new Tooltip({ position, description: morph.tooltip });
-    // setting the master in the tooltoip definition above directly leads to be tooltip.cs.js being imported on file-level
-    // which causes circular imports and breaks the system
-    System.import('lively.morphic/tooltips.cp.js').then(({ SystemTooltip }) => {
-      this.currentTooltip.master = SystemTooltip;
-      $world.addMorph(this.currentTooltip);
-      this.currentTooltip.update(morph);
-    });
+    this.currentTooltip = part(SystemTooltip, { position });
+    this.currentTooltip.description = morph.tooltip;
+    $world.addMorph(this.currentTooltip);
+    this.currentTooltip.update(morph);
   }
 }
