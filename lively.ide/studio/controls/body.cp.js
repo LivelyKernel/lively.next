f703bde1d28b56390521f85bc1208c4c496a464f
Robin Schreiber <robin.schreiber@me.com>
2022-10-30 23:48:05 +0900

    🧰: explicitly pass null when adding dynamic property without a prop
    Co-authored-by: linusha <mail@linushagemann.de>

--- lively.ide/studio/controls/body.cp.js
+++ lively.ide/studio/controls/body.cp.js
@@ -140,7 +140,7 @@ export class BodyControlModel extends PropertySectionModel {
   activate () {
     this.view.layout = this.view.layout.with({ padding: rect(0, 10, 0, 10) });
     this.view.master = this.activeSectionComponent; // eslint-disable-line no-use-before-define
-    this.addDynamicProperty(false, false);
+    this.addDynamicProperty(null, false);
   }
 
   /**
