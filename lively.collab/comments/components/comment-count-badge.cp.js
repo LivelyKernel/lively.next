882a8c9f2ea64eca12231455611a0fe4179e2809
Robin Schreiber <robin.schreiber@me.com>
2022-10-31 14:27:07 +0900

    💭: fix comment count badge style

--- lively.collab/comments/components/comment-count-badge.cp.js
+++ lively.collab/comments/components/comment-count-badge.cp.js
@@ -1,25 +1,27 @@
 import { component } from 'lively.morphic/components/core.js';
-import { Color } from 'lively.graphics';
-import { TilingLayout, Ellipse, Label } from 'lively.morphic';
+import { Color, Rectangle } from 'lively.graphics';
+import { TilingLayout, ShadowObject, Ellipse, Label, part } from 'lively.morphic';
 import { CommentCountBadgeModel } from '../comment-count-badge.js';
 
 // part(CommentCountBadge).openInWorld();
 const CommentCountBadge = component({
   name: 'comment count badge',
-  type: Ellipse,
   defaultViewModel: CommentCountBadgeModel,
   fill: Color.rgb(255, 23, 68),
+  borderRadius: 50,
+  hasFixedPosition: true,
   layout: new TilingLayout({
-    hugContentsHorizontally: true,
     hugContentsVertically: true,
+    hugContentsHorizontally: true,
     wrapSubmorphs: false,
-    padding: 4
+    padding: Rectangle.inset(5, 0, 5, 0)
   }),
+  dropShadow: new ShadowObject({ distance: 0, blur: 2 }),
   submorphs: [{
     type: Label,
     name: 'badge label',
     fontColor: Color.rgb(255, 255, 255),
-    textAndAttributes: [10, null]
+    textAndAttributes: ['10', null]
   }],
   tooltip: '10 unresolved comment',
   epiMorph: true
