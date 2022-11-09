02504ed388fe6c7eef9314f9a1f6013953d47cf8
linusha <mail@linushagemann.de>
2022-11-09 19:27:24 +0100

    🧰: remove fading on blur from `TextFormattingPopUp`

--- lively.ide/text/rich-text.js
+++ lively.ide/text/rich-text.js
@@ -2,7 +2,6 @@
 import { part, easings } from 'lively.morphic';
 import { Text } from 'lively.morphic';
 import { TextFormattingPopUp } from './text-formatting-popup.cp.js';
-import { Rectangle } from 'lively.graphics';
 
 export class RichTextFormattableText extends Text {
   static get properties () {
@@ -26,7 +25,6 @@ export class RichTextFormattableText extends Text {
   }
 
   /**
-   * description
    * @param {boolean} imm - If true, do not animate and instead remove the popup immediately.
    * @returns {type} description
    */
@@ -34,7 +32,7 @@ export class RichTextFormattableText extends Text {
     if (!this.formattingPopUp) return;
     if (!imm) {
       await this.formattingPopUp.animate({
-        duration: 2000,
+        duration: 200,
         opacity: 0,
         easing: easings.inOutQuad
       });
@@ -43,10 +41,8 @@ export class RichTextFormattableText extends Text {
     this.formattingPopUp = null;
   }
 
-  onHoverOut (evt) {
-    super.onHoverOut(evt);
-    if (!this.formattingPopUp?.bounds().outsetByRect(Rectangle.fromTuple([10, 10, 10, 10])).containsPoint(evt.hand.position)) {
-      this.removeFormattingPopUp();
-    }
+  remove () {
+    this.removeFormattingPopUp(true);
+    super.remove();
   }
 }
