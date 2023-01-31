cef9fd289f7a4493d4e0fd3108c41682b42be4d5
Robin Schreiber <robin.schreiber@me.com>
2023-01-31 12:22:27 +0100

    🎨: avoid incorrect scroll layer adjustment request in text morphs

--- lively.morphic/text/morph.js
+++ lively.morphic/text/morph.js
@@ -2109,7 +2109,11 @@ export class Text extends Morph {
     const textAndAttributes = this.document.textAndAttributes;
     this.textString = '';
     this.document = null;
-    this.renderingState.needsScrollLayerRemoved = true;
+    if (this.renderingState.needsScrollLayerAdded) {
+      this.renderingState.needsScrollLayerAdded = false;
+    } else {
+      this.renderingState.needsScrollLayerRemoved = true;
+    }
     this.requestTextLayoutMeasuring();
     this.textLayout = null;
     this.selection = null;
