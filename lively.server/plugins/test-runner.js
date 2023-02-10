1078e3df5d679f4f2b57d31321bdc7de95cd88dd
linusha <mail@linushagemann.de>
2023-02-10 20:41:36 +0100

    👔: lint `test-runner.js`

--- lively.server/plugins/test-runner.js
+++ lively.server/plugins/test-runner.js
@@ -1,13 +1,12 @@
 import { HeadlessSession } from 'lively.headless';
 
 export default class TestRunner {
-
-  async setup(livelyServer) {
-    console.log('[Test Runner] Started')
+  async setup (livelyServer) {
+    console.log('[Test Runner] Started');
   }
 
-  async run(module_to_test) {
-    let results
+  async run (module_to_test) {
+    let results;
     try {
     this.headlessSession = new HeadlessSession();
     await this.headlessSession.open('http://localhost:9011/worlds/load?name=__newWorld__&showsUserFlap=false',  (sess) => sess.runEval(`$world.name == 'aLivelyWorld'`))
@@ -20,25 +19,24 @@ export default class TestRunner {
     JSON.stringify(results)
     `)
     } catch (err) {
-      results = JSON.stringify({'error': err.message});
-    }
-    finally {
-      this.headlessSession.dispose()
-      return results
+      results = JSON.stringify({ error: err.message });
+    } finally {
+      this.headlessSession.dispose();
+      return results;
     }
   }
 
-  get pluginId() { return 'TestRunner'; }
+  get pluginId () { return 'TestRunner'; }
 
-  async handleRequest(req, res, next) {
+  async handleRequest (req, res, next) {
     if (!req.url.startsWith('/subserver/TestRunner')) {
       return next();
     }
     if (req.url.startsWith('/subserver/TestRunner/')) {
       const [_, module_to_test] = req.url.split('TestRunner/');
-      const results = await this.run(module_to_test)
+      const results = await this.run(module_to_test);
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(results);
     }
   }
-} 
+}
