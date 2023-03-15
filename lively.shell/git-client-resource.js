c004a10fb3907ab621f747ebe44b011ac17795ee
linusha <mail@linushagemann.de>
2023-03-15 19:41:15 +0100

    🐚: always ensure correct working dir for git commands

--- lively.shell/git-client-resource.js
+++ lively.shell/git-client-resource.js
@@ -1,31 +1,27 @@
 
-import { runCommand } from './client-command.js';
-
 import ShellClientResource from './client-resource.js';
 
 export default class GitShellResource extends ShellClientResource {
   constructor (url, l2lClient, options = {}) {
     url = url.replace('git\/', '');
     super(url);
+    this.options.cwd = this.url;
   }
 
   async isGitRepository (withRemote = false) {
     const isDir = await this.isDirectory();
     if (!isDir) return false;
-    // FIXME: this dictates that this function gets executed before any other of the git operations can be performed
-    // would be nicer to make that transparent
-    this.options.cwd = this.url;
-    let cmd = runCommand('test -d ".git"', this.options);
+
+    let cmd = this.runCommand('test -d ".git"');
     await cmd.whenDone();
     if (!withRemote) return cmd.exitCode === 0;
-    cmd = runCommand('git remote', this.options);
+    cmd = this.runCommand('git remote');
     await cmd.whenDone();
     return cmd.exitCode === 0 && cmd.stdout !== '';
   }
 
   async initializeGitRepository () {
-    if (this.options.cwd !== this.url) this.options.cwd = this.url;
-    const cmd = runCommand('git init -b main', this.options);
+    const cmd = this.runCommand('git init -b main');
     await cmd.whenDone();
   }
 
@@ -36,31 +32,31 @@ export default class GitShellResource extends ShellClientResource {
               -H "Authorization: Bearer ${token}" \
               https://api.github.com/user/repos \
               -d '{"name":"${repoName}"}'`;
-    let cmd = runCommand(repoCreationCommand, this.options);
+    let cmd = this.runCommand(repoCreationCommand);
     await cmd.whenDone();
     let addingRemoteCommand = `git remote add origin https://${token}@github.com/${repoUser}/${repoName}.git`;
-    await runCommand(addingRemoteCommand, this.options).whenDone();
+    await this.runCommand(addingRemoteCommand).whenDone();
   }
 
   async commitRepo () {
     let cmdString = `git add . && git commit -m "Commited from withing lively.next at ${Date.now()}"`;
-    let cmd = runCommand(cmdString, this.options);
+    let cmd = this.runCommand(cmdString);
     await cmd.whenDone();
   }
 
   async pushGitRepo () {
     await this.pullGitRepo();
     await this.commitRepo();
-    const cmd = runCommand('git push --set-upstream origin main --force', this.options);
+    const cmd = this.runCommand('git push --set-upstream origin main --force');
     await cmd.whenDone();
   }
 
   // TODO: functioning error handling, especially in the case of conflicts
   async pullGitRepo () {
-    await runCommand('git stash', this.options).whenDone();
-    const pullCmd = runCommand('git pull', this.options);
+    await this.runCommand('git stash').whenDone();
+    const pullCmd = this.runCommand('git pull');
     await pullCmd.whenDone();
-    const cmd = runCommand('git stash pop', this.options);
+    const cmd = this.runCommand('git stash pop');
     await cmd.whenDone();
   }
 }
