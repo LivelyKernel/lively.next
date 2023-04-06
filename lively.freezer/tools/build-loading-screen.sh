91a41c15ea22da85935447830821ed3ac1b67202
Robin Schreiber <robin.schreiber@me.com>
2023-04-06 14:44:52 +0200

    ❄️: add no experimental fetch flag to loading screen build script

--- lively.freezer/tools/build-loading-screen.sh
+++ lively.freezer/tools/build-loading-screen.sh
@@ -2,4 +2,4 @@
 . ../scripts/lively-next-flatn-env.sh
 lively_next_flatn_env "$(dirname "$(pwd)")"
 export FLATN_DEV_PACKAGE_DIRS=$FLATN_DEV_PACKAGE_DIRS:$(pwd);
-node --no-warnings --experimental-import-meta-resolve --experimental-loader ../flatn/resolver.mjs ./tools/build.loading-screen.mjs
+node --no-warnings --no-experimental-fetch --experimental-import-meta-resolve --experimental-loader ../flatn/resolver.mjs ./tools/build.loading-screen.mjs
