1e23ec7e8acfb166ec4583c79583eccc82d07691
linusha <mail@linushagemann.de>
2023-04-12 15:51:28 +0200

    🎨: correctly mount and unmount CSS for HTML morph together with morph

--- lively.morphic/tests/html-morph-test.js
+++ lively.morphic/tests/html-morph-test.js
@@ -1,4 +1,4 @@
-/* global it, describe, beforeEach, afterEach, System */
+/* global it, describe, before, after */
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { createDOMEnvironment } from '../rendering/dom-helper.js';
 import { MorphicEnv } from '../index.js';
@@ -7,20 +7,20 @@ import { morph } from '../index.js';
 import { HTMLMorph } from '../html-morph.js';
 import { pt } from 'lively.graphics';
 
-let world, env;
+let world, env, m;
 function createDummyWorld () {
   return world = morph({ type: 'world', name: 'world', extent: pt(300, 300), env });
 }
 
 describe('html morph', function () {
-  // jsdom sometimes takes its time to initialize...
-  if (System.get('@system-env').node) { this.timeout(10000); }
-
-  beforeEach(async () => env = await MorphicEnv.pushDefault(new MorphicEnv(await createDOMEnvironment())).setWorld(createDummyWorld()));
-  afterEach(() => MorphicEnv.popDefault().uninstall());
+  before(async () => env = await MorphicEnv.pushDefault(new MorphicEnv(await createDOMEnvironment())).setWorld(createDummyWorld()));
+  after(() => {
+    MorphicEnv.popDefault().uninstall();
+    m.remove();
+  });
 
   it('renders html', async () => {
-    let m = world.addMorph(new HTMLMorph({ html: '<div>This is a <h2>test</h2></div>', env }));
+    m = world.addMorph(new HTMLMorph({ html: '<div>This is a <h2>test</h2></div>', env }));
     world.env.forceUpdate();
     expect(m.domNode.innerHTML).equals('<div>This is a <h2>test</h2></div>', 'initial rendering wrong');
     expect(m.domNode.parentNode).equals(env.renderer.getNodeForMorph(m), 'rendered node not child node of morph node');
@@ -28,5 +28,18 @@ describe('html morph', function () {
     m.position = pt(10, 20);
     expect(m.domNode).equals(node, 'node not the same after morph change');
     expect(m.domNode.parentNode).equals(env.renderer.getNodeForMorph(m), 'custom node child node of morph node after change');
+  });
+
+  it('mounts and removes custom CSS node', async () => {
+    m = world.addMorph(new HTMLMorph({ html: '<div>This is a <h2>test</h2></div>', env, name: 'testMorph' }));
+    m.cssDeclaration = '.p { background: green; }';
+    world.env.forceUpdate();
+    const id = m.id;
+    let cssNode = env.domEnv.document.getElementById('css-for-' + id);
+    expect(cssNode).to.be.ok;
+    m.remove();
+    world.env.forceUpdate();
+    cssNode = env.domEnv.document.getElementById('css-for-' + id);
+    expect(cssNode).to.not.be.ok;
   });
 });
