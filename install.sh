aa130c07eea875115ee1ee8e89fa56e206d8dab8
linusha <mail@linushagemann.de>
2023-05-04 14:53:13 +0200

    📦🛠️: make node version check consistent

--- install.sh
+++ install.sh
@@ -4,18 +4,7 @@
 
 lv_next_dir=$PWD
 
-NODE_VERSION=$(node -v)
-NODE_VERSION=$(echo "$NODE_VERSION" | sed -En 's/v([0-9]+)\..*/\1/p')
-
-if [[ $NODE_VERSION -lt 18 ]]; then
-  echo -n 'your node version is not supported. please use node 18.X.'; echo;
-  exit 1;
-fi
-
-if [[ $NODE_VERSION -ge 19 ]]; then
-  echo -n 'your node version is too new, and has known issues. Please use 18.X.'; echo;
-  exit 1;
-fi
+./scripts/node_version_checker.sh || exit 1
 
 export PATH=$lv_next_dir:$lv_next_dir/flatn/bin:$PATH
 export FLATN_PACKAGE_DIRS=
@@ -24,7 +13,7 @@ eval $(node -p 'let PWD=process.cwd();let packages = JSON.parse(require("fs").re
 mkdir lively.next-node_modules
 mkdir esm_cache
 
-node --no-experimental-fetch --inspect --no-warnings --experimental-loader $lv_next_dir/flatn/resolver.mjs \
+node --no-experimental-fetch --no-warnings --experimental-loader $lv_next_dir/flatn/resolver.mjs \
      lively.installer/bin/install.cjs $PWD \
 
 if [ ! "$CI" ];
