cea05534dedf7e119d0e4721418d1d8559dc1997
linusha <mail@linushagemann.de>
2023-05-17 12:49:23 +0200

    🛠️: ensure `local_projects` folder exists after installation

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ export FLATN_PACKAGE_COLLECTION_DIRS=$lv_next_dir/lively.next-node_modules
 eval $(node -p 'let PWD=process.cwd();let packages = JSON.parse(require("fs").readFileSync(PWD+"/lively.installer/packages-config.json")).map(ea => require("path").join(PWD, ea.name));`export FLATN_DEV_PACKAGE_DIRS=${packages.join(":")}`')                                                              
 mkdir lively.next-node_modules
 mkdir esm_cache
+mkdir local_projects
 
 node --no-experimental-fetch --no-warnings --experimental-loader $lv_next_dir/flatn/resolver.mjs \
      lively.installer/bin/install.cjs $PWD \
