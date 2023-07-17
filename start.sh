2f0e0cb5955b12173961097f25ba4fff12feaf36
Robin Schreiber <robin.schreiber@me.com>
2023-07-17 11:28:07 +0200

    🛠️: switch start.sh to loop based restarting machanism
    The previous version of this script caused weird behavior on macos, where restarting worked exactly three times, but stopped afterwards with the server just being terminated.
    As of writing this, we could not figure out why exactly that is.

--- start.sh
+++ start.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-debug=''
-trap './start-server.sh "$debug"' TERM
-if [ "$1" = "--debug" ]; then
-  debug='--debug'
-  ./start-server.sh --debug
-fi
-./start-server.sh
+# The path to your start.sh script
+START_SCRIPT_PATH="./start-server.sh"
+
+# This function is called when the script receives a SIGTERM signal
+handle_sigterm() {
+  echo "Received SIGTERM, will restart"
+}
+
+# This function is called when the script receives a SIGINT signal (Ctrl+C)
+handle_sigint() {
+  echo "Received SIGINT, will exit"
+  exit 1
+}
+
+# Trap SIGTERM and SIGINT signals, and specify the functions to handle them
+trap 'handle_sigterm' SIGTERM
+trap 'handle_sigint' SIGINT
+
+while true
+do
+  bash "$START_SCRIPT_PATH" "$1"
+  sleep 1
+done
