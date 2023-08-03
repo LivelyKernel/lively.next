533f9cd62e36bc08f2a1ece5b10cb6d40a436c90
linusha <mail@linushagemann.de>
2023-08-03 12:57:45 +0200

    🛠️: ensure main `partsbin` exists and is up to date in `install.sh`

--- install.sh
+++ install.sh
@@ -13,6 +13,39 @@ eval $(node -p 'let PWD=process.cwd();let packages = JSON.parse(require("fs").re
 mkdir lively.next-node_modules
 mkdir esm_cache
 mkdir local_projects
+PROJECT_FOLDER_CREATED=$?
+# When we just created the local_projects folder, the partsbin inside cannot exist.
+if (( PROJECT_FOLDER_CREATED == 0 ));
+then
+  git clone https://github.com/LivelyKernel/partsbin ./local_projects/LivelyKernel--partsbin
+  echo "Downloaded up-to-date version of lively.nexts partsbin"
+else
+  cd local_projects
+  if [ -d "LivelyKernel--partsbin" ];
+  # `partsbin` exists, we need to update the repository, while preservering its local state.
+  then
+    echo "Found an existing lively.next partsbin"
+    cd LivelyKernel--partsbin
+    currentBranchName=$(git rev-parse --abbrev-ref HEAD)
+    stashOutput=$(git stash)
+    git checkout main
+    git pull origin main --ff-only
+    git checkout "$currentBranchName"
+    # https://stackoverflow.com/a/12973694/4418325
+    stashOutputWithoutWhiteSpace=$(echo "$stashOutput" | xargs)
+    if [ "$stashOutputWithoutWhiteSpace" != "No local changes to save" ];
+    then
+      git stash pop
+    fi
+    echo "Updated existing lively.next partsbin"
+    cd ..
+  else
+    # `partsbin` does not exist yet, we can just clone it.
+    git clone https://github.com/LivelyKernel/partsbin LivelyKernel--partsbin
+    echo "Downloaded up-to-date version of lively.nexts partsbin"
+  fi
+  cd ..
+fi
 
 node --no-experimental-fetch --no-warnings --experimental-loader $lv_next_dir/flatn/resolver.mjs \
      lively.installer/bin/install.cjs $PWD \
