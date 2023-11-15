da50de4425cf72c1c670fc4d77c97536a9f525c5
Robin Schreiber <robin.schreiber@me.com>
2023-11-15 21:55:05 +0100

    👔: properly import modules via esm instead of global ref

--- lively.server/tests/subserver-test.js
+++ lively.server/tests/subserver-test.js
@@ -1,23 +1,23 @@
-/*global beforeEach, afterEach, before, after, describe, it,System*/
+/* global beforeEach, afterEach, before, after, describe, it,System */
 
-import { expect } from "mocha-es6";
+import { expect } from 'mocha-es6';
 
-import { resource, ensureFetch } from "lively.resources";
-import LivelyServer from "../server.js";
+import { resource, ensureFetch } from 'lively.resources';
+import { module } from 'lively.modules';
+import LivelyServer from '../server.js';
 
 // Array.from(LivelyServer.servers.keys())
 
-var hostname = "localhost", port = 9009, testServer;
+let hostname = 'localhost'; let port = 9009; let testServer;
 
-var subserver1Path = `lively.server/tests/subserver1.js`;
-var subserver1FullPath = System.decanonicalize(subserver1Path);
-var modulesToRemove = [];
+let subserver1Path = 'lively.server/tests/subserver1.js';
+let subserver1FullPath = System.decanonicalize(subserver1Path);
+let modulesToRemove = [];
 
-describe('subservers - lively.server', function() {
-
+describe('subservers - lively.server', function () {
   before(async () => {
     await ensureFetch();
-    testServer = LivelyServer.ensure({port, hostname});
+    testServer = LivelyServer.ensure({ port, hostname });
     await testServer.whenStarted();
   });
 
@@ -28,14 +28,14 @@ describe('subservers - lively.server', function() {
 
   afterEach(async () => {
     modulesToRemove = modulesToRemove.map(ea =>
-      typeof ea === "string" ? lively.modules.module(ea) : ea);
+      typeof ea === 'string' ? module(ea) : ea);
     await Promise.all(modulesToRemove.map(ea => ea.unload()));
     await Promise.all(modulesToRemove.map(ea => resource(ea.id).remove()));
     modulesToRemove.ength = 0;
-    testServer.removePlugin("MySubserver")
+    testServer.removePlugin('MySubserver');
   });
 
-  it("can add and remove subserver", async () => {
+  it('can add and remove subserver', async () => {
     modulesToRemove.push(subserver1Path);
     await resource(subserver1FullPath).write(`
 export default class MySubserver {
@@ -50,11 +50,10 @@ export default class MySubserver {
 `);
     await testServer.addSubserver(subserver1Path);
     let response = await resource(`http://${hostname}:${port}/subserver/MySubserver`).read();
-    expect(response).equals("MySubserver is running!");
+    expect(response).equals('MySubserver is running!');
 
     await testServer.removeSubserver(subserver1Path);
     let response2 = await resource(`http://${hostname}:${port}/subserver/MySubserver`).read();
-    expect(response2).equals("lively.server");
+    expect(response2).equals('lively.server');
   });
-
 });
