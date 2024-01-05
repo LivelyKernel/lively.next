d6d26eba5042c3eadc36c8186b81645cd6d11f3e
Linus Hagemann <mail@linushagemann.de>
2024-01-05 17:57:58 +0100

    🛠️: circumvent buggy github action summary by building it manually

--- scripts/test.js
+++ scripts/test.js
@@ -4,9 +4,9 @@ The script assumes that a lively server is running locally on port 9011.
 This script gets used by `test.sh`. To invoke tests, run `test.sh`.
 You should rarely (never) want to invoke this script here directly.
 
-This script suppots two "modes" of running tests, running them locally or inside of a GitHub Action.
+This script supports two "modes" of running tests, running them locally or inside of a GitHub Action.
 In the latter case, we use some special formatting hints for actions in our output and provide a markdown summary of failing tests
-to be rendered by GitHub.
+to be rendered by GitHub in a file called `test_output.md`. Otherwise, information is logged only to the console.
 Since this script gets called by another shell script, that we need to run further, even if a test fails in here, we communicate some
 summary statistics and other information in the form of text files, that then get used in the outer shell script.
 */
@@ -21,8 +21,7 @@ let passed = 0; let failed = 0; let skipped = 0;
 let markdownListOfFailingTests = '';
 
 if (CI) {
-  console.log(`::notice:: Tests for ${targetPackage} 📦`);
-  fs.appendFileSync('summary.txt', `### Tests for ${targetPackage} 📦\n`);
+  console.log(`Running Tests for ${targetPackage} 📦`);
 } else {
   console.log(`ℹ️ Tests for ${targetPackage} 📦`);
   console.log('');
@@ -44,8 +43,7 @@ const req = http.request(options, res => {
       data = JSON.parse(data);
       if (!Object.keys(data).length) {
         if (CI) {
-          console.log(`::notice:: ${targetPackage} does not contain any tests\n`);
-          fs.appendFileSync('summary.txt', `ℹ️ ${targetPackage} does not contain any tests.\n`);
+          console.log(`ℹ️ ${targetPackage} does not contain any tests\n`);
         } else {
           console.log(`ℹ️ ${targetPackage} does not contain any tests`);
         }
@@ -101,7 +99,7 @@ const req = http.request(options, res => {
           } else {
             failed += 1;
             if (CI) {
-              console.log(`::error:: ${test.fullTitle} failed ❌`);
+              console.log(`${test.fullTitle} failed ❌`);
               markdownListOfFailingTests = markdownListOfFailingTests + `- ${test.fullTitle} failed ❌\n`;
             } else {
               console.log(`${test.fullTitle} failed ❌`);
@@ -112,20 +110,17 @@ const req = http.request(options, res => {
         else console.log('');
       });
         console.log(`SUMMARY-passed:${passed}`);
-        if (CI) fs.appendFileSync('summary.txt', `✅ ${passed} tests passed\n`);
         console.log(`SUMMARY-skipped:${skipped}`);
-        if (CI) fs.appendFileSync('summary.txt', `⏩ ${skipped} tests skipped\n`);
         console.log(`SUMMARY-failed:${failed}`);
-        if (CI) fs.appendFileSync('summary.txt', `❌ ${failed} tests failed\n`);
         if (CI && markdownListOfFailingTests !== '') {
-          const firstLineOfSummary = `While running the tests for ${targetPackage}, the following ${failed} test(s) failed:\n`;
+          const firstLineOfSummary = `\n---\nWhile running the tests for **${targetPackage}**, the following ${failed} test(s) failed:\n`;
           fs.appendFileSync('failing.txt', firstLineOfSummary);
           fs.appendFileSync('failing.txt', markdownListOfFailingTests);
         }
     } catch (err) {
       if (CI) {
         console.log(`::error:: Running the tests produced the following error:\n"${err}"`);
-        fs.appendFileSync('summary.txt', `❌ Running the tests produced the following error:\n"${err}"\n`);
+        fs.appendFileSync('test_output.md', `\n---\n❌ Running the tests for **${targetPackage}** produced the following error:\n"${err}"\n`);
       } else {
         console.log(`❌ Running the tests produced the following error:\n"${err}"`);
       }
@@ -137,7 +132,7 @@ const req = http.request(options, res => {
 req.on('error', err => {
   if (CI) {
     console.log(`::error:: Error while trying to get the results of tests for ${targetPackage}`);
-    fs.appendFileSync('summary.txt', `❌ Error while trying to get the results of tests for ${targetPackage}\n`);
+    fs.appendFileSync('test_output.md', `❌ Error while trying to get the results of tests for ${targetPackage}\n`);
   }
   else console.log(`❌ Error while trying to get the results of tests for ${targetPackage}`);
   console.log(err);
