49625270dffe9c0c3bf3231a11f1063725ea99ad
Robin Schreiber <robin.schreiber@me.com>
2024-01-08 13:44:52 +0900

    🎨: fix whenFontReady() chrome bug for now

--- lively.morphic/text/morph.js
+++ lively.morphic/text/morph.js
@@ -1090,6 +1090,11 @@ export class Text extends Morph {
   }
 
   async whenFontLoaded () {
+    // FIXME: there seems to be a bug in the current version of chrome (120),
+    //        where fonts.check() returns true, even though the font face is still
+    //        unloaded or still loading. As a remporary measure, we will now
+    //        wait for the fonts.ready promise, which appears to fix the issues.
+    await document.fonts.ready;
     if (this.allFontsLoaded()) return true;
     const usedFonts = this.usedFonts();
     const success = await Promise.all(usedFonts.map(fontString => this.fontMetric.supportedFontCache.has(fontString) || document.fonts.load(fontString)));
