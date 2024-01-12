a5e77117b46a601f698cd0a53f036adb1e688741
Linus Hagemann <mail@linushagemann.de>
2024-01-12 14:19:11 +0100

    🛠️: fix identification of changes in nested folders

--- scripts/tests_for_branch.sh
+++ scripts/tests_for_branch.sh
@@ -8,14 +8,14 @@ fi
 CHANGED_FILES=$(git diff --name-only origin/$GITHUB_BASE_REF...)
 
 # Use -E for MacOS-compatible lazy evaluation
-# to ensure that we only match lively core **directories** and not files that include lively in their name 
-RELEVANT_CHANGED_FILES="$(grep -E 'lively\..+?\/.+' <<< "$CHANGED_FILES")"
+# to ensure that we only match lively core **directories** and not files that include lively in their name.
+# Use `cut` to only collect the first part of each path, yielding top-level lively packages.
+# This might make it so that the script needs to be called from inside the lively.next folder - for CI its ok.
+CHANGED_FOLDERS="$(grep -E 'lively\..+?\/.+' <<< "$CHANGED_FILES" | cut -d "/" -f1)"
 # Check if any file was affected, otherwise we need to explicitly tell `test.sh`
-if [ -z "${RELEVANT_CHANGED_FILES}" ];
+if [ -z "${CHANGED_FOLDERS}" ];
 then
     CHANGED_FOLDERS='none'
-else
-    CHANGED_FOLDERS="$(dirname $RELEVANT_CHANGED_FILES)"
 fi
 # Echo to force output into a single string.
 echo $(sort -u <<< "$CHANGED_FOLDERS")
