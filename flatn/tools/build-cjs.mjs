4fd58d126d7c512f029cf4c0c7cc03cefe513d42
Robin Schreiber <robin.schreiber@me.com>
2024-01-22 14:02:45 +0900

    ❄️🫓📂: ensure build scripts return exit code 1 on error

--- flatn/tools/build-cjs.mjs
+++ flatn/tools/build-cjs.mjs
@@ -3,34 +3,41 @@ import { rollup } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import { flatnResolve } from '../module-resolver.js';
 
-const bundle = await rollup({
-  input: './index.js',
-  plugins: [
-    {
-      resolveId: async (id, parentURL) => {
-        // directly use flatn to resolve this shit
-        if (id.startsWith('node:'))
-          return { id: id.replace('node:', ''), external: true };
-        try {
-          if (id.startsWith('lively.')) {
-            return await flatnResolve(id, parentURL);
+try {
+  
+  const bundle = await rollup({
+    input: './index.js',
+    plugins: [
+      {
+        resolveId: async (id, parentURL) => {
+          // directly use flatn to resolve this shit
+          if (id.startsWith('node:'))
+            return { id: id.replace('node:', ''), external: true };
+          try {
+            if (id.startsWith('lively.')) {
+              return await flatnResolve(id, parentURL);
+            }
+            if (!id.startsWith('.')) 
+              return await flatnResolve(id, parentURL);
+          } catch (err) {
+            return null; 
           }
-          if (!id.startsWith('.')) 
-            return await flatnResolve(id, parentURL);
-        } catch (err) {
-          return null; 
         }
-      }
-    },
-    commonjs({
-      ignoreDynamicRequires: true,
-      exclude: [/node:.*/]
-    })
-  ]
-});
+      },
+      commonjs({
+        ignoreDynamicRequires: true,
+        exclude: [/node:.*/]
+      })
+    ]
+  });
+  
+  await bundle.write({
+    format: 'cjs',
+    inlineDynamicImports: true,
+    file: 'flatn-cjs.js'
+  });
 
-await bundle.write({
-  format: 'cjs',
-  inlineDynamicImports: true,
-  file: 'flatn-cjs.js'
-});
+} catch (err) {
+  console.log(err);
+  process.exit(1);
+}
