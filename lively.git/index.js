56639e9a7dcd27bc6f27f0e35828c8cf06649e16
Linus Hagemann <mail@linushagemann.de>
2024-02-15 16:43:13 +0100

    🛤️: add functions to interact with secrets/deploy keys via GitHub API

--- lively.git/index.js
+++ lively.git/index.js
@@ -1,5 +1,9 @@
 /* global fetch */
 import { currentUserToken } from 'lively.user';
+import { default as libsod } from 'esm://cache/libsodium-wrappers';
+
+// TODO: This could all use some sensible error handling -lh 15.02.2024
+
 export class GitHubAPIWrapper {
   static async remoteRepoInfos (repoOwner, repoName) {
     const token = currentUserToken();
@@ -11,6 +15,75 @@ export class GitHubAPIWrapper {
       }
     });
     return await res.json();
+  }
+
+  static async addDeployKey (repoOwner, repoName, title, key) {
+    const res = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/keys`, {
+      method: 'POST',
+      headers: {
+        accept: 'application/vnd.github+json',
+        authorization: `Bearer ${currentUserToken()}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      },
+      body: JSON.stringify({
+        title,
+        key,
+        read_only: 'true'
+      })
+    });
+  }
+
+  static async listActionSecrets (repoOwner, repoName) {
+    const res = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/actions/secrets`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${currentUserToken()}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    const secretData = await res.json();
+    return secretData.secrets.map(s => s.name);
+  }
+
+  static async retrieveRepositoriesPublicKey (repoOwner, repoName) {
+    const res = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/actions/secrets/public-key`, {
+      headers: {
+        accept: 'application/vnd.github+json',
+        authorization: `Bearer ${currentUserToken()}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    const keyData = await res.json();
+    return {
+      key: keyData.key,
+      id: keyData.key_id
+    };
+  }
+
+  static async addOrUpdateRepositorySecret (repoOwner, repoName, secretName, secret, publicKey, publicKeyId) {
+    await libsod.ready;
+    // Convert the secret and key to a Uint8Array.
+    let binkey = libsod.from_base64(publicKey, libsod.base64_variants.ORIGINAL);
+    let binsec = libsod.from_string(secret);
+
+    // Encrypt the secret using libsodium
+    let encBytes = libsod.crypto_box_seal(binsec, binkey);
+
+    // Convert the encrypted Uint8Array to Base64
+    const encryptedSecret = libsod.to_base64(encBytes, libsod.base64_variants.ORIGINAL);
+    const res = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/actions/secrets/${secretName}`, {
+      method: 'PUT',
+      headers: {
+        accept: 'application/vnd.github+json',
+        authorization: `Bearer ${currentUserToken()}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      },
+      body: JSON.stringify({
+        encrypted_value: `${encryptedSecret}`,
+        key_id: publicKeyId
+      })
+    });
   }
 
   static async listGithubBranches (repoOwner, repoName) {
