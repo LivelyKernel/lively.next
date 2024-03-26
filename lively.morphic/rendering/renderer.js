de602e9c99bbd39da2bdefbbdcc65454cc387e24
Robin Schreiber <robin.schreiber@me.com>
2024-03-26 16:19:26 +0100

    🎨: ensure that scroll is set after submorph bounds updates

--- lively.morphic/rendering/renderer.js
+++ lively.morphic/rendering/renderer.js
@@ -153,7 +153,7 @@ export default class Renderer {
     for (let morph of morphsToHandle) {
       if (morph.renderingState.hasMorphRemoved) this.morphsWithStructuralChanges.unshift(morph);
       else if (morph.renderingState.hasStructuralChanges) this.morphsWithStructuralChanges.push(morph);
-      if (morph.renderingState.needsRerender) this.renderedMorphsWithChanges.push(morph);
+      if (morph.renderingState.needsRerender) this.renderedMorphsWithChanges.unshift(morph);
       if (morph.renderingState.animationAdded) this.renderedMorphsWithAnimations.push(morph);
       if (morph.renderingState.cssLayoutToMeasureWith) this.renderedMorphsToBeMeasured.unshift([morph, morph.renderingState.cssLayoutToMeasureWith]);
     }
