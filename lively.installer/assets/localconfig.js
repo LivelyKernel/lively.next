a262bb3907f2e7962d4674ab687a76b12ab6e55c
Linus Hagemann <mail@linushagemann.de>
2024-04-03 13:06:06 +0200

    📦: add config for improved ease of use

--- lively.installer/assets/localconfig.js
+++ lively.installer/assets/localconfig.js
@@ -1,2 +1,4 @@
 // This module can be used for statically customizing the lively browser client
 // It gets run when a lively world loads.  See lively.morphic/config.js for default settings.
+
+import { config } from "lively.morphic";
