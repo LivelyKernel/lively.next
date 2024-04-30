daa7917790a52bd47ffd046d4a8957c1de3bd170
Linus Hagemann <mail@linushagemann.de>
2024-04-30 15:50:41 +0200

    🧰🎨: add option to fade in or always show scrollbars in world browser

--- lively.ide/studio/world-browser.cp.js
+++ lively.ide/studio/world-browser.cp.js
@@ -1,3 +1,4 @@
+/* global SERVER_URL */
 /* global FormData */
 /* eslint-disable no-use-before-define */
 import { easings, ViewModel, touchInputDevice, World, MorphicDB, Image, HTMLMorph, Morph, Icon, TilingLayout, Label, ConstraintLayout, ShadowObject, component, part } from 'lively.morphic';
@@ -21,6 +22,7 @@ import { resource } from 'lively.resources';
 import { defaultDirectory } from 'lively.ide/shell/shell-interface.js';
 import { signal } from 'lively.bindings';
 import { StatusMessageError } from 'lively.halos/components/messages.cp.js';
+import { config } from 'lively.morphic';
 
 export const missingSVG = `data:image/svg+xml;utf8,
 <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="question-circle" class="svg-inline--fa fa-question-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="lightgray" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"></path></svg>
@@ -642,6 +644,10 @@ export class WorldBrowserModel extends ViewModel {
   }
 
   async viewDidLoad () {
+    if (lively.FreezerRuntime) {
+      const localconfigContent = await resource(SERVER_URL + '/localconfig.js').read();
+      if (localconfigContent.replaceAll(' = ', '=').includes('config.hideScrollbarsInWorldBrowser=true')) config.hideScrollbarsInWorldBrowser = true;
+    }
     await this.displayItems();
     this.onRefresh('showCloseButton');
   }
@@ -709,7 +715,7 @@ export class WorldBrowserModel extends ViewModel {
   }
 
   async displayItems () {
-    const { loadingIndicator } = this.ui;
+    const { loadingIndicator, worldList } = this.ui;
     if (!loadingIndicator.visible) {
       await loadingIndicator.animate({
         visible: true, duration: 300
@@ -717,6 +723,7 @@ export class WorldBrowserModel extends ViewModel {
     }
     let entities = this.playgroundsMode ? await this.db.latestCommits('world') : await Project.listAvailableProjects();
     this.reset();
+    if (config.hideScrollbarsInWorldBrowser) worldList.clipMode = 'hidden';
     // Filter out the project that is opened anyways.
     if ($world && $world.openedProject) entities = entities.filter(availableProject => availableProject._projectName !== $world.openedProject.name);
     this.previews = entities.map(entity => {
@@ -738,7 +745,6 @@ export class WorldBrowserModel extends ViewModel {
           reactToSubmorphAnimations: true
         });
       };
-
       return placeholder;
     });
     fun.debounceNamed('hide-ld-' + loadingIndicator.id, 300, () => {
@@ -808,10 +814,6 @@ export class GrowingWorldList extends Morph {
     };
   }
 
-  onLoad () {
-    this.clipMode = 'auto';
-  }
-
   // switch to a lazy list, that has items and adds them to the morph until it covers the whole
   // viewport. Then consecutively adds in batches of the current #[width / item.width] until all
   // items are consumed
@@ -829,7 +831,7 @@ export class GrowingWorldList extends Morph {
   }
 
   onHoverIn () { this.clipMode = 'auto'; }
-  onHoverOut () { if (!touchInputDevice) this.clipMode = 'hidden'; }
+  onHoverOut () { if (!touchInputDevice && config.hideScrollbarsInWorldBrowser) this.clipMode = 'hidden'; }
 
   // add top and bottom buffer
 
@@ -1529,7 +1531,7 @@ const WorldBrowser = component({
   {
     type: GrowingWorldList,
     name: 'world list',
-    clipMode: 'hidden',
+    clipMode: 'scroll',
     extent: pt(879.7, 580),
     fill: Color.rgba(46, 75, 223, 0),
     items: [],
