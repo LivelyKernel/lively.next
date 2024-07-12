416258d7a4a66abc4cbf8c936587090f915b09d6
Robin Schreiber <robin.schreiber@me.com>
2024-07-12 12:22:34 +0200

    🎨: apply rotation for root of component for now

--- lively.morphic/components/policy.js
+++ lively.morphic/components/policy.js
@@ -1052,7 +1052,7 @@ export class StylePolicy {
    */
   synthesizeSubSpec (submorphNameInPolicyContext, ownerOfScope, previousTarget) {
     const isRoot = !submorphNameInPolicyContext;
-    const transformProps = ['extent', 'position', 'rotation', 'scale', 'lineHeight'];
+    const transformProps = ['extent', 'position', 'scale', 'lineHeight'];
     let subSpec = this.getSubSpecFor(submorphNameInPolicyContext) || {}; // get the sub spec for the submorphInPolicyContext
 
     if (subSpec.isPolicy) {
