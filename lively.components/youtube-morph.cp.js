cbf7b2bd736150cac5c1f8fd6000fd2f1faef189
Linus Hagemann <mail@linushagemann.de>
2024-07-16 17:10:03 +0200

    🎛️🎨: adapt CSS for YT lib, make import robust & max-width configurable

--- lively.components/youtube-morph.cp.js
+++ lively.components/youtube-morph.cp.js
@@ -1,16 +1,18 @@
 import { component, ViewModel, HTMLMorph } from 'lively.morphic';
-import liteYouTubeEmbed from 'lite-youtube-embed';
 import { pt } from 'lively.graphics/geometry-2d.js';
 
 class YouTubeEmbedViewModel extends ViewModel {
   static get properties () {
     return {
-      videoID: { defaultValue: 'baY3SaIhfl0' }
+      videoID: { defaultValue: 'baY3SaIhfl0' },
+      maxWidth: { defaultValue: 'unset' }
     };
   }
 
   async viewDidLoad () {
-    this.view.html = `<lite-youtube videoid="${this.videoID}" width="100%" height="100%" style="max-width: unset; height: 100%;"></lite-youtube>`;
+    // Browsers do not like us importing this in more than once place...
+    window.liteYouTubeEmbed ? null : (window.liteYouTubeEmbed = await System.import('lite-youtube-embed'));
+    this.view.html = `<lite-youtube videoid="${this.videoID}" width="100%" height="100%" style="max-width: ${this.maxWidth};"></lite-youtube>`;
   }
 }
 
