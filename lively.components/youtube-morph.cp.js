5da48522c481de61a482cc3e2f21683d9fdcd7e7
Linus Hagemann <mail@linushagemann.de>
2024-07-22 18:24:39 +0200

    🎛️: prevent multiple instances of `lite-youtube-embed` from executing
    Co-authored-by: Robin Schreiber <robin.schreiber@me.com>

--- lively.components/youtube-morph.cp.js
+++ lively.components/youtube-morph.cp.js
@@ -1,5 +1,6 @@
 import { component, ViewModel, HTMLMorph } from 'lively.morphic';
 import { pt } from 'lively.graphics/geometry-2d.js';
+import { fun } from 'lively.lang';
 
 class YouTubeEmbedViewModel extends ViewModel {
   static get properties () {
@@ -10,8 +11,12 @@ class YouTubeEmbedViewModel extends ViewModel {
   }
 
   async viewDidLoad () {
-    // Browsers do not like us importing this in more than once place...
-    window.liteYouTubeEmbed ? null : (window.liteYouTubeEmbed = await System.import('lite-youtube-embed'));
+    // Browsers do not like us importing this in more than once...
+    if (!window.liteYouTubeEmbed) {
+      await fun.guardNamed('import-lite-youtube-embed', async () => {
+        window.liteYoutubeEmbed = await System.import('lite-youtube-embed');
+      })();
+    }
     this.view.html = `<lite-youtube videoid="${this.videoID}" width="100%" height="100%" style="max-width: ${this.maxWidth};"></lite-youtube>`;
   }
 }
