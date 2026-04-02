030f35bb14c525fcee30b4f3d176ab1b44e0ed1e
merryman <robin.schreiber@me.com>
2026-04-02 18:00:12 +0200

    fix: revert jspm generator env to ["browser"]
    The env: ["browser", "module", "import"] change caused jspm to resolve
    PouchDB to index-browser.es.js (minified ESM) instead of
    index-browser.js. The minified build has a top-level `var re` that
    gets incorrectly scope-captured by lively.modules, breaking PouchDB's
    IndexedDB initialization.

--- lively.server/plugins/lib-lookup.js
+++ lively.server/plugins/lib-lookup.js
@@ -68,7 +68,7 @@ async function findAvailableCDNVersion (name, failingVersion) {
 
 function createGenerator (inputMap, resolutions) {
   return new Generator({
-    env: ["browser", "module", "import"],
+    env: ["browser"],
     defaultProvider: 'jspm.io',
     inputMap,
     ...(Object.keys(resolutions).length ? { resolutions } : {})
